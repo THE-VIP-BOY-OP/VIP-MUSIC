9d4140ca2d139d63ec233566f9a6e65c2858a8de
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-02-17 12:41:30 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAIjTGKPYCq3keRZgNbshxtJ5k7H609OAAIZBgACYAF5VIerYoMcSln8JAQ")
+                await message.reply_sticker("CAACAgQAAxkBAANlY-8T3DH_f4W5G0epCHYwEj68IXkAApINAAKg8ylTZIjlbuvUoPsuBA")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
