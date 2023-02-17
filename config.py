9ef82eac6b9313d8a468777f0d2fee98be928a2d
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-02-17 09:06:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/XcuteboyX/....hh")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/XcuteboyX/ANUJ-X-MUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
