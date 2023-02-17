b4cee77e7d869e9f7cd251397a75e9fc69ece7af
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-02-17 08:52:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,12 +16,12 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴀɴᴏɴ ダ ᴍᴜsɪᴄ")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/XcuteboyX/SofiaMusicBot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://te.legra.ph/file/b3509c179bda853d9a7b3.mp4")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
