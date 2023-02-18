51533b8102499abc55e853fbeda13e99799e7f8e
Abhijeet <122460282+Gugdhh@users.noreply.github.com>
2023-02-18 17:48:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://te.legra.ph/file/be1d79f0f8d48c7e65c43.mp4")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Gugdhh/ABHI_MUSIC_BOOOOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
