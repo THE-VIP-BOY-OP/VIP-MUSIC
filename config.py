a96ba320c2f8c4a1d7d6f03055e033a18b6feecc
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-13 19:24:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIPMUSIC",
+    "https://github.com/THE-VIP-BOY-OP/ABHI_MUSIC_BOOOOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
