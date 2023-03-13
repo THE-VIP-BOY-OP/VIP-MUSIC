2d332bfe92506e44d1334c27d3ce2cbc407f7bce
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-13 19:18:48 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,13 +9,13 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🤗 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🤗",
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🙀 ʜᴇʟᴩ 🙀",
+                text="ʜᴇʟᴩ",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
@@ -30,21 +30,26 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🤗 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🤗",
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🙀 ʜᴇʟᴩ 🙀", callback_data="settings_back_helper"
+                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="☆ sᴜᴩᴩᴏʀᴛ ☆", url=config.SUPPORT_GROUP
+                text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="😎 ᴀɴᴜᴊ 😎", user_id=OWNER
+                text="🥀 ᴍᴀɪɴᴛᴀɪɴᴇʀ 🥀", user_id=OWNER
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
             )
         ],
      ]
