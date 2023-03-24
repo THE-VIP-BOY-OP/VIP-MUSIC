1b83f3c7b8c867874159af06d957835495828a30
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-25 01:55:28 +0530

    Update help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -59,7 +59,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_sticker("CAACAgQAAxkBAAEIS-JkHgLJstzZ_79COyhWcb11sxYbiAACGBAAAnxX8VA0u63rdZ5xay8E")
+        await update.reply_sticker("CAACAgQAAxkBAAEIS-tkHgcHBD5exIFrJe_zYjrzwOoOzAACGxAAAnxX8VBqvu_MKA-UNC8E")
         await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
