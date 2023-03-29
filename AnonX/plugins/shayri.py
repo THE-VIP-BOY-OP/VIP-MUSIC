bd49160bdcfecd65552ee2e6a3fcbdd93bfa64bd
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-30 00:01:10 +0530

    Update shayri.py

--- AnonX/plugins/shayri.py
+++ AnonX/plugins/shayri.py
@@ -1,1 +1,30 @@
 
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from AnonX import app
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+SHAYRI = [ "SOON UPDATING TOO MANY SHAYRI... PLEASE WAIT NA🥲🥲" ]
+
+@app.on_message(
+    filters.command("shayri")
+    & filters.group
+    & ~filters.edited & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        text = random.choice(SHAYRI),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC")
+                ]
+            ]
+        ),
+    )
