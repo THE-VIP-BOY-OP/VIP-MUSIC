832b187ddabb7c44b49cb7f587772ffe4ef1e0ba
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-30 17:35:45 +0530

    Update shayri.py

--- AnonX/plugins/shayri.py
+++ AnonX/plugins/shayri.py
@@ -60,8 +60,7 @@ SHAYRI_COMMAND = get_command("SHAYRI_COMMAND")
     & filters.group
     & ~filters.edited & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
-    await message.reply_text( 
-        text = random.choice(SHAYRI),
+    await message.reply_text(f"{reply.from_user.mention} {(random.choice(SHAYRI))}"),
         reply_markup=InlineKeyboardMarkup(
             [
                 [
