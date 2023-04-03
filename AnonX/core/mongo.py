abc4e836b192c3577a89d62571f693be7e19e61f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-03 15:23:57 +0530

    Update mongo.py

--- AnonX/core/mongo.py
+++ AnonX/core/mongo.py
@@ -9,4 +9,4 @@ from ..logging import LOGGER
 _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
 _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
 mongodb = _mongo_async_.Vip
-pymongodb = _mongo_sync_.Vi
+pymongodb = _mongo_sync_.Vip
