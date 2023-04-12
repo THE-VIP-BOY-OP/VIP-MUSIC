77ca76e35e6350b232877d267042c87b98729bfd
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-04-12 18:23:54 +0530

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -67,7 +67,7 @@ async def init():
     except:
         pass
     await Anon.decorators()
-    LOGGER("AnonX").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("AnonX").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗕𝗥𝗔𝗡𝗗𝗘𝗗♨️\n╚═════ஜ۩۞۩ஜ════╝")
     await idle()
 
 
