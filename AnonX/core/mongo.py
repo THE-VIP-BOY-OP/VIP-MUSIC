342d8d2b59bd4912b6c25e1dcb07dbf3e770b650
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-06 13:15:33 +0530

    Add files via upload

---
+++ AnonX/core/mongo.py
@@ -0,0 +1,12 @@
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+
+import config
+
+from ..logging import LOGGER
+
+
+_mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+_mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+mongodb = _mongo_async_.Vip
+pymongodb = _mongo_sync_.Vip
