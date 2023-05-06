8dc3d8a3529b3ac0f5e540fbb7e7117a716797a6
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-06 13:37:25 +0530

    Add files via upload

---
+++ AnonX/plugins/telegraph.py
@@ -0,0 +1,16 @@
+from telegraph import upload_file
+from pyrogram import filters
+from AarohiX import app
+
+
+@app.on_message(filters.command('tgm'))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("🌹ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...ʙʏ ᴀᴀʀᴏʜɪ ᴍᴜsɪᴄ🌹")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://telegra.ph" + x
+
+        i.edit(f'🇾ᴏᴜʀ🇹ᴇʟᴇɢʀᴀᴘʜ 👉 {url}')
