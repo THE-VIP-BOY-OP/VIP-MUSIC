371da7c3fa8ea4eb166ced3c539d6a67cdcf764c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-10 10:29:18 +0530

    Update reload.py

--- VipX/plugins/reload.py
+++ VipX/plugins/reload.py
@@ -48,8 +48,8 @@ from VipX import app
 from VipX.core.call import Vip
 from VipX.misc import db
 from VipX.utils.database import get_authuser_names, get_cmode
-from VipX.utils.decorators import (ActualAdminCB, AdminActual,
-from VipX.utils.command import commandpro                     language)
+from VipX.utils.decorators import (ActualAdminCB, AdminActual, language)
+from VipX.utils.command import commandpro                     
 from VipX.utils.formatters import alpha_to_int
 
 ### Multi-Lang Commands
