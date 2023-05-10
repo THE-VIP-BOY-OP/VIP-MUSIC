b35db0ac9f624d07484272978a39cc4069772d11
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-05-10 22:21:17 +0530

    Update telegraph.py

--- VipX/plugins/telegraph.py
+++ VipX/plugins/telegraph.py
@@ -7,7 +7,7 @@ from VipX import app
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("🌹ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...ʙʏ ᴀᴀʀᴏʜɪ ᴍᴜsɪᴄ🌹")
+        i = message.reply("🌹𝐌𝐚𝐤𝐢𝐧𝐠 𝐀 𝐋𝐢𝐧𝐤 𝐎𝐟 𝐘𝐨𝐮𝐫 𝐃𝐨𝐜𝐮𝐦𝐞𝐧𝐭 𝐁𝐚𝐛𝐲....🌹")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
