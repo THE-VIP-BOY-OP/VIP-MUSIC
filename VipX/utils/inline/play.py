cae8fc82b294189d9ab964f3c2de4af099825706
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-11 09:39:00 +0530

    Update play.py

--- VipX/utils/inline/play.py
+++ VipX/utils/inline/play.py
@@ -550,3 +550,396 @@ def telegram_markup_timer(_, chat_id, played, dur):
 
     return buttons
     
+def stream_markup(_, videoid, chat_id):
+
+    buttons  = [   
+
+            [
+
+            InlineKeyboardButton(
+
+                text="▷",
+
+                callback_data=f"ADMIN Resume|{chat_id}",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="II", callback_data=f"ADMIN Pause|{chat_id}"
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
+
+            ),
+
+        ],
+
+        [
+
+            InlineKeyboardButton(
+
+                text="《",
+
+                callback_data=f"ADMIN 1|{chat_id}",
+
+            ),
+
+            
+
+            InlineKeyboardButton(
+
+                text="ᴄʟᴏsᴇ", callback_data=f"close",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="》",
+
+                callback_data=f"ADMIN 2|{chat_id}",
+
+            ),
+
+        ],
+
+    ]
+
+    return buttons
+
+def telegram_markup(_, chat_id):
+    buttons  = [   
+            [
+            InlineKeyboardButton(
+                text="▷",
+                callback_data=f"ADMIN Resume|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="II", callback_data=f"ADMIN Pause|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+
+                text="《",
+
+                callback_data=f"ADMIN 1|{chat_id}",
+
+            ),
+            
+            InlineKeyboardButton(
+
+                text="ᴄʟᴏsᴇ", callback_data=f"close",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="》",
+
+                callback_data=f"ADMIN 2|{chat_id}",
+
+            ),
+
+        ],
+    ]
+
+    return buttons
+## Search Query Inline
+
+def track_markup(_, videoid, user_id, channel, fplay):
+
+    buttons = [
+
+        [
+
+            InlineKeyboardButton(
+
+                text=_["P_B_1"],
+
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text=_["P_B_2"],
+
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+
+            ),
+
+        ],
+
+        [
+
+            InlineKeyboardButton(
+
+                text="《",
+
+                callback_data=f"ADMIN 1|{chat_id}",
+
+            ),
+
+            
+
+            InlineKeyboardButton(
+
+                text="ᴄʟᴏsᴇ", callback_data=f"close",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="》",
+
+                callback_data=f"ADMIN 2|{chat_id}",
+
+            ),
+
+        ],
+
+    ]
+
+    return buttons
+## Live Stream Markup
+
+def livestream_markup(_, videoid, user_id, mode, channel, fplay):
+
+    buttons = [
+
+        [
+
+            InlineKeyboardButton(
+
+                text=_["P_B_3"],
+
+                callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+
+            ),
+
+        ],
+
+        [
+
+            InlineKeyboardButton(
+
+                text=_["CLOSEMENU_BUTTON"],
+
+                callback_data=f"forceclose {videoid}|{user_id}",
+
+            ),
+
+        ]
+
+    ]
+
+    return buttons
+
+## wtf
+
+def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
+
+    buttons = [
+
+        [
+
+            InlineKeyboardButton(
+
+                text=_["P_B_1"],
+
+                callback_data=f"AnonPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text=_["P_B_2"],
+
+                callback_data=f"AnonPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+
+            ),
+
+        ],
+
+        [
+
+            InlineKeyboardButton(
+
+                text=_["CLOSE_BUTTON"],
+
+                callback_data=f"forceclose {videoid}|{user_id}",
+
+            ),
+
+        ],
+
+    ]
+
+    return buttons
+## Slider Query Markup
+
+def slider_markup(
+
+    _, videoid, user_id, query, query_type, channel, fplay
+
+):
+
+    query = f"{query[:20]}"
+
+    buttons = [
+
+        [
+
+            InlineKeyboardButton(
+
+                text=_["P_B_1"],
+
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text=_["P_B_2"],
+
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+
+            ),
+
+        ],
+
+        [
+
+            InlineKeyboardButton(
+
+                text="◁",
+
+                callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text=_["CLOSE_BUTTON"],
+
+                callback_data=f"forceclose {query}|{user_id}",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="▷",
+
+                callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+
+            ),
+
+        ],
+
+    ]
+
+    return buttons
+
+## Extra Shit
+
+close_keyboard = InlineKeyboardMarkup( 
+
+            [
+
+                [
+
+                    InlineKeyboardButton(
+
+                        text="✯ ᴄʟᴏsᴇ ✯", callback_data="close"
+
+                    )
+
+                ]    
+
+            ]
+
+        )
+
+## Queue Markup
+
+def queue_markup(_, videoid, chat_id):
+
+    buttons = [
+
+        [
+
+            InlineKeyboardButton(
+
+                text="▷",
+
+                callback_data=f"ADMIN Resume|{chat_id}",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="II", callback_data=f"ADMIN Pause|{chat_id}"
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
+
+            ),
+
+        ],
+
+        [  
+
+            InlineKeyboardButton(
+
+                text="《",
+
+                callback_data=f"ADMIN 1|{chat_id}",
+
+            ),
+
+            
+
+            InlineKeyboardButton(
+
+                text="ᴄʟᴏsᴇ", callback_data=f"close",
+
+            ),
+
+            InlineKeyboardButton(
+
+                text="》",
+
+                callback_data=f"ADMIN 2|{chat_id}",
+
+            ),
+
+        ],
+
+    ]
+
+    return buttons
