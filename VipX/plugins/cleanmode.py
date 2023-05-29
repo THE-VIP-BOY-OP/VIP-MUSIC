0700f8fbc15806e9c9644fa9550b8d5469857997
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-30 01:04:49 +0530

    Update cleanmode.py

--- VipX/plugins/cleanmode.py
+++ VipX/plugins/cleanmode.py
@@ -8,7 +8,7 @@ from pyrogram.raw import types
 
 import config
 from config import adminlist, chatstats, clean, userstats
-OWNER_ID.append(6201816030)
+.append(6
 from strings import get_command
 from VipX import app, userbot
 from VipX.misc import SUDOERS
