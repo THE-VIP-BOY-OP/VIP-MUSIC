a32a7fa5839d69c12e0804c4518c781cb75417a5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-07-10 08:13:16 +0530

    Update play.py

--- VipX/utils/inline/play.py
+++ VipX/utils/inline/play.py
@@ -1,7 +1,7 @@
 import math
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import SUPPORT_CHANNEL, SUPPORT_GROUP, GROUP_USERNAME, CHANNEL_USERNAME
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP, GROUP_USERNAME, CHANNEL_USERNAME
 from VipX import app
 
 import config
