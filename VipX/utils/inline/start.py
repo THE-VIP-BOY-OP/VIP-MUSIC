ca326e011a7d6b5740aea1d22a5138cc24020a5b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-07-20 19:42:25 +0530

    Update start.py

--- VipX/utils/inline/start.py
+++ VipX/utils/inline/start.py
@@ -2,18 +2,11 @@ from typing import Union
 import re
 import os
 from os import getenv
-
 from dotenv import load_dotenv
-
 from pyrogram import filters
-
-
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 import config
-load_dotenv()
-GROUP_USERNAME = getenv("GROUP_USERNAME")
-CHANNEL_USERNAME = getenv("CHANNEL_USERNAME")
+from config import GROUP_USERNAME, CHANNEL_USERNAME
 
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
