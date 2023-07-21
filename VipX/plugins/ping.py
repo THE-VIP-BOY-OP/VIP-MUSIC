00936993b6cb9afd9c6435fd982c10800bf34f7d
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-07-21 19:48:53 +0530

    Update ping.py

--- VipX/plugins/ping.py
+++ VipX/plugins/ping.py
@@ -28,9 +28,9 @@ async def ping_com(client, message: Message, _):
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        _["ping_3"],)
+        _["ping_3"])
     await response.edit_text(
-        _["ping_4"],)
+        _["ping_4"])
     await response.edit_text(
         _["ping_2"].format(
             resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
