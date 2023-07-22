f4f644496b30526c30c08aaabc7d3e61bdbabf73
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-07-22 07:59:30 +0530

    Update gali.py

--- VipX/plugins/gali.py
+++ VipX/plugins/gali.py
@@ -147,6 +147,6 @@ async def help(client: Client, message: Message):
 @app.on_message(
     filters.command("gali")
     & filters.group
-    & ~filters.edited & filter.group & ~filters.edited)
+    & ~filters.edited & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_text("**𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐈𝐬 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐃𝐦, 𝐆𝐨 𝐓𝐨 𝐁𝐨𝐭 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐀𝐧𝐝 𝐓𝐲𝐩𝐞 /gali 𝐂𝐨𝐦𝐦𝐚𝐧𝐝.**")
