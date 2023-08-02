bfe4580238492f44a3c89e3ed711a9e457257f3a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-08-02 11:25:49 +0530

    Update next.py

--- VipX/plugins/next.py
+++ VipX/plugins/next.py
@@ -26,7 +26,7 @@ HELP_COMMAND = get_command("HELP_COMMAND")
     & ~BANNED_USERS
 )
 @app.on_callback_query(
-    filters.regex("settings_back_helper") & ~BANNED_USERS
+    filters.regex("settings_back_helper2") & ~BANNED_USERS
 )
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
