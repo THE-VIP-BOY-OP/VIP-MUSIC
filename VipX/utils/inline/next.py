cc7d0b870721dc00f02ff89f0c7a7a01ec16212c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-08-02 11:40:18 +0530

    Update next.py

--- VipX/utils/inline/next.py
+++ VipX/utils/inline/next.py
@@ -3,7 +3,7 @@ from typing import Union
 import asyncio
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-def help_pannel(_, START: Union[bool, int] = None):
+def help_pannel2(_, START: Union[bool, int] = None):
     first = [
         InlineKeyboardButton(
             text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
