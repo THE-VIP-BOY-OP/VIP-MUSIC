76f21e37b0f255c1825226cf1c1d1ddf6280f99a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-08-08 09:57:29 +0530

    Update thumbnails.py

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -124,7 +124,7 @@ async def gen_thumb(videoid, user_id):
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
-                (650, 25),
+                (400, 25),
                 f"STARTED PLAYING",
                 fill="white",
                 stroke_width=3,
