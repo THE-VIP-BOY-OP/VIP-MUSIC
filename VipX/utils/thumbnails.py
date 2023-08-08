b29e8fec889646c436c05cd31f99533259147de9
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-08-08 22:42:45 +0530

    Update thumbnails.py

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -147,7 +147,7 @@ async def gen_thumb(videoid, user_id):
             pass
         text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
         draw.text(
-            ((1280 - text_w) / 2.25, 240),
+            ((1280 - text_w) / 2, 240),
             f"Duration:- {duration} Minutes",
             fill="pink",
             font=arial,
