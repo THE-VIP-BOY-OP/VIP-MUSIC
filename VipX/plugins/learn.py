b84ab851febfaf300ab6b86d869cac82c1d87baa
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-08-20 11:59:35 +0530

    Update learn.py

--- VipX/plugins/learn.py
+++ VipX/plugins/learn.py
@@ -1,2 +1,15 @@
 # 𝗪𝗘𝗟𝗖𝗢𝗠𝗘 𝗜𝗡 𝗢𝗨𝗥 𝗟𝗘𝗔𝗥𝗡𝗜𝗡𝗚 𝗙𝗜𝗟𝗘 𝗗𝗔𝗥𝗟𝗜𝗡𝗚.
 # 𝗜 𝗪𝗜𝗟𝗟 𝗧𝗘𝗟𝗟 𝗨𝗛 𝗔𝗕𝗢𝗨𝗧 𝗨𝗦𝗘 𝗢𝗙 𝗖𝗢𝗠𝗠𝗔𝗠𝗗𝗦.
+
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from VipX import app
+import string
+from strings import get_command
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
