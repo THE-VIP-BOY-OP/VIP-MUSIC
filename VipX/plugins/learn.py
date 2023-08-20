7dc6f37072561804aaca9a61b0cf0adba73bfb25
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-08-20 13:50:03 +0530

    Update learn.py

--- VipX/plugins/learn.py
+++ VipX/plugins/learn.py
@@ -11,9 +11,9 @@ import time
 from VipX import app
 import string
 from strings import get_command
-
+import config
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
+from config import START_IMAGE_URL
 # Command
 PING_COMMAND = ["ping", "alive"]
 START_COMMAND = ["start", "mstart"]
@@ -85,7 +85,7 @@ GBANNED_COMMAND = ["gbannedusers", "gbanlist", "gbanned"]
     & ~filters.edited & filters.private & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/2ff2dab0dd5953e674c79.jpg",
+        photo=f"START_IMAGE_URL",
         caption=f"""**◈ 𝐓𝙷𝙸𝚂 𝐂𝙾𝙼𝙼𝙰𝙽𝙳 𝐔𝚂𝙴 𝐈𝙽 𝐎𝙽𝙻𝚈 𝐆𝚁𝙾𝚄𝙿𝚂 𝐁𝙰𝙱𝚈 **\n**◈ 𝐆𝙾 𝐓𝙾 𝐆𝚁𝙾𝚄𝙿𝚂/𝐀𝙳𝙳 𝐌𝙴 𝐈𝙽 𝐆𝚁𝙾𝚄𝙿𝚂 𝐀𝙽𝙳 𝐔𝚂𝙴 /play 𝐂𝙾𝙼𝙼𝙰𝙽𝙳.**\n**◈ 𝐓𝙷𝙰𝙽𝙺 𝐔𝙷 𝐁𝙰𝙱𝚈.**""",
         reply_markup=InlineKeyboardMarkup(
             [
