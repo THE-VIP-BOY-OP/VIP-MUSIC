1195394ceebc308bd7b920e61fcf8577d79a4f0a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-09-14 22:00:50 +0530

    Update bot.py

--- VipX/core/bot.py
+++ VipX/core/bot.py
@@ -16,7 +16,6 @@ class VipXBot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
         )
 
     async def start(self):
