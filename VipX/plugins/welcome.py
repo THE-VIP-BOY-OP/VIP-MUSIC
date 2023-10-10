485f83f54706b193a47aa05c5094e0195aad35e4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-10 09:15:37 +0530

    Create welcome.py

---
+++ VipX/plugins/welcome.py
@@ -0,0 +1,16 @@
+python
+from pyrogram import Client, filters
+
+# Create a new instance of the Client
+app = Client("my_bot")
+
+# Create a filter to handle new member join events
+@Client.on_message(filters.new_chat_members)
+def welcome_new_members(client, message):
+    chat_id = message.chat.id
+    new_members = message.new_chat_members
+    for member in new_members:
+        client.send_message(chat_id=chat_id, text=f"Welcome {member.first_name}!")
+
+# Run the bot
+app.run()
