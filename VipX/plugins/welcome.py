620d11d23a987ab1d2286e9760a0d53a06f867e1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-10 16:39:52 +0530

    Update welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -12,23 +12,23 @@ chat = message.chat
 for member in message.new_chat_members:
 count = await app.get_chat_members_count(chat.id)
 
-        msg = (
-            f"🌷{message.from_user.mention} Welcome in a New Group🥳\n\n"
-            f"📌Chat Name: {message.chat.title}\n"
-            f"🔐Chat Username: @{message.chat.username}\n"
-            f"💖Your ID: {message.from_user.id}\n"
-            f"✍️Your Username: @{message.from_user.username}\n"
-            f"👥Completed {count} Members🎉"
-        )
+msg = (
+    f"🌷{message.from_user.mention} Welcome in a New Group🥳\n\n"
+    f"📌Chat Name: {message.chat.title}\n"
+    f"🔐Chat Username: @{message.chat.username}\n"
+    f"💖Your ID: {message.from_user.id}\n"
+    f"✍️Your Username: @{message.from_user.username}\n"
+    f"👥Completed {count} Members🎉"
+      )
       
-        # Send the group's profile photo
-        group_photo = await app.get_chat(chat.id).photo.big_file_id
+# Send the group's profile photo
+group_photo = await app.get_chat(chat.id).photo.big_file_id
         
-        await app.send_photo(
-            message.chat.id,
-            photo=group_photo,
-            caption=msg,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(f"Invite Me", url=f"https://t.me/{app.username}?startgroup=true")]
-            ])
-        )
+await app.send_photo(
+    message.chat.id,
+    photo=group_photo,
+    caption=msg,
+    reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton(f"Invite Me", url=f"https://t.me/{app.username}?startgroup=true")]
+    ])
+)
