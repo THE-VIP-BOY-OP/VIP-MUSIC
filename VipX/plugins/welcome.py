5c1d0fbb2a3562175582b7a3bd0b221b110feab1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-10 08:33:20 +0530

    Update welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -1,36 +1,31 @@
-import random
-from pyrogram import Client
-from pyrogram.types import Message
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from VipX import app  
+python
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-photo = [
-    "https://telegra.ph/file/1b819cfbcb2a2d3c738f6.jpg",
-    "https://telegra.ph/file/3021c823c7f006658682f.jpg",
-    "https://telegra.ph/file/05561f0fbf323e057ab87.jpg",
-    "https://telegra.ph/file/7a6b51ee0077724254ca7.jpg",
-    "https://telegra.ph/file/b3de9e03e5c8737ca897f.jpg",
-]
+# Setup logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+# Define your bot token
+TOKEN = 'config.BOT_TOKEN'
+
+# Create an updater object
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
 
-@app.on_message(filters.new_chat_members, group=3)
-async def join_watcher(_, message):    
-    chat = message.chat
-    
-    for members in message.new_chat_members:
-        
-            count = await app.get_chat_members_count(chat.id)
-            
+def welcome_new_members(update, context):
+    chat_id = update.message.chat_id
+    new_members = update.message.new_chat_members
+    for member in new_members:
+        context.bot.send_message(chat_id=chat_id,
+                                 text=f"Welcome {member.first_name}!"
+                                 )
 
-            msg = (
-                f"**🌷{message.from_user.mention} 𝐖ᴇʟᴄᴏᴍᴇ 𝐈ɴ ᴀ 𝐍ᴇᴡ 𝐆ʀᴏᴜᴘ🥳**\n\n"
-                f"**📌𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ:** {message.chat.title}\n"
-                f"**🔐𝐂ʜᴀᴛ 𝐔.𝐍:** @{message.chat.username}\n"
-                f"**💖𝐔ʀ 𝐈d:** {message.from_user.id}\n"
-                f"**✍️𝐔ʀ 𝐔.𝐍aмe:** @{message.from_user.username}\n"
-                f"**👥𝐂ᴏᴍᴘʟᴇᴛᴇᴅ {count} 𝐌ᴇᴍʙᴇʀ𝐬🎉**"
-            )
-            await message.reply_photo(message.chat.id, photo=random.choice(photo), caption=msg, reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(f"𝐊ɪᴅɴᴀᴘ 𝐌ᴇ", url=f"https://t.me/{app.username}?startgroup=true")]
-         ]))
+# Create a handler for new members joining
+new_member_handler = MessageHandler(Filters.status_update.new_chat_members, welcome_new_members)
+dispatcher.add_handler(new_member_handler)
+
+
+# Start the bot
+updater.start_polling()
