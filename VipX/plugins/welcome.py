1e965ff7482946ab41214a4440bdddee93d2c535
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-10 17:34:34 +0530

    Update welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -3,8 +3,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from VipX import app
-from pyrogram.types import ChatPermissions
+from VipX import app  
 
 
 @app.on_message(filters.new_chat_members, group=3)
@@ -14,39 +13,15 @@ async def join_watcher(_, message):
     for members in message.new_chat_members:
         
             count = await app.get_chat_members_count(chat.id)
-        
-        
+
             msg = (
-                f"🌷{message.from_user.mention} 𝐖ᴇʟᴄᴏᴍᴇ 𝐈ɴ ᴀ 𝐍ᴇw 𝐆ʀᴏᴜᴘ🥳\n\n"
+                f"🌷{message.from_user.mention} 𝐖ᴇʟᴄᴏᴍᴇ 𝐈ɴ ᴀ 𝐍ᴇᴡ 𝐆ʀᴏᴜᴘ🥳\n\n"
                 f"📌𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ: {message.chat.title}\n"
                 f"🔐𝐂ʜᴀᴛ 𝐔.𝐍: @{message.chat.username}\n"
                 f"💖𝐔ʀ 𝐈d: {message.from_user.id}\n"
                 f"✍️𝐔ʀ 𝐔.𝐍aмe: @{message.from_user.username}\n"
                 f"👥𝐂ᴏᴍᴘʟᴇᴛᴇᴅ {count} 𝐌ᴇᴍʙᴇʀs🎉"
             )
-            await app.send_photo(message.chat.id, photo={await app.get_chat(message.chat.id).photo.big_file_id}, caption=msg, reply_markup=InlineKeyboardMarkup([
+            await app.send_photo(message.chat.id, photo=f"https://t.me/{message.chat.username}/photo}", caption=msg, reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton(f"𝐊ɪᴅɴᴀᴘ 𝐌ᴇ", url=f"https://t.me/{app.username}?startgroup=true")]
          ]))
-
-
-@app.on_message(filters.chat_created, group=3)
-async def set_default_permissions(_, message):
-    
-    chat = message.chat
-    permissions = ChatPermissions(
-        can_send_messages=True,
-        can_send_media_messages=True,
-        can_send_stickers=True,
-        can_send_other_messages=True,
-        can_add_web_page_previews=True,
-        can_invite_users=True,
-        can_pin_messages=True,
-        can_change_info=True,
-        can_delete_messages=True,
-        can_edit_messages=True,
-        can_promote_members=True,
-        can_restrict_members=True,
-        can_ban_users=True,
-        can_unban_users=True,
-    )
-    await chat.set_permissions(permissions)
