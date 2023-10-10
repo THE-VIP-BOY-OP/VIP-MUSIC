b325344bcef3b45d1a8213941836d1fcf975f3e3
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-10 09:31:06 +0530

    Update welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -6,10 +6,10 @@ from VipX import app
 # Create a filter to handle new member join events
 @app.on_message(filters.new_chat_members)
 def welcome_new_members(_, message):
-    chat_id = message.chat.id
+
     new_members = message.new_chat_members
     for member in new_members:
-        app.send_message(chat_id=chat_id, text=f"Welcome {member.first_name}!")
+        app.send_message(message.chat.id, text=f"Welcome {member.first_name}!")
 
 # Run the bot
 app.run()
