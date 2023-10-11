26a7816901e39f2b57efb4ee0aeeb53e79057f93
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-11 06:29:27 +0530

    Update welcome.py

--- VipX/plugins/welcome.py
+++ VipX/plugins/welcome.py
@@ -23,7 +23,7 @@ async def join_watcher(_, message):
             count = await app.get_chat_members_count(chat.id)
 
             msg = (
-                f"**🌷{message.from_user.mention} 𝐖ᴇʟᴄᴏᴍᴇ 𝐈ɴ ᴀ 𝐍ᴇᴡ 𝐆ʀᴏᴜᴘ🥳\n\n"
+                f"**🌷𝐇ᴇʏ{message.from_user.mention} 𝐖ᴇʟᴄᴏᴍᴇ 𝐈ɴ ᴀ 𝐍ᴇᴡ 𝐆ʀᴏᴜᴘ🥳\n\n"
                 f"**📌𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ:** {message.chat.title}\n"
                 f"**🔐𝐂ʜᴀᴛ 𝐔.𝐍:** @{message.chat.username}\n"
                 f"**💖𝐔ʀ 𝐈d:** {message.from_user.id}\n"
