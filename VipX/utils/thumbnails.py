6bcfe0affc7d9de67e131916eba9f562590f891f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-22 09:35:41 +0530

    Update thumbnails.py

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -124,7 +124,7 @@ async def gen_thumb(videoid, chat_id):
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
-                (25, 0.1),
+                (255, 0.1),
                 f"VIP  MUSIC",
                 fill="white",
                 stroke_width=4,
