a37b53b26a4a4be98388932ace04b5f04f2e87e2
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-10-22 11:08:11 +0530

    Update thumbnails.py

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -295,7 +295,7 @@ async def gen_qthumb(videoid, chat_id):
             pass
         text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
         draw.text(
-            ((1280 - text_w) / 2, 685),
+            ((1280 - text_w) / 30, 685),
             f"Duration: {duration} Mins",
             fill="white",
             font=arial,
