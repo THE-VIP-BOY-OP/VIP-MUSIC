09c6934dfa69aab8967e8ff0d2378a2e76310a76
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-11-01 13:01:31 +0530

    Update block.py

--- VipX/plugins/block.py
+++ VipX/plugins/block.py
@@ -14,7 +14,7 @@ UNBLOCK_COMMAND = get_command("UNBLOCK_COMMAND")
 BLOCKED_COMMAND = get_command("BLOCKED_COMMAND")
 
 
-@app.on_message(filters.command(BLOCK_COMMAND) & SUDOERS)
+@app.on_message(filters.command(BLOCK_COMMAND, prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 @language
 async def useradd(client, message: Message, _):
     if not message.reply_to_message:
@@ -47,7 +47,7 @@ async def useradd(client, message: Message, _):
     )
 
 
-@app.on_message(filters.command(UNBLOCK_COMMAND) & SUDOERS)
+@app.on_message(filters.command(UNBLOCK_COMMAND, prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 @language
 async def userdel(client, message: Message, _):
     if not message.reply_to_message:
@@ -71,7 +71,7 @@ async def userdel(client, message: Message, _):
     await message.reply_text(_["block_4"])
 
 
-@app.on_message(filters.command(BLOCKED_COMMAND) & SUDOERS)
+@app.on_message(filters.command(BLOCKED_COMMAND, prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
 @language
 async def sudoers_list(client, message: Message, _):
     if not BANNED_USERS:
