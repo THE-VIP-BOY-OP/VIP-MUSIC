caaea752f73bc2b431b74e74a4656ccb8ad2d3ec
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-11-03 13:02:11 +0530

    Update userbot.py

--- VipX/core/userbot.py
+++ VipX/core/userbot.py
@@ -13,35 +13,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="VipXAss1",
+            "VipXAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="VipXAss2",
+            "VipXAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="VipXAss3",
+            "VipXAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="VipXAss4",
+            "VipXAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="VipXAss5",
+            "VipXAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
