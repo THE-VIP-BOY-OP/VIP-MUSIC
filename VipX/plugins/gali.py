145b7bb81219c7f608e894609b0fe7ac1cf2a46f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-11-03 10:14:46 +0530

    Update gali.py

--- VipX/plugins/gali.py
+++ VipX/plugins/gali.py
@@ -135,8 +135,7 @@ GALI = [ "𝗠𝗔̂𝗔̂𝗗𝗔𝗥𝗖𝗛Ø𝗗 𝗧𝗘𝗥𝗜 𝗠𝗔́
 
 @app.on_message(
     filters.command("gali", prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
-    & filters.private
-    & ~filters.edited & filters.private & ~filters.edited)
+    & filters.private)
 async def help(client: Client, message: Message):
     await message.reply_text(
         text = random.choice(GALI),
@@ -146,7 +145,6 @@ async def help(client: Client, message: Message):
 
 @app.on_message(
     filters.command("gali", prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
-    & filters.group
-    & ~filters.edited & filters.group & ~filters.edited)
+    & filters.group )
 async def help(client: Client, message: Message):
     await message.reply_text("**𝐓𝐡𝐢𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐈𝐬 𝐎𝐧𝐥𝐲 𝐅𝐨𝐫 𝐃𝐦, 𝐆𝐨 𝐓𝐨 𝐁𝐨𝐭 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐀𝐧𝐝 𝐓𝐲𝐩𝐞 /gali 𝐂𝐨𝐦𝐦𝐚𝐧𝐝.**")
