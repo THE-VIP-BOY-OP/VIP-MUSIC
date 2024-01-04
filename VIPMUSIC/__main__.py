c9c98d524faa78ff8df93a48d2909acb9e62a392
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-04 21:59:22 +0530

    Add files via upload

---
+++ VIPMUSIC/__main__.py
@@ -0,0 +1,53 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from VIPMUSIC import LOGGER, app, userbot
+from VIPMUSIC.core.call import VIP
+from VIPMUSIC.misc import sudo
+from VIPMUSIC.plugins import ALL_MODULES
+from VIPMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("VIPMUSIC.plugins" + all_module)
+    LOGGER("VIPMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    await userbot.start()
+    await VIP.start()
+    await VIP.decorators()
+    LOGGER("VIPMUSIC").info(
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("VIPMUSIC").info("𝗦𝗧𝗢𝗣 𝗗𝗔𝗫𝗫 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
