e579f7c7dc9d157b33b9d3eeccdd6becc96a1fc9
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-05 01:20:13 +0530

    Create __init__.py

---
+++ VIPMUSIC/__init__.py
@@ -0,0 +1,28 @@
+from VIPMUSIC.core.bot import VIP
+from VIPMUSIC.core.dir import dirr
+from VIPMUSIC.core.git import git
+from VIPMUSIC.core.userbot import Userbot
+from VIPMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = VIP()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
