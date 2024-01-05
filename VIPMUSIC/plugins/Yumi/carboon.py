2da26d7798bd2f3eb13657516e48ce4853e2dfab
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-05 19:39:05 +0530

    Update carboon.py

--- VIPMUSIC/plugins/Yumi/carboon.py
+++ VIPMUSIC/plugins/Yumi/carboon.py
@@ -1,4 +1,4 @@
-from pyrogram import filters
+"""from pyrogram import filters
 from pyrogram.types import Message
 
 from VIPMUSIC import app
@@ -16,4 +16,35 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴩʟᴏᴀᴅɪɴɢ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴄᴀʀʙᴏɴ...")
     await pbot.send_photo(message.chat.id, carbon)
     await m.delete()
+    carbon.close()"""
+import aiohttp
+from io import BytesIO
+from VIPMUSIC import app
+from pyrogram import filters
+
+
+
+async def make_carbon(code):
+    url = "https://carbonara.solopov.dev/api/cook"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json={"code": code}) as resp:
+            image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
+
+@app.on_message(filters.command("carbon"))
+async def _carbon(client, message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴀ ᴄᴀʀʙᴏɴ.**")
+        return
+    if not (replied.text or replied.caption):
+        return await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴀ ᴄᴀʀʙᴏɴ.**")
+    text = await message.reply("Processing...")
+    carbon = await make_carbon(replied.text or replied.caption)
+    await text.edit("**ᴜᴘʟᴏᴀᴅɪɴɢ...**")
+    await message.reply_photo(carbon)
+    await text.delete()
     carbon.close()
