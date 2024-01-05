5b6aa0b85fb14e4c323088fb2d6184bfbaca2310
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-05 07:48:52 +0530

    Add files via upload

---
+++ VIPMUSIC/plugins/tools/gitinfo.py
@@ -0,0 +1,57 @@
+import aiohttp
+from pyrogram import filters
+from VIPMUSIC import app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@app.on_message(filters.command(["github", "git"]))
+async def github(_, message):
+    if len(message.command) != 2:
+        await message.reply_text("/git VIP-BOY-OP")
+        return
+
+    username = message.text.split(None, 1)[1]
+    URL = f'https://api.github.com/users/{username}'
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(URL) as request:
+            if request.status == 404:
+                return await message.reply_text("404")
+
+            result = await request.json()
+
+            try:
+                url = result['html_url']
+                name = result['name']
+                company = result['company']
+                bio = result['bio']
+                created_at = result['created_at']
+                avatar_url = result['avatar_url']
+                blog = result['blog']
+                location = result['location']
+                repositories = result['public_repos']
+                followers = result['followers']
+                following = result['following']
+
+                caption = f"""ɢɪᴛʜᴜʙ ɪɴғᴏ ᴏғ {name}
+                
+ᴜsᴇʀɴᴀᴍᴇ: {username}
+ʙɪᴏ: {bio}
+ʟɪɴᴋ: [Here]({url})
+ᴄᴏᴍᴩᴀɴʏ: {company}
+ᴄʀᴇᴀᴛᴇᴅ ᴏɴ: {created_at}
+ʀᴇᴩᴏsɪᴛᴏʀɪᴇs: {repositories}
+ʙʟᴏɢ: {blog}
+ʟᴏᴄᴀᴛɪᴏɴ: {location}
+ғᴏʟʟᴏᴡᴇʀs: {followers}
+ғᴏʟʟᴏᴡɪɴɢ: {following}"""
+
+            except Exception as e:
+                print(str(e))
+                pass
+
+    # Create an inline keyboard with a close button
+    close_button = InlineKeyboardButton("Close", callback_data="close")
+    inline_keyboard = InlineKeyboardMarkup([[close_button]])
+
+    # Send the message with the inline keyboard
+    await message.reply_photo(photo=avatar_url, caption=caption, reply_markup=inline_keyboard)
---
+++ VIPMUSIC/plugins/tools/vctools.py
@@ -0,0 +1,81 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from VIPMUSIC import app
+from config import OWNER_ID
+# vc on
+@app.on_message(filters.video_chat_started)
+async def brah(_, msg):
+       await msg.reply("😍ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ🥳")
+# vc off
+@app.on_message(filters.video_chat_ended)
+async def brah2(_, msg):
+       await msg.reply("**😕ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ💔**")
+
+# invite members on vc
+@app.on_message(filters.video_chat_members_invited)
+async def brah3(app :app, message:Message):
+           text = f"{message.from_user.mention} 👈ɪɴᴠɪᴛᴇᴅᴛ ᴛᴏ👉 "
+           x = 0
+           for user in message.video_chat_members_invited.users:
+             try:
+               text += f"[{user.first_name}](tg://user?id={user.id}) "
+               x += 1
+             except Exception:
+               pass
+           try:
+             await message.reply(f"{text} 🤭🤭")
+           except:
+             pass
+
+
+####
+
+@app.on_message(filters.command("math", prefixes="/"))
+def calculate_math(client, message):   
+    expression = message.text.split("/math ", 1)[1]
+    try:        
+        result = eval(expression)
+        response = f"ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs : {result}"
+    except:
+        response = "ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ"
+    message.reply(response)
+
+###
+@app.on_message(filters.command("leavegroup")& filters.user(OWNER_ID))
+async def bot_leave(_, message):
+    chat_id = message.chat.id
+    text = f"sᴜᴄᴄᴇssғᴜʟʟʏ   ʟᴇғᴛ  !!."
+    await message.reply_text(text)
+    await app.leave_chat(chat_id=chat_id, delete=True)
+
+
+####
+
+
+@app.on_message(filters.command(["spg"], ["/", "!", "."]))
+async def search(event):
+    msg = await event.respond("Searching...")
+    async with aiohttp.ClientSession() as session:
+        start = 1
+        async with session.get(f"https://content-customsearch.googleapis.com/customsearch/v1?cx=ec8db9e1f9e41e65e&q={event.text.split()[1]}&key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM&start={start}", headers={"x-referer": "https://explorer.apis.google.com"}) as r:
+            response = await r.json()
+            result = ""
+            
+            if not response.get("items"):
+                return await msg.edit("No results found!")
+            for item in response["items"]:
+                title = item["title"]
+                link = item["link"]
+                if "/s" in item["link"]:
+                    link = item["link"].replace("/s", "")
+                elif re.search(r'\/\d', item["link"]):
+                    link = re.sub(r'\/\d', "", item["link"])
+                if "?" in link:
+                    link = link.split("?")[0]
+                if link in result:
+                    # remove duplicates
+                    continue
+                result += f"{title}\n{link}\n\n"
+            prev_and_next_btns = [Button.inline("▶️Next▶️", data=f"next {start+10} {event.text.split()[1]}")]
+            await msg.edit(result, link_preview=False, buttons=prev_and_next_btns)
+            await session.close()
