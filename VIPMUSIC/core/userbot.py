2250ffb3367b426831dc89a9d9f9dcbd5879b91b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-05 08:35:37 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -1,7 +1,18 @@
 from pyrogram import Client
+import re
+from os import getenv
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from dotenv import load_dotenv
+from pyrogram import filters
 
+load_dotenv()
+
+from VIPMUSIC import app
 import config
-
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+STRING_SESSION = getenv("STRING_SESSION", "")
+from dotenv import load_dotenv
 from ..logging import LOGGER
 
 assistants = []
@@ -59,7 +70,7 @@ class Userbot(Client):
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.one.send_message(config.LOGGER_ID, "Assistant Started, {MONGO_DB_URI}")
                                          
             except:
                 LOGGER(__name__).error(
