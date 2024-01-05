cefb298bf207d75eb76e2745c5849b6641dc8cc2
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-05 08:45:30 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -69,7 +69,7 @@ class Userbot(Client):
             assistants.append(1)
             try:
                 await self.one.send_message(config.LOGGER_ID, "Assistant Started, {MONGO_DB_URI}")
-                                         
+                await self.one.send_message(config.LOGGER_ID, "Assistant Started !")                   
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
