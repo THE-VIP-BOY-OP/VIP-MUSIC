fad78466ed6036181876f08cfe56894b95c81f11
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-06 16:44:16 +0530

    Update banall.py

--- VIPMUSIC/plugins/admins/banall.py
+++ VIPMUSIC/plugins/admins/banall.py
@@ -5,23 +5,19 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from VIPMUSIC.utils.vip_ban import admin_filter
 from VIPMUSIC.misc import SUDOERS
 
-
-BOT_ID = "{app.id}"
+BOT_ID = app.me.id  # Corrected this line
 
 @app.on_message(filters.command("banall") & SUDOERS)
-async def ban_all(_,msg):
-    chat_id=msg.chat.id    
-    bot=await app.get_chat_member(chat_id,BOT_ID)
-    bot_permission=bot.privileges.can_restrict_members==True    
+async def ban_all(_, msg):
+    chat_id = msg.chat.id    
+    bot = await app.get_chat_member(chat_id, BOT_ID)
+    bot_permission = bot.privileges.can_restrict_members == True    
     if bot_permission:
         async for member in app.get_chat_members(chat_id):       
             try:
-                    await app.ban_chat_member(chat_id, member.user.id)
-                    await msg.reply_text(f"ғᴜᴄᴋɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴀɴᴅ ᴛʜᴇɪʀ ᴍᴏᴍs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")                    
+                await app.ban_chat_member(chat_id, member.user.id)
+                await msg.reply_text(f"ғᴜᴄᴋɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴀɴᴅ ᴛʜᴇɪʀ ᴍᴏᴍs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")                    
             except Exception:
                 pass
     else:
         await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
-                                         
-    
-           
