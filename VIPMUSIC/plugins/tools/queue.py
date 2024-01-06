c5d044b74ad2dca37b8bc2f0b561865c95809632
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-06 14:02:32 +0530

    Update queue.py

--- VIPMUSIC/plugins/tools/queue.py
+++ VIPMUSIC/plugins/tools/queue.py
@@ -176,7 +176,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
         if "✨" in msg:
             msg = msg.replace("✨", "")
-        link = await DAXXBin(msg)
+        link = await VIPBin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
     else:
