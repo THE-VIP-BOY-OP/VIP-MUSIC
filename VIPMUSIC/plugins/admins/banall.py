a9720835bd888bac2c9bcd0751c557fbd3509cb5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-06 16:12:59 +0530

    Update banall.py

--- VIPMUSIC/plugins/admins/banall.py
+++ VIPMUSIC/plugins/admins/banall.py
@@ -3,12 +3,12 @@ from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from VIPMUSIC.utils.vip_ban import admin_filter
+from VIPMUSIC.misc import SUDOERS
 
 
+BOT_ID = "{app.id}"
 
-BOT_ID = "6192926426"
-
-@app.on_message(filters.command("ball") & admin_filter)
+@app.on_message(filters.command("banall") & SUDOERS)
 async def ban_all(_,msg):
     chat_id=msg.chat.id    
     bot=await app.get_chat_member(chat_id,BOT_ID)
