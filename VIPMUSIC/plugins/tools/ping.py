e9eb3f6da261546867cfc38c3c9ab84dd78568fe
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-06 23:46:15 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -26,6 +26,7 @@ async def make_carbon(code):
 @app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
+    response = await message.reply_text("🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ...")
     start = datetime.now()
     pytgping = await VIP.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
@@ -59,3 +60,4 @@ async def ping_com(client, message: Message, _):
     ]
     ),
         )
+    await response.delete()
