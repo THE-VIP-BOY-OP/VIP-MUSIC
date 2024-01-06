352d3de535c3126b7f7f9e8ca949515669670e7c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-06 19:40:00 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -1,66 +1,35 @@
-import requests
+from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
-from datetime import datetime
 from VIPMUSIC import app
 from VIPMUSIC.core.call import VIP
 from VIPMUSIC.utils import bot_sys_stats
 from VIPMUSIC.utils.decorators.language import language
 from VIPMUSIC.utils.inline import supp_markup
 from config import BANNED_USERS, PING_IMG_URL
-
-CARBON_API_URL = "https://carbonara.solopov.dev/api/cook"
-
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
-@language
-async def ping_com(client, message: Message, _):
-    start = datetime.now()
-    pytgping = await VIP.ping()
-    UP, CPU, RAM, DISK = await bot_sys_stats()
-    resp = (datetime.now() - start).microseconds / 1000
-
-    # Prepare the code to be sent to Carbon
-    code = f"""
-from pyrogram import filters
-from pyrogram.types import Message
-from datetime import datetime
+import aiohttp
+from io import BytesIO
 from VIPMUSIC import app
-from VIPMUSIC.core.call import VIP
-from VIPMUSIC.utils import bot_sys_stats
-from VIPMUSIC.utils.decorators.language import language
-from VIPMUSIC.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from pyrogram import filters
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+
+
+async def make_carbon(code):
+    url = "https://carbonara.solopov.dev/api/cook"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json={"code": code}) as resp:
+            image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+@app.on_message(filters.command("pong", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     pytgping = await VIP.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-    await message.reply_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
-    )
-"""
-
-    # Carbon API Request payload
-    payload = {
-        "code": code,
-        "backgroundColor": "white",
-    }
-
-    try:
-        # Make the request to Carbon API
-        response = requests.post(CARBON_API_URL, json=payload)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-
-        # Get the image URL from the response
-        image_url = response.json().get("url")
-
-        # Send the image URL as a reply
-        await message.reply_text(f"Generated Carbon Image: {image_url}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error making request to Carbon API: {e}")
-        await message.reply_text("An error occurred while processing the image. Please try again.")
+    text =  _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping)
+    carbon = await make_carbon(text)
+    await message.reply_photo(carbon)
+    await text.delete()
