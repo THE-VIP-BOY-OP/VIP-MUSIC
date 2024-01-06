6b8b49a5bb6dbe5b9bf54a05ea3f82b7a5b47c98
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-06 18:46:21 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -8,12 +8,8 @@ from VIPMUSIC.core.call import VIP
 from VIPMUSIC.utils import bot_sys_stats
 from VIPMUSIC.utils.decorators.language import language
 from VIPMUSIC.utils.inline import supp_markup
-from config import BANNED_USERS
-from datetime import datetime
-from PIL import Image, ImageDraw, ImageFont
-import requests
+from config import BANNED_USERS, PING_IMG_URL
 
-# ... (previous imports)
 
 @app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
@@ -22,32 +18,7 @@ async def ping_com(client, message: Message, _):
     pytgping = await VIP.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-    
-    # Prepare the code to be sent to Carbon
-    code = f"""Response Time: {resp} ms
-UP: {UP} | RAM: {RAM} | CPU: {CPU} | DISK: {DISK}
-Pyrogram Ping: {pytgping}"""
-
-    # Carbon API endpoint
-    carbon_api_url = "https://carbonara.solopov.dev/api/cook"
-
-    # Request payload
-    payload = {
-        "code": code,
-        "backgroundColor": "white",
-        "theme": "seti",
-        "dropShadow": True,
-        "fontSize": 18,
-        "lineNumbers": False,
-        "watermark": False,
-    }
-
-    # Make the request to Carbon API
-    response = requests.post(carbon_api_url, json=payload)
-
-    # Save the image locally
-    with open("carbon_ping_result.png", "wb") as f:
-        f.write(response.content)
-
-    # Send the image
-    await message.reply_photo(photo="carbon_ping_result.png", caption="Ping results drawn on Carbon with colorful text")
+    await message.reply_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
