33ac5838f2f85920d18596cc14adf3303df1fdbd
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-11 21:00:04 +0530

    Update autoleave.py

--- VIPMUSIC/plugins/misc/autoleave.py
+++ VIPMUSIC/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001802990747
-                                and i.chat.id != -1001802990747
+                                and i.chat.id != -1001465277194
+                                and i.chat.id != -1002120144597
                             ):
                                 if left == 20:
                                     continue
