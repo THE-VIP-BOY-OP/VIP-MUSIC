aac0a4890d132e47cbde021087951cfb2ca0f3c0
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-18 11:12:43 +0530

    Update autoapprove.py

--- VIPMUSIC/plugins/bot/autoapprove.py
+++ VIPMUSIC/plugins/bot/autoapprove.py
@@ -105,4 +105,4 @@ async def autoapprove(client: app, message: ChatJoinRequest):
     print(f"{user.first_name} Joined 🤝")  # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        await client.send_photo(chat_id=chat.id, photo=welcome_photo, caption=TEXT.format(mention=user.mention, title=chat.title))
