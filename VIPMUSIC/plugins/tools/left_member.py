d9e84097704d1ab470399e98c2befc45f369300f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-23 13:44:19 +0530

    Update left_member.py

--- VIPMUSIC/plugins/tools/left_member.py
+++ VIPMUSIC/plugins/tools/left_member.py
@@ -101,7 +101,7 @@ async def member_has_left(client: app, member: ChatMemberUpdated):
             photo=welcome_photo,
             caption=caption,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(button_text, url=f"https://t.me/{user.username}")]
+                [InlineKeyboardButton(button_text, url=profile)]
             ])
         )
     except RPCError as e:
