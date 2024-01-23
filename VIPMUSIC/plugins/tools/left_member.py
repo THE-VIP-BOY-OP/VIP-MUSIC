c77c20a67344b75d7b2f3bd63cdffec7695def2b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-23 14:24:56 +0530

    Update left_member.py

--- VIPMUSIC/plugins/tools/left_member.py
+++ VIPMUSIC/plugins/tools/left_member.py
@@ -94,7 +94,7 @@ async def member_has_left(client: app, member: ChatMemberUpdated):
 
         # Generate a deep link to open the user's profile
         profile = f"https://t.me/{user.username}" if https://t.me/{user.username} else https://t.me/{app.username}
-
+        profile_link = f"openmessage://?user_id={user.id}"
         # Send the message with the photo, caption, and button
         await client.send_photo(
             chat_id=member.chat.id,
