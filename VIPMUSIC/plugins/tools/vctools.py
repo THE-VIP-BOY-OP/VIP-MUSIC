a2ee120591daeb83ace7e1b38d0bc8f343943201
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-25 07:13:19 +0530

    Update vctools.py

--- VIPMUSIC/plugins/tools/vctools.py
+++ VIPMUSIC/plugins/tools/vctools.py
@@ -14,7 +14,7 @@ async def brah2(_, msg):
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
 async def brah3(app :app, message:Message):
-           text = f"**➻** {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛᴇᴅ ᴛᴏ ɪɴ ᴠᴄ**\n\n**➻**"
+           text = f"**➻** {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ**\n\n**➻ **"
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
