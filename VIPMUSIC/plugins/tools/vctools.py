d28368bbe720142f540323235d2d8eed8b78a065
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-25 07:20:54 +0530

    Update vctools.py

--- VIPMUSIC/plugins/tools/vctools.py
+++ VIPMUSIC/plugins/tools/vctools.py
@@ -2,6 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from VIPMUSIC import app
 from config import OWNER_ID
+from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
@@ -13,19 +16,28 @@ async def brah2(_, msg):
 
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
-async def brah3(app :app, message:Message):
-           text = f"**➻** {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ**\n\n**➻ **"
-           x = 0
-           for user in message.video_chat_members_invited.users:
-             try:
-               text += f"[{user.first_name}](tg://user?id={user.id}) "
-               x += 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f" {text} 🤭🤭")
-           except:
-             pass
+async def brah3(app: app, message: Message):
+    text = f"**➻** {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ**\n\n**➻ **"
+    x = 0
+    for user in message.video_chat_members_invited.users:
+        try:
+            text += f"[{user.first_name}](tg://user?id={user.id}) "
+            x += 1
+        except Exception:
+            pass
+
+    try:
+        invite_link = await app.export_chat_invite_link(message.chat.id)
+        reply_text = f"{text} 🤭🤭"
+
+        # Creating the inline button with the invite link
+        join_button = InlineKeyboardButton("๏ ᴊᴏɪɴ ᴠᴄ ๏", url=invite_link)
+        keyboard = InlineKeyboardMarkup([[join_button]])
+
+        await message.reply(reply_text, reply_markup=keyboard)
+    except Exception as e:
+        print(f"Error: {e}")
+
 
 
 ####
