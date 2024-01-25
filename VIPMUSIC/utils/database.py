f151c2b0f36d8b6e288a45847f89ca9767803e34
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-25 15:38:15 +0530

    Update database.py

--- VIPMUSIC/utils/database.py
+++ VIPMUSIC/utils/database.py
@@ -21,6 +21,7 @@ playtypedb = mongodb.playtypedb
 skipdb = mongodb.skipmode
 sudoersdb = mongodb.sudoers
 usersdb = mongodb.tgusersdb
+privatedb = mongodb.privatechats
 
 # Shifting to memory [mongo sucks often]
 active = []
@@ -37,7 +38,7 @@ pause = {}
 playmode = {}
 playtype = {}
 skipmode = {}
-
+privatechats = {}
 
 async def get_assistant_number(chat_id: int) -> str:
     assistant = assistantdict.get(chat_id)
@@ -644,3 +645,34 @@ async def remove_banned_user(user_id: int):
     if not is_gbanned:
         return
     return await blockeddb.delete_one({"user_id": user_id})
+
+# Private Served Chats
+
+
+async def get_private_served_chats() -> list:
+    chats_list = []
+    async for chat in privatedb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def is_served_private_chat(chat_id: int) -> bool:
+    chat = await privatedb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def add_private_chat(chat_id: int):
+    is_served = await is_served_private_chat(chat_id)
+    if is_served:
+        return
+    return await privatedb.insert_one({"chat_id": chat_id})
+
+
+async def remove_private_chat(chat_id: int):
+    is_served = await is_served_private_chat(chat_id)
+    if not is_served:
+        return
+    return await privatedb.delete_one({"chat_id": chat_id})
+
