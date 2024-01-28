69d3b8ae677302cdb574b76e4a6bdcb57134b055
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-28 23:43:02 +0530

    Update settings.py

--- VIPMUSIC/utils/inline/settings.py
+++ VIPMUSIC/utils/inline/settings.py
@@ -16,7 +16,7 @@ def setting_markup(_):
             InlineKeyboardButton(text=_["ST_B_4"], callback_data="VM"),
         ],
         [
-            InlineKeyboardButton(text= "๏ ʜᴇʟᴘ ๏, callback_data=f"settingsback_helper"),
+            InlineKeyboardButton(text= "๏ ʜᴇʟᴘ ๏", callback_data=f"settingsback_helper"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
