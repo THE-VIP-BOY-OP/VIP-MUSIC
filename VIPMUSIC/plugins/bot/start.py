6c8723e18e286da3237810d70bd64589f8ea7662
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-28 12:19:29 +0530

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -49,7 +49,7 @@ YUMI_PICS = [
 
 ]
 
-@app.on_message(filters.command(["tstart"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
