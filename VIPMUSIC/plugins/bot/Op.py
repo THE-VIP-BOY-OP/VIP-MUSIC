2805209adbf87ba347894a699827b7b75d018ae1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-28 08:30:00 +0530

    Update Op.py

--- VIPMUSIC/plugins/bot/Op.py
+++ VIPMUSIC/plugins/bot/Op.py
@@ -361,20 +361,3 @@ def callback_query(client, callback_query):
             reply_markup=InlineKeyboardMarkup(PAGE2_BUTTON)
         )
         
-    elif callback_query.data == "hb21":
-        PAGE2_BUTTON = [
-            [InlineKeyboardButton("๏ ʙᴀᴄᴋ ๏", callback_data="START READING")]
-        ]
-        callback_query.edit_message_text(
-           helpers.HELP_21,
-           reply_markup=InlineKeyboardMarkup(PAGE2_BUTTON)
-       )
-
-  elif callback_query.data == "hb22":
-      PAGE2_BUTTON = [
-          [InlineKeyboardButton("๏ ʙᴀᴄᴋ ๏", callback_data="START READING")]
-       ]
-       callback_query.edit_message_text(
-          helpers.HELP_22,
-          reply_markup=InlineKeyboardMarkup(PAGE2_BUTTON)
-       )
