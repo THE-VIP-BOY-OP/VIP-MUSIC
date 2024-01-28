84e3792968840969229c9e1667d42d51d9a2a364
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-28 14:27:10 +0530

    Update pause.py

--- VIPMUSIC/plugins/admins/pause.py
+++ VIPMUSIC/plugins/admins/pause.py
@@ -1,21 +1,11 @@
 from pyrogram import filters
-from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from VIPMUSIC import app
 from VIPMUSIC.core.call import VIP
 from VIPMUSIC.utils.database import is_music_playing, music_off
 from VIPMUSIC.utils.decorators import AdminRightsCheck
 from VIPMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
-
-buttons = [
-
-        [
-            InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ", callback_data=f"ADMIN Replay|{chat_id}"),
-        ],
-    ]
-
 
 @app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
@@ -24,6 +14,14 @@ async def pause_admin(cli, message: Message, _, chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
     await VIP.pause_stream(chat_id)
+    
+    buttons = [
+        [
+            InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],
+    ]
+    
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(buttons)
     )
