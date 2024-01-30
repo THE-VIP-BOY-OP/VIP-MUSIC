c27bc2ef6380b0f6285566b3e062d8a6863abbb0
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-30 22:56:59 +0530

    Update sudoers.py

--- VIPMUSIC/plugins/sudo/sudoers.py
+++ VIPMUSIC/plugins/sudo/sudoers.py
@@ -49,7 +49,7 @@ async def userdel(client, message: Message, _):
 
 photo_url = "https://telegra.ph/file/20b4a9fd06ea4a9457a61.jpg"
 
-@app.on_message(filters.command(["sudolist", "listsudo", "sudoers"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["sudolist", "listsudo", "sudoers"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS & ~BANNED_USERS)
 @language
 async def sudoers_list(client, message: Message, _):
     keyboard = []
