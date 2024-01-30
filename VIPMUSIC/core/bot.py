4df3c2d1d447d05d2eec3df31ddc64e0596b8be4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-30 11:55:11 +0530

    Update bot.py

--- VIPMUSIC/core/bot.py
+++ VIPMUSIC/core/bot.py
@@ -10,7 +10,7 @@ class VIP(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="VIPMUSIC",
+            name="AVIPMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
