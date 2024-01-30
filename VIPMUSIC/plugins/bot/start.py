71f400a864da81bff378f7b6a4fb768331ad7a6d
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-30 13:52:22 +0530

    Update and rename start (1).py to start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -94,13 +94,44 @@ async def start_pm(client, message: Message, _):
                     chat_id=config.LOGGER_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-    else:
-        out = private_panel(_)
-        await message.reply_photo(
-            photo=config.START_IMG_URL,
-            caption=_["start_2"].format(message.from_user.mention, app.mention),
-            reply_markup=InlineKeyboardMarkup(out),
-        )
+            else:
+                out = private_panel(_)
+    m = await message.reply_text("**๏**")  # Displaying animation
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ s**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪɴ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪɴɢ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪɴɢ ʙ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪɴɢ ʙᴏ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪɴɢ ʙᴏᴛ**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪɴɢ ʙᴏᴛ.**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪɴɢ ʙᴏᴛ...**")
+    await asyncio.sleep(0.1)
+    await m.edit_text("**๏ sᴛᴀʀᴛɪɴɢ ʙᴏᴛ.**")
+    await asyncio.sleep(0.1)
+    await m.delete()  # Delete animation message
+    await message.reply_photo(
+        photo=config.START_IMG_URL,
+        caption=_["start_2"].format(message.from_user.mention, app.mention),
+        reply_markup=InlineKeyboardMarkup(out),
+    )
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
