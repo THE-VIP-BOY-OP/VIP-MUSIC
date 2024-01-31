53ff18d49434860d6546849365de70c82adbeb08
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-31 10:05:12 +0530

    Update autobroadcast.py

--- VIPMUSIC/plugins/tools/autobroadcast.py
+++ VIPMUSIC/plugins/tools/autobroadcast.py
@@ -6,13 +6,7 @@ from pyrogram.errors import FloodWait
 
 from VIPMUSIC import app
 from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import (
-    get_active_chats,
-    get_authuser_names,
-    get_client,
-    get_served_chats,
-    get_served_users,
-)
+from VIPMUSIC.utils.database import (get_active_chats, get_authuser_names, get_client,get_served_chats, get_served_users)
 from VIPMUSIC.utils.decorators.language import language
 from VIPMUSIC.utils.formatters import alpha_to_int
 from config import adminlist
