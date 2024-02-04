2026aae62e2872c629bb20900ff50b6478d44766
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-04 12:59:26 +0530

    Update play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -32,7 +32,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
         [
             InlineKeyboardButton(
                 text="Next",
-                callback_data=f"dilXaditis"
+                callback_data=f"Piyush"
             ),
         ],
     ]
@@ -85,7 +85,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text="Next",
-                callback_data=f"dilXaditis"
+                callback_data=f"Piyush"
             ),
         ],
     ]
@@ -121,7 +121,7 @@ def stream_markup(_, chat_id):
         [
             InlineKeyboardButton(
                 text="Next",
-                callback_data=f"dilXaditis"
+                callback_data=f"Piyush"
             ),
         ],
     ]
@@ -265,7 +265,7 @@ def queue_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                 text="Next",
-                callback_data=f"dilXaditis"
+                callback_data=f"Piyush"
             ),
         ],
     ]
