2010bb9dd3cbcf306ee92f9ab3a4511f639d26d8
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-04 12:19:44 +0530

    Update play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -45,23 +45,15 @@ def stream_markup_timer(_, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-    if 0 < umm <= 10:
+    if 0 < umm <= 90:
         bar = "◉—————————"
-    elif 10 < umm < 20:
+    elif 90 < umm < 91:
         bar = "—◉————————"
-    elif 20 <= umm < 30:
+    elif 91 <= umm < 92:
         bar = "——◉———————"
-    elif 30 <= umm < 40:
+    elif 92 <= umm < 93:
         bar = "———◉——————"
-    elif 40 <= umm < 50:
-        bar = "————◉—————"
-    elif 50 <= umm < 60:
-        bar = "—————◉————"
-    elif 60 <= umm < 70:
-        bar = "——————◉———"
-    elif 70 <= umm < 80:
-        bar = "———————◉——"
-    elif 80 <= umm < 95:
+    elif 93 <= umm < 95:
         bar = "————————◉—"
     else:
         bar = "—————————◉"
