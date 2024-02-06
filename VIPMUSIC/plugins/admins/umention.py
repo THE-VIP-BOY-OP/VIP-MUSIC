95802e6057d2af602b7cfebb858193f7137e552d
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2024-02-06 16:39:25 +0530

    Update umention.py

--- VIPMUSIC/plugins/admins/umention.py
+++ VIPMUSIC/plugins/admins/umention.py
@@ -8,28 +8,34 @@ from pyrogram.types import ChatPermissions
 from VIPMUSIC import app
 from VIPMUSIC.utils.vip_ban import admin_filter
 
-SPAM_CHATS = []
+SPAM_CHATS = {}
 
-@app.on_message(filters.command(["mentionall", "all", "djj"], prefixes=["/", "@", "#"]) & admin_filter)
+@app.on_message(filters.command(["mentionalll", "alll", "djsj"], prefixes=["/", "@", "#"]) & admin_filter)
 async def tag_all_users(_, message):
     global SPAM_CHATS
-    SPAM_CHATS.append(message.chat.id)
+    chat_id = message.chat.id
     text = message.text.split(None, 1)[1]
-    while message.chat.id in SPAM_CHATS:
+    SPAM_CHATS[chat_id] = True
+    f = True
+    while f:
+        while SPAM_CHATS.get(message.chat.id) == False:
+            await message.reply_text("**ᴛᴀɢ ᴀʟʟ sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")
+            return
         usernum = 0
         usertxt = ""
-        async for m in app.get_chat_members(message.chat.id):
-            if message.chat.id not in SPAM_CHATS:
-                break
-            if m.user.is_bot:
-                continue
-            usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
-                await app.send_message(message.chat.id, f'{text}\n{usertxt}')
-                usernum = 0
-                usertxt = ""
-                await asyncio.sleep(2)
+        try:
+            async for m in app.get_chat_members(message.chat.id):
+                if m.user.is_bot:
+                    continue
+                usernum+= 1
+                usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+                if usernum == 5:
+                    await app.send_message(message.chat.id, f'{text}\n{usertxt}')
+                    usernum = 0
+                    usertxt = ""
+                    await asyncio.sleep(2)
+        except Exception as e:
+            print(e)
 
 @app.on_message(filters.command(["stopdj", "stopall", "cancelmention", "offmention", "mentionoff", "alloff", "cancelall", "allcancel"], prefixes=["/", "@", "#"]))
 async def cancelcmd(_, message):
