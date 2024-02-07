fc824aa9b701b97c0d992803393b982ae3f06ed5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-07 18:08:25 +0530

    Update playlist.py

--- VIPMUSIC/plugins/play/playlist.py
+++ VIPMUSIC/plugins/play/playlist.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import os
 from random import randint
 
@@ -16,7 +7,6 @@ from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
 from config import BANNED_USERS, SERVER_PLAYLIST_LIMIT
-from strings import get_command
 from VIPMUSIC import Carbon, YouTube, app
 from VIPMUSIC.utils.database import (delete_playlist, get_playlist,
                                        get_playlist_names,
