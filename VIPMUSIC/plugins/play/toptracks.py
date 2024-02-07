557f49bd03fe4888fa82243d7b95d9d27829741c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-07 17:16:01 +0530

    Update toptracks.py

--- VIPMUSIC/plugins/play/toptracks.py
+++ VIPMUSIC/plugins/play/toptracks.py
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
 
 import asyncio
 
@@ -13,14 +5,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_global_tops,
+from VIPMUSIC import app
+from VIPMUSIC.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from VIPMUSIC.utils.decorators.language import languageCB
+from VIPMUSIC.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from YukkiMusic.utils.stream.stream import stream
+from VIPMUSIC.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
