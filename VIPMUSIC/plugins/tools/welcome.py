b71ae0ee4cd8b34cbc813ee3bc33cf20df1d442c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-11 19:32:51 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -168,10 +168,10 @@ async def greet_group(_, member: ChatMemberUpdated):
             member.chat.id,
             photo=welcomeimg,
             caption=f"""
-**❅─────✧❅✦❅✧─────❅**"""
-f"""      **✦ ᴡᴇʟᴄᴏᴍᴇ ✦**"""
+**❅─────✧❅✦❅✧─────❅**
+     ㅤㅤㅤ**✦ ᴡᴇʟᴄᴏᴍᴇ ✦**
 
-f"""▰▰▰▰▰▰▰▰▰▰▰▰▰
+▰▰▰▰▰▰▰▰▰▰▰▰▰
 **➻ ɴᴀᴍᴇ »** {user.mention}
 **➻ ɪᴅ »** `{user.id}`
 **➻ ᴜ_ɴᴀᴍᴇ »** @{user.username}
