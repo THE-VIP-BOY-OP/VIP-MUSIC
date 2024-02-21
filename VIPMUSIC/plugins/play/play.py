f2f39e01ceef3031e859befb66f964c75f2b5ae7
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-21 12:38:00 +0530

    Update play.py

--- VIPMUSIC/plugins/play/play.py
+++ VIPMUSIC/plugins/play/play.py
@@ -45,7 +45,7 @@ async def play_commnd(
     url,
     fplay,
 ):
-    mystic = await message.edit_text(
+    mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
     plist_id = None
