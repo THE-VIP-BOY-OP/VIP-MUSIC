836eca48e018c4aa15e208cd8ff26009b5781c4d
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-21 19:26:19 +0530

    Update playlist.py

--- VIPMUSIC/utils/inline/playlist.py
+++ VIPMUSIC/utils/inline/playlist.py
@@ -9,14 +9,8 @@ def botplaylist_markup(_):
                 text=_["PL_B_1"],
                 callback_data="get_playlist_playmode",
             ),
-            InlineKeyboardButton(
-                text=_["PL_B_8"], callback_data="get_top_playlists"
-            ),
         ],
         [
-            InlineKeyboardButton(
-                text=_["PL_B_4"], callback_data="PM"
-            ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
             ),
