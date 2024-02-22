a16163cdc1da0a01722036e3bd7e982bff2b8728
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-22 11:48:38 +0530

    Update youtube_playlist.py

--- VIPMUSIC/plugins/play/youtube_playlist.py
+++ VIPMUSIC/plugins/play/youtube_playlist.py
@@ -1,3 +1,99 @@
+import os
+import requests
+from random import randint
+from VIPMUSIC.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+)
+
+from pykeyboard import InlineKeyboard
+from pyrogram import filters
+from pyrogram.types import (InlineKeyboardButton, CallbackQuery,
+                            InlineKeyboardMarkup, Message)
+from VIPMUSIC.utils import close_markup
+from config import BANNED_USERS, SERVER_PLAYLIST_LIMIT
+from VIPMUSIC import Carbon, app
+from VIPMUSIC.utils.decorators.language import language, languageCB
+from VIPMUSIC.utils.inline.playlist import (botplaylist_markup,
+                                              get_playlist_markup,
+                                              warning_markup)
+from VIPMUSIC.utils.pastebin import VIPBin
+import time
+import yt_dlp
+from youtube_search import YoutubeSearch
+from youtubesearchpython import VideosSearch
+from youtubesearchpython import SearchVideos
+
+from VIPMUSIC.utils.stream.stream import stream
+from typing import Dict, List, Union
+
+from VIPMUSIC.core.mongo import mongodb
+
+
+
+playlistdb = mongodb.playlist
+playlist = []
+# Playlist Databse
+
+
+async def _get_playlists(chat_id: int) -> Dict[str, int]:
+    _notes = await playlistdb.find_one({"chat_id": chat_id})
+    if not _notes:
+        return {}
+    return _notes["notes"]
+
+
+async def get_playlist_names(chat_id: int) -> List[str]:
+    _notes = []
+    for note in await _get_playlists(chat_id):
+        _notes.append(note)
+    return _notes
+
+
+async def get_playlist(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name
+    _notes = await _get_playlists(chat_id)
+    if name in _notes:
+        return _notes[name]
+    else:
+        return False
+
+
+async def save_playlist(chat_id: int, name: str, note: dict):
+    name = name
+    _notes = await _get_playlists(chat_id)
+    _notes[name] = note
+    await playlistdb.update_one(
+        {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
+    )
+
+
+
+async def delete_playlist(chat_id: int, name: str) -> bool:
+    notesd = await _get_playlists(chat_id)
+    name = name
+    if name in notesd:
+        del notesd[name]
+        await playlistdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"notes": notesd}},
+            upsert=True,
+        )
+        return True
+    return False
+
+
+
+
+# Command
+ADDPLAYLIST_COMMAND = ("addplaylist")
+PLAYLIST_COMMAND = ("playlist")
+DELETEPLAYLIST_COMMAND = ("delplaylist")
+
 import json
 from pytube import Playlist
 from pytube import YouTube
