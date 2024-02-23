659f80a70c543bcaddecf290c7d1fc96962e755b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-23 15:16:48 +0530

    Update vctools.py

--- VIPMUSIC/plugins/tools/vctools.py
+++ VIPMUSIC/plugins/tools/vctools.py
@@ -50,17 +50,6 @@ def calculate_math(client, message):
         response = "ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ"
     message.reply(response)
 
-###
-@app.on_message(filters.command("leavegroup")& filters.user(OWNER_ID))
-async def bot_leave(_, message):
-    chat_id = message.chat.id
-    text = f"sᴜᴄᴄᴇssғᴜʟʟʏ   ʟᴇғᴛ  !!."
-    await message.reply_text(text)
-    await app.leave_chat(chat_id=chat_id, delete=True)
-
-
-####
-
 
 @app.on_message(filters.command(["spg"], ["/", "!", "."]))
 async def search(event):
