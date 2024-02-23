40714dddd6ed4554bdcbb161783f176acf326284
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-23 13:38:16 +0530

    Update play.py

--- VIPMUSIC/plugins/play/play.py
+++ VIPMUSIC/plugins/play/play.py
@@ -20,6 +20,14 @@ from VIPMUSIC.utils.inline import (
     slider_markup,
     track_markup,
 )
+from VIPMUSIC.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+)
 from VIPMUSIC.utils.logger import play_logs
 from VIPMUSIC.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
