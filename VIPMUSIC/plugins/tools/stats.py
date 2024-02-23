2c6bd7d78fbbd274c0c2bd9d15d1d5820d588bf5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-23 15:45:44 +0530

    Update stats.py

--- VIPMUSIC/plugins/tools/stats.py
+++ VIPMUSIC/plugins/tools/stats.py
@@ -19,7 +19,7 @@ from VIPMUSIC.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["stats", "gstats"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["stats", "gstats"]) & ~BANNED_USERS)
 @language
 async def stats_global(client, message: Message, _):
     upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
