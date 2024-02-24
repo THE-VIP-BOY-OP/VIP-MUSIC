c1982fe34787f3ffac4f5945c0f39079e0cc1c7e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-24 20:20:12 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -178,10 +178,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
 
 **❅─────✧❅✦❅✧─────❅**
 """)
-                reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton(button_text, url=deep_link)],
-                    [InlineKeyboardButton(text=add_button_text, url=add_link)],
-                ])
+                
             
             
         except Exception as e:
