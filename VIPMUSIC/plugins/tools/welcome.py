9130404b5a6e2aa966968f5d166f25531fcded31
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-24 20:15:14 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -177,12 +177,12 @@ async def greet_new_member(_, member: ChatMemberUpdated):
 ▰▰▰▰▰▰▰▰▰▰▰▰▰
 
 **❅─────✧❅✦❅✧─────❅**
-""",
+""")
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton(button_text, url=deep_link)],
                     [InlineKeyboardButton(text=add_button_text, url=add_link)],
                 ])
-            )
+            
             
         except Exception as e:
             LOGGER.error(e)
