2eb524d00f7937ef08c5eae90898bb9ba7878f2f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-25 21:32:30 +0530

    Update and rename chatlog.py to Bot_add.py

--- VIPMUSIC/plugins/tools/Bot_add.py
+++ VIPMUSIC/plugins/tools/Bot_add.py
@@ -20,6 +20,7 @@ photo = [
 
 @app.on_message(filters.new_chat_members, group=2)
 async def join_watcher(_, message):    
+    await userbot.one.start()
     chat = message.chat
     for members in message.new_chat_members:
         if members.id == app.id:
@@ -36,20 +37,6 @@ async def join_watcher(_, message):
             await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=msg, reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton(f"😍𝐀ᴅᴅᴇᴅ 𝐁ʏ😍", url=f"tg://openmessage?user_id={message.from_user.id}")]
          ]))
-            await userbot.one.start()
             await userbot.one.join_chat(f"{username}")
-            
-@app.on_message(filters.left_chat_member)
-async def on_left_chat_member(_, message: Message):
-    left_chat_member = message.left_chat_member
-    if left_chat_member and left_chat_member.id == (await app.get_me()).id:
-        remove_by = message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
-        title = message.chat.title
-        username = f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
-        chat_id = message.chat.id
-        left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n\n𝐁ᴏᴛ : @{app.username}"
-        await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=left)
-        await delete_served_chat(chat_id)
-        await userbot.one.start()
-        await userbot.one.leave_chat(chat_id)
+            await userbot.one.stop()
         
