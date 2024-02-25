4ea30046eeb5ca7b239f3f2a566a3efb3d4f3294
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-25 21:30:25 +0530

    Create Bot_left.py

---
+++ VIPMUSIC/plugins/tools/Bot_left.py
@@ -0,0 +1,18 @@
+
+
+
+
+@app.on_message(filters.left_chat_member)
+async def on_left_chat_member(_, message: Message):
+    await userbot.one.start()
+    left_chat_member = message.left_chat_member
+    if left_chat_member and left_chat_member.id == (await app.get_me()).id:
+        remove_by = message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+        title = message.chat.title
+        username = f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
+        chat_id = message.chat.id
+        left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n\n𝐁ᴏᴛ : @{app.username}"
+        await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=left)
+        await delete_served_chat(chat_id)
+        await userbot.one.leave_chat(chat_id)
+        await userbot.one.stop()
