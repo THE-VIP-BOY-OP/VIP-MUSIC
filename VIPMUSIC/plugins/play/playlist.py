9db5cc63b142dfd83555c12b63fc0d5f2542a8c0
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-27 09:40:40 +0530

    Update playlist.py

--- VIPMUSIC/plugins/play/playlist.py
+++ VIPMUSIC/plugins/play/playlist.py
@@ -344,8 +344,8 @@ async def add_playlist(client, message: Message, _):
         try:
             from pytube import YouTube
             
-            channel_username = query.split("/")[-1].split("?")[0]
-            videos = YouTube(f"https://youtube.com/{channel_username}")
+            channel_username = query
+            videos = YouTube(query)
             video_urls = [video['url'] for video in videos]
             
         except Exception as e:
