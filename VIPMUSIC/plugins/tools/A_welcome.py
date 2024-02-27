23324d7e9fbf46e73a8fbb195215d7b98c832d26
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-27 14:35:04 +0530

    Update A_welcome.py

--- VIPMUSIC/plugins/tools/A_welcome.py
+++ VIPMUSIC/plugins/tools/A_welcome.py
@@ -142,7 +142,7 @@ async def greet_new_members(_, member: ChatMemberUpdated):
         
         # Add the modified condition here
         if member.new_chat_member and not member.old_chat_member and member.new_chat_member.status != "kicked":
-            welcome_text = f"""**ᴡᴇʟᴄᴏᴍᴇ** {user.mention}\n**@{user.username}**"""
+            welcome_text = f"""**Wᴇʟᴄᴏᴍᴇ** {user.mention}\n**@{user.username}**"""
             await asyncio.sleep(3) 
             await userbot.one.send_message(chat_id, text=welcome_text)
     except Exception as e:
