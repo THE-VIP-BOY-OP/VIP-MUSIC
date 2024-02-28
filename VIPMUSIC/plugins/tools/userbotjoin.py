51c72b21b11d3bd9ffb29d201ce5152b79401c96
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-28 22:19:34 +0530

    Update userbotjoin.py

--- VIPMUSIC/plugins/tools/userbotjoin.py
+++ VIPMUSIC/plugins/tools/userbotjoin.py
@@ -83,22 +83,7 @@ async def join_group(client, message):
             await userbot.join_chat(message.chat.username)
             return
 
-   # Condition: Group username is present, bot is admin and Userbot is banned but bot has no invite user power
-    if message.chat.username and chat_member.status == ChatMemberStatus.ADMINISTRATOR:
-        userbot_member = await app.get_chat_member(chat_id, userbot.id)
-        if userbot_member.status not in ["banned", "restricted"]:
-            # Check if bot has invite user power
-            if not chat_member.can_invite_users:
-                await message.reply_text("I have no invite user power to invite assistant, please provide me")
-                return
-            try:
-                await app.unban_chat_member(chat_id, userbot.id)
-                await message.reply("Assistant is unbanned")
-                invite_link = await app.create_chat_invite_link(chat_id)
-                await userbot.join_chat(invite_link.invite_link)
-                await message.reply("Assistant was banned, now unbanned, and joined!")
-            except Exception as e:
-                await message.reply(str(e))
+   # 
         return
     # Condition 7: Group username is not present/private group, bot is admin
     if not message.chat.username and chat_member.status == ChatMemberStatus.ADMINISTRATOR:
@@ -117,62 +102,7 @@ async def join_group(client, message):
             return
 
 
-    # Condition 1: Group username is not present/group is private, bot is admin and Userbot is banned but bot has no ban power
-    if not message.chat.username and chat_member.status == ChatMemberStatus.ADMINISTRATOR:
-        userbot_member = await app.get_chat_member(chat_id, userbot.id)
-        if userbot_member.status in ["banned", "restricted"]:
-            # Check if bot has ban power
-            if not chat_member.can_restrict_members:
-                await message.reply_text("I don't have ban power, please provide me")
-                return
-            try:
-                await app.unban_chat_member(chat_id, userbot.id)
-                await message.reply("Assistant is unbanned")
-                invite_link = await app.create_chat_invite_link(chat_id)
-                await userbot.join_chat(invite_link.invite_link)
-                await message.reply("Assistant was banned, now unbanned, and joined!")
-            except Exception as e:
-                await message.reply(str(e))
-        return
-    
-    # Condition 2: Group username is not present/group is private, bot is admin and Userbot is banned but bot has no invite user power
-    if not message.chat.username and chat_member.status == ChatMemberStatus.ADMINISTRATOR:
-        userbot_member = await app.get_chat_member(chat_id, userbot.id)
-        if userbot_member.status in ["banned", "restricted"]:
-            # Check if bot has invite user power
-            if not chat_member.can_invite_users:
-                await message.reply_text("I don't have invite user power, please provide me")
-                return
-            try:
-                await app.unban_chat_member(chat_id, userbot.id)
-                await message.reply("Assistant is unbanned")
-                invite_link = await app.create_chat_invite_link(chat_id)
-                await userbot.join_chat(invite_link.invite_link)
-                await message.reply("Assistant was banned, now unbanned, and joined!")
-            except Exception as e:
-                await message.reply(str(e))
-        return
-    
-    
-    
-    # Condition 4: Group username is present, bot is admin and Userbot is banned but bot has no ban power
-    if message.chat.username and chat_member.status == ChatMemberStatus.ADMINISTRATOR:
-        userbot_member = await app.get_chat_member(chat_id, userbot.id)
-        if userbot_member.status in ["banned", "restricted"]:
-            # Check if bot has ban power
-            if not chat_member.can_restrict_members:
-                await message.reply_text("I have no ban power to unban assistant, please provide me")
-                return
-            try:
-                await app.unban_chat_member(chat_id, userbot.id)
-                await message.reply("Assistant is unbanned")
-                invite_link = await app.create_chat_invite_link(chat_id)
-                await userbot.join_chat(invite_link.invite_link)
-                await message.reply("Assistant was banned, now unbanned, and joined!")
-            except Exception as e:
-                await message.reply(str(e))
-        return
-            
+    # 
 
 
 
