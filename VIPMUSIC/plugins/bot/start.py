9abbce2d955e1659f5b1dbbf4dc47d20d5c98fe1
DED-LEGEND <159163158+DED-LEGEND@users.noreply.github.com>
2024-02-28 07:25:06 +0530

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -36,7 +36,7 @@ YUMI_PICS = [
 
 
 
-@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["dxlucky"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
