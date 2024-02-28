70825d2a23caa4b42cda3a70144e84ba6373b560
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-28 13:57:10 +0530

    Update userbotjoin.py

--- VIPMUSIC/plugins/tools/userbotjoin.py
+++ VIPMUSIC/plugins/tools/userbotjoin.py
@@ -7,12 +7,14 @@ from VIPMUSIC import app
 import asyncio
 import random
 from pyrogram import Client, filters
-from pyrogram.enums import ChatType, ChatMemberStatus
-from pyrogram.errors import UserNotParticipant
-from pyrogram.types import ChatPermissions
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import (
+    ChatAdminRequired,
+    InviteRequestSent,
+    UserAlreadyParticipant,
+    UserNotParticipant,
+)
 from VIPMUSIC import app
-from VIPMUSIC.utils.vip_ban import admin_filter
-
 from VIPMUSIC.utils.vip_ban import admin_filter
 from VIPMUSIC.utils.decorators.userbotjoin import UserbotWrapper
 from VIPMUSIC.utils.database import get_assistant
@@ -22,49 +24,31 @@ links = {}
 @app.on_message(filters.group & filters.command(["userbotjoin", f"userbotjoin@{app.username}"]) & ~filters.private)
 async def join_group(client, message):
     userbot = await get_assistant(message.chat.id)
+    
     if message.chat.username:
-        # Userbot joins the username
-        await userbot.join_chat(message.chat.username)
-        await message.reply("Successfully joined!")
-        return
-
-    if app.id.status in (
-            ChatMemberStatus.ADMINISTRATOR,
-            ChatMemberStatus.OWNER
-        ):
-            is_admin = True
-    if not is_admin:
-        return await message.reply("Please make me admin for invite my assistant here. ")
-
-    elif userbot.id in message.chat.id:
-        # Userbot already joined, no action required
-        await message.reply("Bot's assistant already joined this group.")
-
+        try:
+            await userbot.join_chat(message.chat.username)
+            await message.reply("Successfully joined!")
+        except UserNotParticipant:
+            member = await app.get_chat_member(message.chat.id, userbot.id)
+            if (
+                    member.status == ChatMemberStatus.BANNED
+                    or member.status == ChatMemberStatus.RESTRICTED
+                ):
+                await app.unban_chat_member(message.chat.id, userbot.id)
+                invite_link = await app.export_chat_invite_link(message.chat.id)
+                await userbot.join_chat(invite_link)
+                await message.reply("Assistant was banned, now unbanned, and joined!")
+                
+            else:
+                await message.reply("Assistant is banned, unban it firstly.")
     else:
-        # Generate invite link and store it
-        invite_link = await app.export_chat_invite_link(message.chat.id)
-        await userbot.join_chat(invite_link)
-        await message.reply("Bot's assistant joined successfully!")
-
-
-@app.on_message(filters.group)
-async def check_member_status(client, message):
-    userbot = await get_assistant(message.chat.id)
-    if message.from_user.id == "userbot.id":
-        if message.new_chat_members:
-            for member in message.new_chat_members:
-                if member.id == userbot.get_me().id:
-                    await message.reply("Assistant already joined this group.")
-                    return
-    else:
-        # Check if the assistant is banned or restricted
-        member = await userbot.get_chat_member(message.chat.id, "userbot.id")
-        if member.status in ["kicked", "restricted"]:
-            await userbot.unban_chat_member(message.chat.id, "userbot.id")
-            await message.reply("Assistant was banned, now unbanned.")
-        else:
-            await message.reply("Assistant is banned, unban it.")
-
+        try:
+            invite_link = await app.export_chat_invite_link(message.chat.id)
+            await userbot.join_chat(invite_link)
+            await message.reply("Bot's assistant joined successfully!")
+        except ChatAdminRequired:
+            await message.reply("I am not admin.")
 
         
 @app.on_message(filters.command("userbotleave") & filters.group & admin_filter)
