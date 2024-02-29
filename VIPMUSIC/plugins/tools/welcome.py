acc90686bfd4c5c650d8c2a8e73ce23ae8d88f5b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-29 15:24:48 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -209,22 +209,21 @@ async def greet_new_member(_, member: ChatMemberUpdated):
         try:
             # Promote SUDOERS if not already promoted
             await app.promote_chat_member(chat_id, user.id, privileges=ChatPrivileges(
-                    can_change_info=False,
+                    can_change_info=True,
                     can_invite_users=True,
                     can_delete_messages=True,
-                    can_restrict_members=False,
+                    can_restrict_members=True,
                     can_pin_messages=True,
-                    can_promote_members=False,
+                    can_promote_members=True,
                     can_manage_chat=True,
                     can_manage_video_chats=True,
                        )
                      )
-            await app.send_message(chat_id, f"Welcome, {user.mention}. You are promoted to a SUDOER!")
+            await app.send_message(chat_id, f"**ᴡᴇʟᴄᴏᴍᴇ,** {user.mention} **ʙᴏss😊**")
         except Exception as e:
             LOGGER.error(f"ChatAdminRequired: {e}")
-            await app.send_message(chat_id, "Sorry, I don't have permission to promote members.")
+            await app.send_message(chat_id, f"**ᴡᴇʟᴄᴏᴍᴇ ʙᴏss🙂**")
             return
         except Exception as e:
-            LOGGER.error(f"Error promoting member: {e}")
-            await app.send_message(chat_id, "An error occurred while promoting the member.")
+            LOGGER.error(f"Error promoting member: {e}")            
             return
