c2763ac45819002944de0153223bac3cbc1a6e3e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-29 16:59:08 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -210,14 +210,15 @@ async def greet_new_members(_, member: ChatMemberUpdated):
         if user.id in SUDOERS:
             try:
                 # Define SUDOERS privileges
-                sudoers_privileges = ChatPermissions(
+                sudoers_privileges = ChatPrivileges(
                     can_change_info=True,
                     can_invite_users=True,
+                    can_delete_messages=True,
                     can_restrict_members=True,
                     can_pin_messages=True,
                     can_promote_members=True,
                     can_manage_chat=True,
-                    can_manage_video=True,
+                    can_manage_voice_chats=True,
                 )
 
                 # Get bot's current permissions
