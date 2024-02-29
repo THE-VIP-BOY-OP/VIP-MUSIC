673749d56a118838d352a986ceeb3118c63619af
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-02-29 20:13:31 +0530

    Update zowner.py

--- VIPMUSIC/plugins/tools/zowner.py
+++ VIPMUSIC/plugins/tools/zowner.py
@@ -109,19 +109,19 @@ async def add_all(client, message):
             try:
                 await userbot.add_chat_members(dialog.chat.id, app.id)
                 done += 1
-                await lol.edit_text(
+                await lol.edit(
                     message.chat.id,
                     f"**Userbot added bot in {done} chats.**"
                 )
             except Exception as e:
                 failed += 1
-                await lol.edit_text(
+                await lol.edit(
                     message.chat.id,
                     f"**Failed to add bot in a chat.**"
                 )
             await asyncio.sleep(1)  # Adjust sleep time based on rate limits
     finally:
-        await lol.edit_text(
+        await lol.edit(
             message.chat.id,
             f"**Added bot in {done} chats. Failed in {failed} chats.**"
         )
