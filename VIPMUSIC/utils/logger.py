5214218b183ed9b50014dac05cbc96adae166275
DED-LEGEND <159163158+DED-LEGEND@users.noreply.github.com>
2024-03-01 06:45:28 +0530

    Update logger.py

--- VIPMUSIC/utils/logger.py
+++ VIPMUSIC/utils/logger.py
@@ -8,18 +8,18 @@ from config import LOGGER_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+<b>{app.mention} 𝗗𝗫 𝗟𝗨𝗖𝗞𝗬 𝗣𝗟𝗔𝗬 𝗟𝗢𝗚</b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>𝗖𝗛𝗔𝗧 𝗜𝗗 :</b> <code>{message.chat.id}</code>
+<b>𝗖𝗛𝗔𝗧 𝗡𝗔𝗠𝗘 :</b> {message.chat.title}
+<b>𝗖𝗛𝗔𝗧 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘 :</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>𝗨𝗦𝗘𝗥 𝗜𝗗 :</b> <code>{message.from_user.id}</code>
+<b>𝗡𝗔𝗠𝗘 :</b> {message.from_user.mention}
+<b>𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘 :</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>𝗤𝗨𝗘𝗥𝗬 :</b> {message.text.split(None, 1)[1]}
+<b>𝗦𝗧𝗥𝗘𝗔𝗠𝗧𝗬𝗣𝗘 :</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
