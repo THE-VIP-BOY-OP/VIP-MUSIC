0d04cd63bc645df4a8efcf35052705872abf3f7c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-01 16:43:05 +0530

    Update block.py

--- VIPMUSIC/plugins/sudo/block.py
+++ VIPMUSIC/plugins/sudo/block.py
@@ -9,7 +9,16 @@ from VIPMUSIC.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["block"]) & SUDOERS)
+from time import time
+
+# Define the list of specific commands to track for spamming
+SPAM_COMMANDS = ["/play", "/start", "/stop"]  # Example commands
+
+# Define a dictionary to track the last message timestamp for each user
+user_last_message_time = {}
+
+# Modify the existing code
+@app.on_message(filters.command(SPAM_COMMANDS) & SUDOERS)
 @language
 async def useradd(client, message: Message, _):
     if not message.reply_to_message:
@@ -22,6 +31,25 @@ async def useradd(client, message: Message, _):
     BANNED_USERS.add(user.id)
     await message.reply_text(_["block_2"].format(user.mention))
 
+    # Additional code to track spamming
+    user_id = message.from_user.id
+    current_time = time()
+
+    # Update the last message timestamp for the user
+    last_message_time = user_last_message_time.get(user_id, 0)
+    if current_time - last_message_time < 60:
+        # If less than 60 seconds have passed since the last message
+        user_last_message_time[user_id] = current_time
+        user_command_count[user_id] = user_command_count.get(user_id, 0) + 1
+        if user_command_count[user_id] > 5:
+            # Block the user if they exceed the threshold
+            await add_gban_user(user_id)
+            BANNED_USERS.add(user_id)
+            await client.send_message(user_id, "You have been blocked for spamming.")
+    else:
+        # If more than 60 seconds have passed, reset the command count and update the message timestamp
+        user_command_count[user_id] = 1
+        user_last_message_time[user_id] = current_time
 
 @app.on_message(filters.command(["unblock"]) & SUDOERS)
 @language
