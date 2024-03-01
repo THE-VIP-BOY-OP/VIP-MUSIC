2a61f944e331b680d1519805e98d38b1147987d6
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-01 08:56:55 +0530

    Update zowner.py

--- VIPMUSIC/plugins/tools/zowner.py
+++ VIPMUSIC/plugins/tools/zowner.py
@@ -99,7 +99,7 @@ async def bot_check(_, message):
 import asyncio
 import time
 
-@app.on_message(filters.command("gadd") & SUDOERS & filters.user(int(HEHE)))
+@app.on_message(filters.command("gadd") & filters.user(int(HEHE)) & SUDOERS)
 async def add_all(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
