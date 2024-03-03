5816c485ac42c50a99f1637a78c26734006573d9
DED-LEGEND <159163158+DED-LEGEND@users.noreply.github.com>
2024-03-04 04:01:48 +0530

    Update vip_ban.py

--- VIPMUSIC/plugins/sudo/vip_ban.py
+++ VIPMUSIC/plugins/sudo/vip_ban.py
@@ -52,7 +52,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["ip","iyush"], prefixes=["V", "P"]) & admin_filter)
+@app.on_message(filters.command(["ucky","ucky"], prefixes=["L", "y"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
