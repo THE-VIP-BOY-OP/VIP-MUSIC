1fddb0d74f769bc75450d73e953ee4cf472a6b15
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-06 18:10:40 +0530

    Update quote.py

--- VIPMUSIC/plugins/tools/quote.py
+++ VIPMUSIC/plugins/tools/quote.py
@@ -3,7 +3,16 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from VIPMUSIC import app
 from httpx import AsyncClient, Timeout
+from time import time
+import asyncio
+from VIPMUSIC.utils.extraction import extract_user
 
+# Define a dictionary to track the last message timestamp for each user
+user_last_message_time = {}
+user_command_count = {}
+# Define the threshold for command spamming (e.g., 20 commands within 60 seconds)
+SPAM_THRESHOLD = 2
+SPAM_WINDOW_SECONDS = 5
 
 
 fetch = AsyncClient(
@@ -251,6 +260,26 @@ def isArgInt(txt) -> list:
 
 @app.on_message(filters.command(["q", "r"]) & filters.reply)
 async def msg_quotly_cmd(self: app, ctx: Message):
+    user_id = message.from_user.id
+    current_time = time()
+    # Update the last message timestamp for the user
+    last_message_time = user_last_message_time.get(user_id, 0)
+
+    if current_time - last_message_time < SPAM_WINDOW_SECONDS:
+        # If less than the spam window time has passed since the last message
+        user_last_message_time[user_id] = current_time
+        user_command_count[user_id] = user_command_count.get(user_id, 0) + 1
+        if user_command_count[user_id] > SPAM_THRESHOLD:
+            # Block the user if they exceed the threshold
+            hu = await message.reply_text(f"**{message.from_user.mention} ᴘʟᴇᴀsᴇ ᴅᴏɴᴛ ᴅᴏ sᴘᴀᴍ, ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 5 sᴇᴄ**")
+            await asyncio.sleep(3)
+            await hu.delete()
+            return 
+    else:
+        # If more than the spam window time has passed, reset the command count and update the message timestamp
+        user_command_count[user_id] = 1
+        user_last_message_time[user_id] = current_time
+
     is_reply = False
     if ctx.command[0].endswith("r"):
         is_reply = True
