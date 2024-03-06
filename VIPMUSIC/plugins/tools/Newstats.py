b16ffc9f1454eca93eb0fbeed12459d8ca9a9047
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2024-03-06 14:38:10 +0530

    Update Newstats.py

--- VIPMUSIC/plugins/tools/Newstats.py
+++ VIPMUSIC/plugins/tools/Newstats.py
@@ -15,9 +15,9 @@ from pyrogram.errors import (
     UserNotParticipant,
 )
 
-async def get_dialog_count(dialog_type):
+async def get_dialog_count(dialog_type, userbot):
     count = 0
-    async for dialog in app.get_dialogs():
+    async for dialog in userbot.get_dialogs():
         if dialog_type == "GROUP" and dialog.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
             count += 1
         elif dialog_type == "USERS" and dialog.chat.type == enums.ChatType.PRIVATE:
@@ -27,15 +27,18 @@ async def get_dialog_count(dialog_type):
     return count
 
 
-async def get_bot_statss():
-    group_count = await get_dialog_count("GROUP")
-    user_count = await get_dialog_count("USERS")
-    channel_count = await get_dialog_count("CHANNELS")
+async def get_bot_statss(userbot):
+    group_count = await get_dialog_count("GROUP", userbot)
+    user_count = await get_dialog_count("USERS", userbot)
+    channel_count = await get_dialog_count("CHANNELS", userbot)
     return {"Groups": group_count, "Users": user_count, "Channels": channel_count}
 
 @app.on_message(filters.command("botstats"))
-async def bot_statss(client, message):      
-  stats = await get_bot_statss()        
+async def bot_statss(client, message):   
+  chat_id = message.chat.id
+  userbot = await get_assistant(message.chat.id)
+  userbot_id = userbot.id
+  stats = await get_bot_statss(userbot)        
   stats_text = "\n".join([f"{key}: {value}" for key, value in stats.items()])   
   await message.reply(f"Bot Stats:\n{stats_text}")
 
