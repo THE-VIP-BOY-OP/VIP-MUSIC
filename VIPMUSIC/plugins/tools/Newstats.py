681cfcecbf4cb20e66af57da932e0974b19dcf7b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-06 11:29:12 +0530

    Update Newstats.py

--- VIPMUSIC/plugins/tools/Newstats.py
+++ VIPMUSIC/plugins/tools/Newstats.py
@@ -2,6 +2,18 @@ from VIPMUSIC import app
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
+from VIPMUSIC.misc import SUDOERS
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant
+import random
+from pyrogram import Client, filters
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import (
+    ChatAdminRequired,
+    InviteRequestSent,
+    UserAlreadyParticipant,
+    UserNotParticipant,
+)
 
 async def get_dialog_count(dialog_type):
     count = 0
@@ -27,3 +39,30 @@ async def bot_statss(client, message):
   stats_text = "\n".join([f"{key}: {value}" for key, value in stats.items()])   
   await message.reply(f"Bot Stats:\n{stats_text}")
 
+@app.on_message(filters.command("statbot"))
+async def stats_count(client, message):
+
+    count= 0
+    failed = 0
+    lol = await message.reply("🔄 **bot stats counting**")
+    try:
+        
+        async for dialog in app.get_dialogs():
+            if dialog.chat.id == -1001733534088:
+                continue
+            try:
+                await app.get_dialog_count(dialog.chat.id)
+                count += 1
+                await lol.edit(
+                    f"**ᴜsᴇʀʙᴏᴛ ʟᴇᴀᴠɪɴɢ ᴀʟʟ ɢʀᴏᴜᴘ...**\n\n**ʟᴇғᴛ:** {count} ᴄʜᴀᴛs.\n**ғᴀɪʟᴇᴅ:** {failed} ᴄʜᴀᴛs."
+                )
+            except BaseException:
+                failed += 1
+                await lol.edit(
+                    f"**ᴜsᴇʀʙᴏᴛ ʟᴇᴀᴠɪɴɢ...**\n\n**ʟᴇғᴛ:** {count} chats.\n**ғᴀɪʟᴇᴅ:** {failed} chats."
+                )
+            await asyncio.sleep(0.2)
+    finally:
+        await app.send_message(
+            message.chat.id, f"**✅ ʟᴇғᴛ ғʀᴏᴍ:* {count} chats.\n**❌ ғᴀɪʟᴇᴅ ɪɴ:** {failed} chats."
+        )
