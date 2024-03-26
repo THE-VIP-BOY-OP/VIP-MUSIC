666ce4c0d6d2b85633e84ab972ae5ef45cbe507e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-26 09:08:26 +0530

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -166,7 +166,7 @@ async def start_gp(client, message: Message, _):
         user_last_message_time[user_id] = current_time
         
     out = start_panel(_)
-    invitelink = await app.export_chat_invite_link(message.chat_id)
+    invitelink = await app.export_chat_invite_link(message.chat.id)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
         photo=config.START_IMG_URL,
