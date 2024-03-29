505c18226e35da527f02d22344bf3d8366555ddb
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-29 23:53:37 +0530

    Update play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -264,12 +264,7 @@ def queue_markup(_, videoid, chat_id):
                 url=f"https://t.me/{app.username}?startgroup=true",
             ),
         ],
-        [
-            InlineKeyboardButton(
-                text= "✚ ᴀᴅᴅ ɪɴ ʏᴏᴜʀ ᴘʟᴀʏʟɪsᴛ ✚",
-                callback_data=f"vip_playlist {videoid}"
-            ),
-        ],
+        
         
         [
             InlineKeyboardButton(
