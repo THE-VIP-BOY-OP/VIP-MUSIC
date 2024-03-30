4b28fa5f7b37bb69d9d7c26b3aa677cbe6b6de03
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-30 11:52:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vivekkumar0708/VIP-MUSIC",
+    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
