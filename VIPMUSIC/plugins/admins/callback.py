8c7c7573f172c14fd7b933534f9c6188fcaf27f0
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-30 13:25:08 +0530

    Update callback.py

--- VIPMUSIC/plugins/admins/callback.py
+++ VIPMUSIC/plugins/admins/callback.py
@@ -79,7 +79,7 @@ async def del_back_playlist(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     videoid, chat_id = callback_request.split("|")
-    buttons = panel_markup_4(_, videoid, chat_id)
+    buttons = panel_markup_4(_, chat_id, played, dur)
     chat_id = CallbackQuery.message.chat.id
     try:
         await CallbackQuery.edit_message_reply_markup(
