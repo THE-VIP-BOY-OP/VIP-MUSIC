5965b3b186ea7cd9c4fa5f8c7c2b815f5bc20c5e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-30 17:41:04 +0530

    Update download_video.py

--- VIPMUSIC/plugins/tools/download_video.py
+++ VIPMUSIC/plugins/tools/download_video.py
@@ -4,15 +4,13 @@ import asyncio
 import os
 import time
 from urllib.parse import urlparse
-from pyrogram.types import (InlineKeyboardButton, CallbackQuery,
-                            InlineKeyboardMarkup, Message)
+
 import wget
 from pyrogram import filters
 from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
-from pytube import YouTube
-from youtubesearchpython import VideosSearch
+
 from VIPMUSIC import app
 import asyncio
 import os
@@ -29,6 +27,7 @@ import asyncio
 from VIPMUSIC.utils.extraction import extract_user
 
 
+
 import asyncio
 import os
 import wget
@@ -40,103 +39,77 @@ from yt_dlp import YoutubeDL
 from VIPMUSIC import app
 from VIPMUSIC.utils.extraction import extract_user
 
-
-
-
 BANNED_USERS = []
 
-
-@app.on_callback_query(filters.regex("downloadvideo") & ~filters.user(BANNED_USERS))
-async def downloadvideo(client, callback_query):
-    callback_data = callback_query.data.strip()
-    video_id = callback_data.split(None, 1)[1]
-    user_id = callback_query.from_user.id
-    user_name = callback_query.from_user.first_name
-    user_link = f"[{user_name}](tg://user?id={user_id})"
-
-    message = await client.send_message(callback_query.message.chat.id, f"Searching, please wait...")
-    
-    try:
-        yt = YouTube(f"https://youtu.be/{video_id}")
-        yt_stream = yt.streams.filter(progressive=True, file_extension="mp4").first()
-        if yt_stream:
-            yt_stream.download()
-            file_stark = f"{yt.title}.mp4"
-            capy = f"❄ **Title:** [{yt.title}]({yt.watch_url})\n💫 **Channel:** {yt.author}\n🥀 **Requested by:** {user_link}"
-            await client.send_video(
-                callback_query.message.chat.id,
-                video=open(file_stark, "rb"),
-                duration=int(yt.length),
-                file_name=str(yt.title),
-                thumb=yt.thumbnail_url,
-                caption=capy,
-                supports_streaming=True,
-                progress_args=(
-                    message,
-                    f"Please wait...\n\nUploading `{yt.video_id}` from YouTube servers...💫",
-                    file_stark,
-                ),
-            )
-            await message.delete()
-            os.remove(file_stark)
-        else:
-            await message.edit("No stream available for the provided video.")
-    except Exception as e:
-        await message.edit(f"Error: {str(e)}")
-
-
-
-@app.on_callback_query(filters.regex("downloadaudio") & ~filters.user(BANNED_USERS))
-async def downloadaudio(client, CallbackQuery):
+@app.on_callback_query(filters.regex("download_video") & ~filters.user(BANNED_USERS))
+async def download_video(client, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
-    videoid = callback_data.split()[1]  # Extract video ID from callback data
-    m = await client.send_message(CallbackQuery.message.chat.id, f"**🔄 Searching {videoid}... **")
-    ydl_ops = {"format": "bestaudio[ext=m4a]"}
-    try:
-        results = YouTube(f"https://youtube.com/{videoid}")
-        link = f"https://youtube.com{videoid}"
-        title = results.title
-        thumbnail = results.thumbnails
-        thumb_name = f"{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        duration = results[0]["duration"]
+    videoid = callback_data.split(None, 1)[1]
+    user_id = CallbackQuery.from_user.id
+    await CallbackQuery.message.delete()
+    user_id = CallbackQuery.from_user.id
+    user_name = CallbackQuery.from_user.first_name
+    chutiya = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
 
-        # Add these lines to define views and channel_name
-        views = results.views
-        channel_name = results.channel
-
-    except Exception as e:
-        await m.edit("**⚠️ No results found. Make sure you typed the correct song name.**")
-        print(str(e))
-        return
-    await m.edit("**📥 Downloading...**")
-    try:
-        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-        secmul, dur, dur_arr = 1, 0, duration.split(":")
-        for i in range(len(dur_arr) - 1, -1, -1):
-            dur += int(float(dur_arr[i])) * secmul
-            secmul *= 60
-        await m.edit("**📤 Uploading...**")
-
-        await client.send_audio(
-            CallbackQuery.message.chat.id,
-            audio=open(audio_file, "rb"),
-            thumb=thumb_name,
-            title=title,
-            caption=f"{title}\nViews➪ {views}\nChannel➪ {channel_name}",
-            duration=dur
+    pablo = await client.send_message(CallbackQuery.message.chat.id, f"Searching, please wait...")
+    if not videoid:
+        await pablo.edit(
+            "Song not found on YouTube.\n\nMaybe you wrote it wrong, learn to write properly!"
         )
-        await m.delete()
-    except Exception as e:
-        await m.edit(" - An error occurred!!")
-        print(e)
+        return
 
+    search = SearchVideos(f"https://youtube.com/{videoid}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi.get("search_result", [])
+    if not mio:
+        await pablo.edit("Song not found on YouTube.")
+        return
+
+    mo = mio[0].get("link", "")
+    thum = mio[0].get("title", "")
+    fridayz = mio[0].get("id", "")
+    thums = mio[0].get("channel", "")
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
     try:
-        os.remove(audio_file)
-        os.remove(thumb_name)
+        with YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(url, download=True)
+
     except Exception as e:
-        print(e)
+        await pablo.edit(f"**Failed to download.** \n**Error:** `{str(e)}`")
+        return
+
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"❄ **Title:** [{thum}]({mo})\n💫 **Channel:** {thums}\n✨ **Searched:** {videoid}\n🥀 **Requested by:** {chutiya}"
+    await client.send_video(
+        CallbackQuery.message.chat.id,
+        video=open(file_stark, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=sedlyf,
+        caption=capy,
+        supports_streaming=True,
+        progress_args=(
+            pablo,
+            f"Please wait...\n\nUploading `{videoid}` from YouTube servers...💫",
+            file_stark,
+        ),
+    )
+    await pablo.delete()
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
