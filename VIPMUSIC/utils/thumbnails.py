f08237c464d7f928a1a9c8a2ab4d9537af8f7bf2
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-03-31 08:17:11 +0530

    Update thumbnails.py

--- VIPMUSIC/utils/thumbnails.py
+++ VIPMUSIC/utils/thumbnails.py
@@ -1,33 +1,28 @@
 import os
-import re
-import random
-
 import aiofiles
 import aiohttp
-
-from PIL import Image, ImageDraw, ImageEnhance
-from PIL import ImageFilter, ImageFont, ImageOps
-
-from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
-
-from VIPMUSIC import app
 from config import YOUTUBE_IMG_URL
-
+from VIPMUSIC import app
 
 async def get_thumb(videoid):
+    if os.path.isfile(f"cache/{videoid}.png"):
+        return f"cache/{videoid}.png"
+
+    url = f"https://www.youtube.com/watch?v={videoid}"
     try:
-        query = f"https://www.youtube.com/watch?v={videoid}"
-        results = VideosSearch(query, limit=1)
+        results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
-            title = result["title"]
-            duration = result["duration"]
-            views = result["viewCount"]["short"]
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-            channellink = result["channel"]["link"]
-            channel = result["channel"]["name"]
-            link = result["link"]
-            published = result["publishedTime"]
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                    await f.write(await resp.read())
+                    await f.close()
+
+        return f"cache/{videoid}.png"
     except Exception as e:
         print(e)
         return YOUTUBE_IMG_URL
