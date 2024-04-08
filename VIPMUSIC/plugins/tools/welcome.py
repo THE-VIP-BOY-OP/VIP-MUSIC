7d5c6552dda84b5a2afb2f97f0990f83c7742386
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-08 20:03:14 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -100,7 +100,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("VIPMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((850, 850))
+    pfp = pfp.resize((860, 860))
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=100)
     welcome_font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=50)
@@ -114,7 +114,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     # Draw user's ID
     draw.text((2030, 1450), f': {uname}', fill=(12000, 12000, 12000), font=font)
     
-    pfp_position = (280, 350)
+    pfp_position = (280, 340)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
