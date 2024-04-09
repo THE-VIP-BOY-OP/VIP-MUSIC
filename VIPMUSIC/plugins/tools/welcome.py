37c950561fbf1c535c7bde36cc051dff14f839d9
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-09 08:38:00 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -97,7 +97,6 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 
-
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("VIPMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
@@ -125,21 +124,24 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     center_y = pfp_position[1] + pfp.height / 2
     radius = min(pfp.width, pfp.height) / 2
 
-    # Draw circular outline in saffron, white, and green colors with thicker lines
-    draw.ellipse([(center_x - radius - 5, center_y - radius - 5),
-                  (center_x + radius + 5, center_y + radius + 5)],
-                 outline=(255, 153, 51), width=10)  # Saffron color
+    # Draw circular outline in saffron color
+    draw.ellipse([(center_x - radius - 10, center_y - radius - 10),
+                  (center_x + radius + 10, center_y + radius + 10)],
+                 outline=(255, 153, 51), width=5)  
 
-    draw.ellipse([(center_x - radius - 5, center_y - radius - 5),
-                  (center_x + radius + 5, center_y + radius + 5)],
-                 outline=(255, 255, 255), width=10)  # White color
+    # Draw circular outline in white color
+    draw.ellipse([(center_x - radius - 20, center_y - radius - 20),
+                  (center_x + radius + 20, center_y + radius + 20)],
+                 outline=(255, 255, 255), width=5)  
 
-    draw.ellipse([(center_x - radius - 5, center_y - radius - 5),
-                  (center_x + radius + 5, center_y + radius + 5)],
-                 outline=(0, 128, 0), width=10)  # Green color
+    # Draw circular outline in green color
+    draw.ellipse([(center_x - radius - 30, center_y - radius - 30),
+                  (center_x + radius + 30, center_y + radius + 30)],
+                 outline=(0, 128, 0), width=5)  
 
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
+
 
 
 
