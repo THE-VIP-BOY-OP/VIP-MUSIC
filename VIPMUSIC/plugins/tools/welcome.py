97b3131c50edc243b0dbde64d80a574ff0aa9002
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-09 19:53:08 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -110,15 +110,15 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     
     # Draw user's name with shining red fill and dark saffron border
     draw.text((1770, 1015), f': {user}', fill=(255, 0, 0), font=font)
-    draw.text((1770, 1015), f': {user}', fill=None, font=font, stroke_fill=(255, 153, 51), stroke_width=2)
+    draw.text((1770, 1015), f': {user}', fill=None, font=font, stroke_fill=(255, 153, 51), stroke_width=8)
     
     # Draw user's username with shining blue fill and white border
     draw.text((1530, 1230), f': {id}', fill=(0, 0, 255), font=font)
-    draw.text((1530, 1230), f': {id}', fill=None, font=font, stroke_fill=(255, 255, 255), stroke_width=2)
+    draw.text((1530, 1230), f': {id}', fill=None, font=font, stroke_fill=(255, 255, 255), stroke_width=8)
     
     # Draw user's ID with white fill and green border
     draw.text((2030, 1450), f': {uname}', fill=(255, 255, 255), font=font)
-    draw.text((2030, 1450), f': {uname}', fill=None, font=font, stroke_fill=(0, 128, 0), stroke_width=2)
+    draw.text((2030, 1450), f': {uname}', fill=None, font=font, stroke_fill=(0, 128, 0), stroke_width=8)
     
     # Resize photo and position
     pfp_position = (255, 323)
