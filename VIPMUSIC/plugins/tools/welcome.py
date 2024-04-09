8209acf32490e21a66538194a7b28f55072ef4f5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-09 07:20:52 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -114,7 +114,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     # Draw user's ID
     draw.text((2030, 1450), f': {uname}', fill=(12000, 12000, 12000), font=font)
     
-    pfp_position = (260, 323)
+    pfp_position = (255, 323)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
