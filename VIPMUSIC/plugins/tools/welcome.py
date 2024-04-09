9951e522c141925ecfe6fd5656d9fe1a27f0a406
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-09 08:30:11 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -97,6 +97,7 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 
+
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("VIPMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
@@ -125,20 +126,21 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     radius = min(pfp.width, pfp.height) / 2
 
     # Draw circular outline in saffron, white, and green colors with thicker lines
-    draw.ellipse([(center_x - radius - 15, center_y - radius - 15),
-                  (center_x + radius + 15, center_y + radius + 15)],
-                 outline=(255, 153, 51), width=20)  # Saffron color
-
-    draw.ellipse([(center_x - radius - 10, center_y - radius - 10),
-                  (center_x + radius + 10, center_y + radius + 10)],
-                 outline=(255, 255, 255), width=20)  # White color
+    draw.ellipse([(center_x - radius - 5, center_y - radius - 5),
+                  (center_x + radius + 5, center_y + radius + 5)],
+                 outline=(255, 153, 51), width=10)  # Saffron color
 
     draw.ellipse([(center_x - radius - 5, center_y - radius - 5),
                   (center_x + radius + 5, center_y + radius + 5)],
-                 outline=(0, 128, 0), width=20)  # Green color
+                 outline=(255, 255, 255), width=10)  # White color
+
+    draw.ellipse([(center_x - radius - 5, center_y - radius - 5),
+                  (center_x + radius + 5, center_y + radius + 5)],
+                 outline=(0, 128, 0), width=10)  # Green color
 
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
+
 
 
 
