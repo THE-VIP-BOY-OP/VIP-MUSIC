29d35a738372108dcbfdd9b972dc850b9681c377
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-21 09:35:13 +0530

    Update get_thumb.py

--- VIPMUSIC/plugins/tools/get_thumb.py
+++ VIPMUSIC/plugins/tools/get_thumb.py
@@ -1,22 +1,18 @@
-from pyrogram import Client, filters
-from youtubesearchpython.__future__ import VideosSearch
-
+from pyrogram import filters
 from VIPMUSIC import app
 
-# Command handler for /getthumbnail
-@app.on_message(filters.command("getthumb", prefixes="/"))
+@app.on_message(filters.command("gethumb", prefixes="/"))
 async def get_thumbnail_command(client, message):
     try:
-        # Extract video ID from the command
-        video_id = message.text.split(maxsplit=1)[1]
+        if len(message.text.split(" ")) < 2:
+            await message.reply("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴠᴀʟɪᴅ ʏᴏᴜᴛᴜʙᴇ ᴜʀʟ.")
+            return
         
-        # Search for the video using video ID
-        query = f"https://www.youtube.com/watch?v={video_id}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail_url = result["thumbnails"][0]["url"].split("?")[0]
-        
-        # Send the thumbnail as a photo
-        await message.reply_photo(thumbnail_url)
+        url = message.text.split(" ")[1]
+        video_id = url.split("v=")[-1] 
+        if "&" in video_id:
+            video_id = video_id.split("&")[0]
+        query = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
+        await message.reply_photo(query)
     except Exception as e:
         await message.reply(f"An error occurred: {e}")
