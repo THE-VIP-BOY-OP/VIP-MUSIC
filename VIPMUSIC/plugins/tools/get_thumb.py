bde97f95624512357784b2740a86f07f4490c8d3
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-21 11:45:12 +0530

    Update get_thumb.py

--- VIPMUSIC/plugins/tools/get_thumb.py
+++ VIPMUSIC/plugins/tools/get_thumb.py
@@ -1,18 +1,41 @@
 from pyrogram import filters
 from VIPMUSIC import app
+import requests
+from bs4 import BeautifulSoup
+import re
 
-@app.on_message(filters.command("gethumb", prefixes="/"))
+def get_video_title(video_id):
+    url = f"https://www.youtube.com/watch?v={video_id}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup.title.string
+
+def extract_video_id(url):
+    regex = r"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})"
+    match = re.match(regex, url)
+    if match:
+        return match.group(1)
+    return None
+
+@app.on_message(filters.command(["getthumb","genthumb","thumb","thumbnail"], prefixes="/"))
 async def get_thumbnail_command(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text("Please provide a YouTube video URL after the command to get thumbnail")
     try:
-        if len(message.text.split(" ")) < 2:
-            await message.reply("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴠᴀʟɪᴅ ʏᴏᴜᴛᴜʙᴇ ᴜʀʟ.")
-            return
-        
+        a = await message.reply_text("Processing...")
         url = message.text.split(" ")[1]
-        video_id = url.split("v=")[-1] 
-        if "&" in video_id:
-            video_id = video_id.split("&")[0]
+        video_id = extract_video_id(url)
+        if not video_id:
+            await message.reply("Please provide a valid YouTube link.")
+            return
+        video_title = get_video_title(video_id)
         query = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
-        await message.reply_photo(query)
+        caption = f"<b>[{video_title}](https://t.me/{app.username}?start=info_{video_id})</b>"
+        await message.reply_photo(query, caption=caption)
+        await a.delete()
+    except requests.exceptions.RequestException:
+        await a.edit("An error occurred while fetching the YouTube video.")
     except Exception as e:
-        await message.reply(f"An error occurred: {e}")
+        await a.edit("An error occurred. Please try again later.")
+        print(f"Error: {e}")
+
