35119565f3045905eaa8b5cd8158a083169c415e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-04-27 15:50:55 +0530

    Create Assistant.py

---
+++ VIPMUSIC/plugins/tools/Assistant.py
@@ -0,0 +1,34 @@
+from pyrogram import Client, filters
+import asyncio
+from pyrogram.types import *
+from pymongo import MongoClient
+import requests
+import random
+from pyrogram.errors import (
+    PeerIdInvalid,
+    ChatWriteForbidden
+)
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+import os
+import re
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+STRING1 = getenv("STRING_SESSION", None)
+
+
+client = Client(STRING1, API_ID, API_HASH)
+
+
+
+
+
+@client.on_message(
+    filters.command("alive", prefixes=["/", ".", "?", "-"])
+    & filters.private & filters.group)
+async def start(client, message):
+    await message.reply_text(f"**ᴀʟᴇxᴀ ᴀɪ ᴜsᴇʀʙᴏᴛ ғᴏʀ ᴄʜᴀᴛᴛɪɴɢ ɪs ᴡᴏʀᴋɪɴɢ**")
+    
