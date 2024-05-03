52584061ca41dab4423900ead8128c4daad4fdc6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-03 15:39:38 +0530

    Update queue.py

--- VIPMUSIC/cplugin/utils/queue.py
+++ VIPMUSIC/cplugin/utils/queue.py
@@ -1,13 +1,4 @@
-#
-# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-from YukkiMusic.misc import clonedb
+from VIPMUSIC.misc import clonedb
 
 
 async def put(
