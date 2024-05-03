c1cff0a044c22b93785f6d8ef3448bc42e111e27
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-03 12:01:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "testing")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
