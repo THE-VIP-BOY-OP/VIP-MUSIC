1c2d8290125186a3636bc9b68dcc5cc88d3812ba
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-04 21:37:18 +0530

    Update play.py

--- VIPMUSIC/cplugin/play.py
+++ VIPMUSIC/cplugin/play.py
@@ -20,7 +20,6 @@ from VIPMUSIC.cplugin.utils import add_active_chat, is_active_chat, stream_on
 from VIPMUSIC.utils.downloaders import audio_dl
 from VIPMUSIC.utils.thumbnails import get_qthumb as gen_qthumb
 from VIPMUSIC.utils.thumbnails import get_thumb as gen_thumb
-from VIPMUSIC.utils.database import add_served_chat, add_served_user
 from typing import Union
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Audio, Message, Voice
@@ -29,7 +28,81 @@ from VIPMUSIC import userbot
 from VIPMUSIC.core.call import VIP
 from .utils.inline import close_key
 from .utils.active import _clear_
+import random
+from typing import Dict, List, Union
 
+from VIPMUSIC import userbot
+from VIPMUSIC.core.mongo import mongodb, pymongodb
+
+authdb = mongodb.adminauth
+authuserdb = mongodb.authuser
+autoenddb = mongodb.autoend
+assdb = mongodb.assistants
+blacklist_chatdb = mongodb.blacklistChat
+blockeddb = mongodb.blockedusers
+chatsdb = mongodb.chats
+channeldb = mongodb.cplaymode
+clonebotdb = pymongodb.clonebotdb
+countdb = mongodb.upcount
+gbansdb = mongodb.gban
+langdb = mongodb.language
+onoffdb = mongodb.onoffper
+playmodedb = mongodb.playmode
+playtypedb = mongodb.playtypedb
+skipdb = mongodb.skipmode
+sudoersdb = mongodb.sudoers
+usersdb = mongodb.tgusersdb
+privatedb = mongodb.privatechats
+suggdb = mongodb.suggestion
+cleandb = mongodb.cleanmode
+queriesdb = mongodb.queries
+userdb = mongodb.userstats
+videodb = mongodb.vipvideocalls
+
+async def is_served_user(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def get_served_users() -> list:
+    users_list = []
+    async for user in usersdb.find({"user_id": {"$gt": 0}}):
+        users_list.append(user)
+    return users_list
+
+
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await usersdb.insert_one({"user_id": user_id})
+
+
+async def get_served_chats() -> list:
+    chats_list = []
+    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
+    
+async def delete_served_chat(chat_id: int):
+    await chatsdb.delete_one({"chat_id": chat_id})
+    
 # SESSION = os.getenv("SESSION", "")
 
 """app2 = Client(
