6df1631ade7cc1ef4ca7bbac1e464dc71b9f2e09
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-05 14:40:59 +0530

    Update broadcast.py

--- VIPMUSIC/cplugin/broadcast.py
+++ VIPMUSIC/cplugin/broadcast.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait
 
 from VIPMUSIC import app
 from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import get_client
+from VIPMUSIC.utils.database import get_client, get_served_chats_clone, get_served_users_clone
 from VIPMUSIC.utils.decorators.language import language
 from VIPMUSIC.utils.formatters import alpha_to_int
 from config import adminlist
@@ -83,49 +83,6 @@ async def remove_active_chat_clone(chat_id: int):
     if chat_id in active:
         active.remove(chat_id)
 
-async def is_served_user_clone(user_id: int) -> bool:
-    user = await usersdbc.find_one({"user_id": user_id})
-    if not user:
-        return False
-    return True
-
-
-async def get_served_users_clone() -> list:
-    users_list = []
-    async for user in usersdbc.find({"user_id": {"$gt": 0}}):
-        users_list.append(user)
-    return users_list
-
-
-async def add_served_user_clone(user_id: int):
-    is_served = await is_served_user(user_id)
-    if is_served:
-        return
-    return await usersdbc.insert_one({"user_id": user_id})
-
-
-async def get_served_chats_clone() -> list:
-    chats_list = []
-    async for chat in chatsdbc.find({"chat_id": {"$lt": 0}}):
-        chats_list.append(chat)
-    return chats_list
-
-
-async def is_served_chat_clone(chat_id: int) -> bool:
-    chat = await chatsdbc.find_one({"chat_id": chat_id})
-    if not chat:
-        return False
-    return True
-
-
-async def add_served_chat_clone(chat_id: int):
-    is_served = await is_served_chat(chat_id)
-    if is_served:
-        return
-    return await chatsdbc.insert_one({"chat_id": chat_id})
-    
-async def delete_served_chat_clone(chat_id: int):
-    await chatsdbc.delete_one({"chat_id": chat_id})
 
 async def _get_authusers(chat_id: int) -> Dict[str, int]:
     _notes = await authuserdb.find_one({"chat_id": chat_id})
