21364f6dfb9a61649c7c00bf2f7dc8d66a82a604
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-05 11:55:44 +0530

    Update pause.py

--- VIPMUSIC/cplugin/pause.py
+++ VIPMUSIC/cplugin/pause.py
@@ -77,7 +77,7 @@ async def music_off(chat_id: int):
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
-        await message.reply_text(_["admin_1"])
+        return await message.reply_text(_["admin_1"])
         
     await music_off(chat_id)
     await VIP.pause_stream(chat_id)
