a576b0edc109d972fa2ea80fe1094f0c4d3996f1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-06 09:04:53 +0530

    Update playlist.py

--- VIPMUSIC/cplugin/playlist.py
+++ VIPMUSIC/cplugin/playlist.py
@@ -2,8 +2,8 @@ import os
 import requests
 from random import randint
 from VIPMUSIC.utils.database import (
-    add_served_chat,
-    add_served_user,
+    add_served_chat_clone,
+    add_served_user_clone,
     blacklisted_chats,
     get_lang,
     is_banned_user,
@@ -44,50 +44,50 @@ SPAM_WINDOW_SECONDS = 5
 from VIPMUSIC.core.mongo import mongodb
 
 
-playlistdb = mongodb.playlist
+playlistdbc = mongodb.playlistclone
 playlist = []
 # Playlist Databse
 
 
-async def _get_playlists(chat_id: int) -> Dict[str, int]:
-    _notes = await playlistdb.find_one({"chat_id": chat_id})
+async def _get_playlists_clone(chat_id: int) -> Dict[str, int]:
+    _notes = await playlistdbc.find_one({"chat_id": chat_id})
     if not _notes:
         return {}
     return _notes["notes"]
 
 
-async def get_playlist_names(chat_id: int) -> List[str]:
+async def get_playlist_names_clone(chat_id: int) -> List[str]:
     _notes = []
-    for note in await _get_playlists(chat_id):
+    for note in await _get_playlists_clone(chat_id):
         _notes.append(note)
     return _notes
 
 
-async def get_playlist(chat_id: int, name: str) -> Union[bool, dict]:
+async def get_playlist_clone(chat_id: int, name: str) -> Union[bool, dict]:
     name = name
-    _notes = await _get_playlists(chat_id)
+    _notes = await _get_playlists_clone(chat_id)
     if name in _notes:
         return _notes[name]
     else:
         return False
 
 
-async def save_playlist(chat_id: int, name: str, note: dict):
+async def save_playlist_clone(chat_id: int, name: str, note: dict):
     name = name
-    _notes = await _get_playlists(chat_id)
+    _notes = await _get_playlists_clone(chat_id)
     _notes[name] = note
-    await playlistdb.update_one(
+    await playlistdbc.update_one(
         {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
     )
 
 
 
-async def delete_playlist(chat_id: int, name: str) -> bool:
-    notesd = await _get_playlists(chat_id)
+async def delete_playlist_clone(chat_id: int, name: str) -> bool:
+    notesd = await _get_playlists_clone(chat_id)
     name = name
     if name in notesd:
         del notesd[name]
-        await playlistdb.update_one(
+        await playlistdbc.update_one(
             {"chat_id": chat_id},
             {"$set": {"notes": notesd}},
             upsert=True,
@@ -109,7 +109,7 @@ DELETEPLAYLIST_COMMAND = ("delplaylist")
     & ~BANNED_USERS
 )
 @language
-async def check_playlist(client, message: Message, _):
+async def check_playlist_clone(client, message: Message, _):
     user_id = message.from_user.id
     current_time = time()
     # Update the last message timestamp for the user
@@ -130,7 +130,7 @@ async def check_playlist(client, message: Message, _):
         user_command_count[user_id] = 1
         user_last_message_time[user_id] = current_time
 
-    _playlist = await get_playlist_names(message.from_user.id)
+    _playlist = await get_playlist_names_clone(message.from_user.id)
     if _playlist:
         get = await message.reply_text(_["playlist_2"])
     else:
@@ -138,7 +138,7 @@ async def check_playlist(client, message: Message, _):
     msg = _["playlist_4"]
     count = 0
     for shikhar in _playlist:
-        _note = await get_playlist(message.from_user.id, shikhar)
+        _note = await get_playlist_clone(message.from_user.id, shikhar)
         title = _note["title"]
         title = title.title()
         duration = _note["duration"]
@@ -162,10 +162,10 @@ async def check_playlist(client, message: Message, _):
 
 async def get_keyboard(_, user_id):
     keyboard = InlineKeyboard(row_width=5)
-    _playlist = await get_playlist_names(user_id)
+    _playlist = await get_playlist_names_clone(user_id)
     count = len(_playlist)
     for x in _playlist:
-        _note = await get_playlist(user_id, x)
+        _note = await get_playlist_clone(user_id, x)
         title = _note["title"]
         title = title.title()
         keyboard.row(
@@ -191,7 +191,7 @@ async def get_keyboard(_, user_id):
     & ~BANNED_USERS
 )
 @language
-async def del_plist_msg(client, message: Message, _):
+async def del_plist_msg_clone(client, message: Message, _):
     user_id = message.from_user.id
     current_time = time()
     # Update the last message timestamp for the user
@@ -212,7 +212,7 @@ async def del_plist_msg(client, message: Message, _):
         user_command_count[user_id] = 1
         user_last_message_time[user_id] = current_time
 
-    _playlist = await get_playlist_names(message.from_user.id)
+    _playlist = await get_playlist_names_clone(message.from_user.id)
     if _playlist:
         get = await message.reply_text(_["playlist_2"])
     else:
@@ -229,7 +229,7 @@ async def play_playlist(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     mode = callback_data.split(None, 1)[1]
     user_id = CallbackQuery.from_user.id
-    _playlist = await get_playlist_names(user_id)
+    _playlist = await get_playlist_names_clone(user_id)
     if not _playlist:
         try:
             return await CallbackQuery.answer(
@@ -276,7 +276,7 @@ async def play_playlist(client, CallbackQuery, _):
     filters.command(["playplaylist", "vplayplaylist"]) & ~BANNED_USERS & filters.group
 )
 @languageCB
-async def play_playlist_command(client, message, _):
+async def play_playlist_command_clone(client, message, _):
     user_id = message.from_user.id
     current_time = time()
     # Update the last message timestamp for the user
@@ -298,7 +298,7 @@ async def play_playlist_command(client, message, _):
         user_last_message_time[user_id] = current_time
         mode = message.command[0][0]
         user_id = message.from_user.id
-    _playlist = await get_playlist_names(user_id)
+    _playlist = await get_playlist_names_clone(user_id)
     if not _playlist:
         try:
             return await message.reply(
@@ -351,7 +351,7 @@ import json
     & ~BANNED_USERS
 )
 @language
-async def add_playlist(client, message: Message, _):
+async def add_playlist_clone(client, message: Message, _):
     
     if len(message.command) < 2:
         return await message.reply_text("**➻ ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴍᴇ ᴀ sᴏɴɢ ɴᴀᴍᴇ ᴏʀ sᴏɴɢ ʟɪɴᴋ ᴏʀ ʏᴏᴜᴛᴜʙᴇ ᴘʟᴀʏʟɪsᴛ ʟɪɴᴋ ᴀғᴛᴇʀ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ..**\n\n**➥ ᴇxᴀᴍᴘʟᴇs:**\n\n▷ `/addplaylist Blue Eyes` (ᴘᴜᴛ ᴀ sᴘᴇᴄɪғɪᴄ sᴏɴɢ ɴᴀᴍᴇ)\n\n▷ /addplaylist [ʏᴏᴜᴛᴜʙᴇ ᴘʟᴀʏʟɪsᴛ ʟɪɴᴋ] (ᴛᴏ ᴀᴅᴅ ᴀʟʟ sᴏɴɢs ғʀᴏᴍ ᴀ ʏᴏᴜᴛᴜʙᴇ ᴘʟᴀʏʟɪsᴛ ɪɴ ʙᴏᴛ ᴘʟᴀʏʟɪsᴛ.)")
@@ -392,7 +392,7 @@ async def add_playlist(client, message: Message, _):
                 "duration": duration,
             }
             
-            await save_playlist(user_id, video_id, plist)
+            await save_playlist_clone(user_id, video_id, plist)
             keyboardes = InlineKeyboardMarkup(
             [
                 [
@@ -437,7 +437,7 @@ async def add_playlist(client, message: Message, _):
                 "duration": duration,
             }
             
-            await save_playlist(user_id, video_id, plist)
+            await save_playlist_clone(user_id, video_id, plist)
             keyboardes = InlineKeyboardMarkup(
             [            
                 [
@@ -459,7 +459,7 @@ async def add_playlist(client, message: Message, _):
             videoid = query.split("/")[-1].split("?")[0]
             user_id = message.from_user.id
             thumbnail = f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
-            _check = await get_playlist(user_id, videoid)
+            _check = await get_playlist_clone(user_id, videoid)
             if _check:
                 try:
                     await add.delete()
@@ -467,7 +467,7 @@ async def add_playlist(client, message: Message, _):
                 except KeyError:
                     pass
 
-            _count = await get_playlist_names(user_id)
+            _count = await get_playlist_names_clone(user_id)
             count = len(_count)
             if count == SERVER_PLAYLIST_LIMIT:
                 try:
@@ -485,7 +485,7 @@ async def add_playlist(client, message: Message, _):
                     "title": title,
                     "duration": duration,
                 }
-                await save_playlist(user_id, videoid, plist)
+                await save_playlist_clone(user_id, videoid, plist)
 
                 # Create inline keyboard with remove button
                 keyboard = InlineKeyboardMarkup(
@@ -524,14 +524,14 @@ async def add_playlist(client, message: Message, _):
             channel_name = results[0]["channel"]
 
             user_id = message.from_user.id
-            _check = await get_playlist(user_id, videoid)
+            _check = await get_playlist_clone(user_id, videoid)
             if _check:
                 try:
                     return await message.reply_photo(thumbnail, caption=_["playlist_8"])
                 except KeyError:
                     pass
 
-            _count = await get_playlist_names(user_id)
+            _count = await get_playlist_names_clone(user_id)
             count = len(_count)
             if count == SERVER_PLAYLIST_LIMIT:
                 try:
@@ -548,7 +548,7 @@ async def add_playlist(client, message: Message, _):
                 "duration": duration_min,
             }
 
-            await save_playlist(user_id, videoid, plist)
+            await save_playlist_clone(user_id, videoid, plist)
 
             # Create inline keyboard with remove button
             keyboard = InlineKeyboardMarkup(
@@ -570,7 +570,7 @@ async def add_playlist(client, message: Message, _):
 @Client.on_callback_query(filters.regex("open_playlist") & ~BANNED_USERS)
 @languageCB
 async def open_playlist(client, CallbackQuery, _):
-    _playlist = await get_playlist_names(CallbackQuery.from_user.id)
+    _playlist = await get_playlist_names_clone(CallbackQuery.from_user.id)
     if _playlist:
         get = await CallbackQuery.message.edit_text(_["playlist_2"])
     else:
@@ -585,7 +585,7 @@ async def del_plist(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     videoid = callback_data.split(None, 1)[1]
     user_id = CallbackQuery.from_user.id
-    deleted = await delete_playlist(
+    deleted = await delete_playlist_clone(
         CallbackQuery.from_user.id, videoid
     )
     if deleted:
@@ -617,12 +617,12 @@ async def del_plist(client, CallbackQuery, _):
 
 @Client.on_callback_query(filters.regex("recover_playlist") & ~BANNED_USERS)
 @languageCB
-async def add_playlist(client, CallbackQuery, _):
+async def add_playlist_clone(client, CallbackQuery, _):
     from VIPMUSIC import YouTube
     callback_data = CallbackQuery.data.strip()
     videoid = callback_data.split(None, 1)[1]
     user_id = CallbackQuery.from_user.id
-    _check = await get_playlist(user_id, videoid)
+    _check = await get_playlist_clone(user_id, videoid)
     if _check:
         try:
             return await CallbackQuery.answer(
@@ -630,7 +630,7 @@ async def add_playlist(client, CallbackQuery, _):
             )
         except:
             return
-    _count = await get_playlist_names(user_id)
+    _count = await get_playlist_names_clone(user_id)
     count = len(_count)
     if count == SERVER_PLAYLIST_LIMIT:
         try:
@@ -653,7 +653,7 @@ async def add_playlist(client, CallbackQuery, _):
         "title": title,
         "duration": duration_min,
     }
-    await save_playlist(user_id, videoid, plist)
+    await save_playlist_clone(user_id, videoid, plist)
     try:
         title = (title[:30]).title()
         keyboardss = InlineKeyboardMarkup(
@@ -671,18 +671,18 @@ async def add_playlist(client, CallbackQuery, _):
 
 @Client.on_callback_query(filters.regex("add_playlist") & ~BANNED_USERS)
 @languageCB
-async def add_playlist(client, CallbackQuery, _):
+async def add_playlist_clone(client, CallbackQuery, _):
     await CallbackQuery.answer("➻ ᴛᴏ ᴀᴅᴅ ᴀ sᴏɴɢ ɪɴ ʏᴏᴜʀ ᴘʟᴀʏʟɪsᴛ ᴊᴜsᴛ ᴛʏᴘᴇ /addplaylist (Here your song name)\n\n➥ ᴇxᴀᴍᴘʟᴇ » /addplaylist Blue Eyes Blue tyes.", show_alert=True)
     
 
 @Client.on_callback_query(filters.regex("vip_playlist") & ~BANNED_USERS)
 @languageCB
-async def add_playlists(client, CallbackQuery, _):
+async def add_playlists_clone(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     videoid = callback_data.split(None, 1)[1]
     user_id = CallbackQuery.from_user.id
     from VIPMUSIC import YouTube
-    _check = await get_playlist(user_id, videoid)
+    _check = await get_playlist_clone(user_id, videoid)
     if _check:
         try:
             from VIPMUSIC import YouTube
@@ -691,7 +691,7 @@ async def add_playlists(client, CallbackQuery, _):
             )
         except:
             return
-    _count = await get_playlist_names(user_id)
+    _count = await get_playlist_names_clone(user_id)
     count = len(_count)
     if count == SERVER_PLAYLIST_LIMIT:
         try:
@@ -714,7 +714,7 @@ async def add_playlists(client, CallbackQuery, _):
         "title": title,
         "duration": duration_min,
     }
-    await save_playlist(user_id, videoid, plist)
+    await save_playlist_clone(user_id, videoid, plist)
     try:
         title = (title[:30]).title()
         return await CallbackQuery.answer(
@@ -728,7 +728,7 @@ DELETE_ALL_PLAYLIST_COMMAND = ("delallplaylist")
 
 @Client.on_message(filters.command(DELETE_ALL_PLAYLIST_COMMAND) & ~BANNED_USERS)
 @language
-async def delete_all_playlists(client, message, _):
+async def delete_all_playlists_clone(client, message, _):
     user_id = message.from_user.id
     current_time = time()
     # Update the last message timestamp for the user
@@ -750,7 +750,7 @@ async def delete_all_playlists(client, message, _):
         user_last_message_time[user_id] = current_time
     from VIPMUSIC import YouTube
     
-    _playlist = await get_playlist_names(user_id)
+    _playlist = await get_playlist_names_clone(user_id)
     if _playlist:
         try:
             upl = warning_markup(_)
@@ -763,12 +763,12 @@ async def delete_all_playlists(client, message, _):
         
 @Client.on_callback_query(filters.regex("del_playlist") & ~BANNED_USERS)
 @languageCB
-async def del_plist(client, CallbackQuery, _):
+async def del_plist_clone(client, CallbackQuery, _):
     from VIPMUSIC import YouTube
     callback_data = CallbackQuery.data.strip()
     videoid = callback_data.split(None, 1)[1]
     user_id = CallbackQuery.from_user.id
-    deleted = await delete_playlist(
+    deleted = await delete_playlist_clone(
         CallbackQuery.from_user.id, videoid
     )
     if deleted:
@@ -796,12 +796,12 @@ async def del_plist(client, CallbackQuery, _):
     filters.regex("delete_whole_playlist") & ~BANNED_USERS
 )
 @languageCB
-async def del_whole_playlist(client, CallbackQuery, _):
+async def del_whole_playlist_clone(client, CallbackQuery, _):
     from VIPMUSIC import YouTube
-    _playlist = await get_playlist_names(CallbackQuery.from_user.id)
+    _playlist = await get_playlist_names_clone(CallbackQuery.from_user.id)
     for x in _playlist:
         await CallbackQuery.answer("➻ ᴏᴋ sɪʀ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ.\n\n➥ ᴅᴇʟᴇᴛɪɴɢ ʏᴏᴜʀ ᴘʟᴀʏʟɪsᴛ...", show_alert=True)
-        await delete_playlist(CallbackQuery.from_user.id, x)
+        await delete_playlist_clone(CallbackQuery.from_user.id, x)
     return await CallbackQuery.edit_message_text(_["playlist_13"])
 
 
@@ -809,7 +809,7 @@ async def del_whole_playlist(client, CallbackQuery, _):
     filters.regex("get_playlist_playmode") & ~BANNED_USERS
 )
 @languageCB
-async def get_playlist_playmode_(client, CallbackQuery, _):
+async def get_playlist_playmode_clone(client, CallbackQuery, _):
     try:
         await CallbackQuery.answer()
     except:
@@ -824,7 +824,7 @@ async def get_playlist_playmode_(client, CallbackQuery, _):
     filters.regex("delete_warning") & ~BANNED_USERS
 )
 @languageCB
-async def delete_warning_message(client, CallbackQuery, _):
+async def delete_warning_message_clone(client, CallbackQuery, _):
     from VIPMUSIC import YouTube
     try:
         await CallbackQuery.answer()
@@ -838,7 +838,7 @@ async def delete_warning_message(client, CallbackQuery, _):
 
 @Client.on_callback_query(filters.regex("home_play") & ~BANNED_USERS)
 @languageCB
-async def home_play_(client, CallbackQuery, _):
+async def home_play_clone(client, CallbackQuery, _):
     from VIPMUSIC import YouTube
     try:
         await CallbackQuery.answer()
@@ -854,10 +854,10 @@ async def home_play_(client, CallbackQuery, _):
     filters.regex("del_back_playlist") & ~BANNED_USERS
 )
 @languageCB
-async def del_back_playlist(client, CallbackQuery, _):
+async def del_back_playlist_clone(client, CallbackQuery, _):
     from VIPMUSIC import YouTube
     user_id = CallbackQuery.from_user.id
-    _playlist = await get_playlist_names(user_id)
+    _playlist = await get_playlist_names_clone(user_id)
     if _playlist:
         try:
             await CallbackQuery.answer(
