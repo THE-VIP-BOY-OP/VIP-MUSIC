6b585cbe668557ca44700d12469540035f613265
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-06 16:11:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ load_dotenv()
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
+
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
