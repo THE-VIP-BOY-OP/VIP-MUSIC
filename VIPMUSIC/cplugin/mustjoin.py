ddc2a80f9a6a1a386e747b09c504863dc12902a1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-06 10:10:38 +0530

    Update mustjoin.py

--- VIPMUSIC/cplugin/mustjoin.py
+++ VIPMUSIC/cplugin/mustjoin.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from DAXXMUSIC import app
+from VIPMUSIC import app
 
 #--------------------------
 
