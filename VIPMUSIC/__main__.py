b3e7abe44f3792ee54c50699ea05f1eb467bef25
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-07 09:27:19 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -39,7 +39,7 @@ async def init():
     for all_module in ALL_MODULES:
         importlib.import_module("VIPMUSIC.plugins" + all_module)
     LOGGER("VIPMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
-    await restart_bots()
+    
     await userbot.start()
     await VIP.start()
     await VIP.decorators()
@@ -49,6 +49,7 @@ async def init():
                     
     await app.stop()
     await userbot.stop()
+    await restart_bots()
     LOGGER("VIPMUSIC").info("                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
     
 
