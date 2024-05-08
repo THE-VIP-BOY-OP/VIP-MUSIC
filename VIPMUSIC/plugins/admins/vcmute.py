c69a901aac59043766032582ea74f4d5b82d645f
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-08 09:33:16 +0000

    Auto fixes

--- VIPMUSIC/plugins/admins/vcmute.py
+++ VIPMUSIC/plugins/admins/vcmute.py
@@ -13,11 +13,14 @@ async def mute_admin(client, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
         return await message.reply_text("ᴇʀʀᴏʀ ! ᴡʀᴏɴɢ ᴜsᴀɢᴇ ᴏꜰ ᴄᴏᴍᴍᴀɴᴅ.")
     if await is_muted(chat_id):
-        return await message.reply_text("➻ ᴍᴜsɪᴄ ɪs ᴀʟʀᴇᴀᴅʏ ᴍᴜᴛᴇᴅ", disable_web_page_preview=True)
+        return await message.reply_text(
+            "➻ ᴍᴜsɪᴄ ɪs ᴀʟʀᴇᴀᴅʏ ᴍᴜᴛᴇᴅ", disable_web_page_preview=True
+        )
     await mute_on(chat_id)
     await VIP.mute_stream(chat_id)
     await message.reply_text(
-        "➻ ᴍᴜsɪᴄ ɪs ᴍᴜᴛᴇᴅ ʙʏ {}!".format(message.from_user.mention), disable_web_page_preview=True
+        "➻ ᴍᴜsɪᴄ ɪs ᴍᴜᴛᴇᴅ ʙʏ {}!".format(message.from_user.mention),
+        disable_web_page_preview=True,
     )
 
 
@@ -27,9 +30,12 @@ async def unmute_admin(client: Client, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
         return await message.reply_text("ᴇʀʀᴏʀ ! ᴡʀᴏɴɢ ᴜsᴀɢᴇ ᴏꜰ ᴄᴏᴍᴍᴀɴᴅ.")
     if not await is_muted(chat_id):
-        return await message.reply_text("➻ ᴍᴜsɪᴄ ɪs ᴀʟʀᴇᴀᴅʏ ᴜɴᴍᴜᴛᴇᴅ", disable_web_page_preview=True)
+        return await message.reply_text(
+            "➻ ᴍᴜsɪᴄ ɪs ᴀʟʀᴇᴀᴅʏ ᴜɴᴍᴜᴛᴇᴅ", disable_web_page_preview=True
+        )
     await mute_off(chat_id)
     await VIP.unmute_stream(chat_id)
     await message.reply_text(
-        "➻ ᴍᴜsɪᴄ ɪs ᴜɴᴍᴜᴛᴇᴅ ʙʏ {}!".format(message.from_user.mention), disable_web_page_preview=True
+        "➻ ᴍᴜsɪᴄ ɪs ᴜɴᴍᴜᴛᴇᴅ ʙʏ {}!".format(message.from_user.mention),
+        disable_web_page_preview=True,
     )
