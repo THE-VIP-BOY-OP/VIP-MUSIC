1cf0af696d12ebd862f2be279ea53ef5fe0a8fb5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-09 08:23:15 +0530

    Update mustjoin.py

--- VIPMUSIC/cplugin/mustjoin.py
+++ VIPMUSIC/cplugin/mustjoin.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from VIPMUSIC import app
-
+from config import SUPPORT_CHAT
 # --------------------------
 
 MUST_JOIN = "VIP_CREATORS"
