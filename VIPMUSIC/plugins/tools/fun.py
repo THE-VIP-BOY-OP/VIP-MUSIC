0fc258a4d7d4eeb43514c0d55985cf852bb1bef5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-11 17:08:21 +0530

    Update tools

---
+++ VIPMUSIC/plugins/tools/fun.py
@@ -0,0 +1,8 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from VIPMUSIC import app 
+
+@app.on_message(filters.command(["dice", "ludo"]))
+async def dice(c, m: Message):
+    dicen = await c.send_dice(m.chat.id, reply_to_message_id=m.id)
+    await dicen.reply_text("results is {0}".format(dicen.dice.value))
