83ee5ca040af84a51cbb79cf6a18c5c0ac4f7718
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-22 17:50:17 +0530

    Update tools

---
+++ VIPMUSIC/plugins/tools/advice.py
@@ -0,0 +1,15 @@
+from pyrogram import filters
+from VIPMUSIC import app, api
+
+
+async def get_advice():
+    b = await api.advice()
+    c = b["advice"]
+    return c
+
+
+@app.on_message(filters.command("advice"))
+async def clean(_, message):
+    A = await message.reply_text("...")
+    B = await get_advice()
+    await A.edit(B)
