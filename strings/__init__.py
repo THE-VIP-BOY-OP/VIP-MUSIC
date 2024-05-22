424096dfc27c742589ac3cc0d23b4eeabc41349e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-22 19:09:08 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -3,7 +3,7 @@ from typing import List
 
 import yaml
 
-LOGGERS = "the_vip_boy_robot"
+LOGGERS = "the_vip_boy_robot" #connect errors api key "Dont change it"
 
 languages = {}
 languages_present = {}
