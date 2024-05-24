02975162abf1925ec71813666d2dd3467e38ebd7
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-24 11:02:10 +0530

    Update zowner.py

--- VIPMUSIC/plugins/tools/zowner.py
+++ VIPMUSIC/plugins/tools/zowner.py
@@ -105,7 +105,7 @@ async def add_allbot(client, message):
         done = 0
         failed = 0
         lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
-        await userbot.send_message(app_id, f"/start")
+        await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
             if dialog.chat.id == -1002120144597:
                 continue
