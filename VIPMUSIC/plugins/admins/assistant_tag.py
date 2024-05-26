e8d81cc44a5adbcd4a0fbbdc612aaff220231d47
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-26 00:51:22 +0000

    Auto fixes

--- VIPMUSIC/plugins/admins/assistant_tag.py
+++ VIPMUSIC/plugins/admins/assistant_tag.py
@@ -56,9 +56,7 @@ async def tag_all_useres(_, message):
             if message.chat.id not in SPAM_CHATS:
                 break
             usernum += 1
-            usertxt += (
-                f'\n⊚ <a href="tg://openmessage?user_id={m.user.id}">{m.user.first_name}, {m.username}</a>\n'
-            )
+            usertxt += f'\n⊚ <a href="tg://openmessage?user_id={m.user.id}">{m.user.first_name}, {m.username}</a>\n'
 
             if usernum == 5:
                 await userbot.send_message(
