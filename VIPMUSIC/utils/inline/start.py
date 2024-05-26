5790af2cac7541f538b63cc4e766cc6dc737c493
Ard Neriw <ardneriw004@gmail.com>
2024-05-26 12:26:20 +0545

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -12,8 +12,8 @@ def start_panel(_):
             ),
         ],
         [
-            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
-            InlineKeyboardButton(text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"),
+            InlineKeyboardButton(text="💮 𝐇𝙴𝙻𝙿 💮", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="💮 𝐒𝙴𝚃 💮", callback_data="settings_helper"),
         ],
         [
             InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_CHAT),
@@ -26,17 +26,17 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
+                text="💮 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💮",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text="𝐆𝚁𝙾𝚄𝙿✨", url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text="𝐌ᴏʀᴇ🥀", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="💮 𝐆𝚁𝙾𝚄𝙿 💮", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="💮 𝐌ᴏʀᴇ 💮", url=config.SUPPORT_CHANNEL),
         ],
         [
             InlineKeyboardButton(
-                text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper"
+                text="💮 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 💮", callback_data="settings_back_helper"
             )
         ],
     ]
