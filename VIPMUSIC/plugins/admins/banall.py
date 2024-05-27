0eae82215ef75c524d465c9e179edab531a1da8b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-27 18:28:09 +0530

    Update banall.py

--- VIPMUSIC/plugins/admins/banall.py
+++ VIPMUSIC/plugins/admins/banall.py
@@ -7,12 +7,13 @@ from VIPMUSIC.misc import SUDOERS
 
 BOT_ID = app.id
 
-
 @app.on_message(filters.command("banall") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id
+    user_id = msg.from_user.id  # ID of the user who issued the command
     bot = await app.get_chat_member(chat_id, BOT_ID)
     bot_permission = bot.privileges.can_restrict_members == True
+
     if bot_permission:
         total_members = 0
         banned_count = 0
@@ -26,13 +27,14 @@ async def ban_all(_, msg):
 
         async for member in app.get_chat_members(chat_id):
             try:
-                await app.ban_chat_member(chat_id, member.user.id)
-                banned_count += 1
+                if member.user.id != user_id and member.user.id not in SUDOERS:
+                    await app.ban_chat_member(chat_id, member.user.id)
+                    banned_count += 1
 
-                if banned_count % 5 == 0:
-                    await ok.edit_text(
-                        f"Banned {banned_count} members out of {total_members}"
-                    )
+                    if banned_count % 5 == 0:
+                        await ok.edit_text(
+                            f"Banned {banned_count} members out of {total_members}"
+                        )
 
             except Exception as e:
                 pass
