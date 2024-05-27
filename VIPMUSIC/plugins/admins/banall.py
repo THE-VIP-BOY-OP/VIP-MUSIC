aad26d3488f0ebf7a904a5b6d0c3f916aca23b9b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-27 14:54:23 +0530

    Update banall.py

--- VIPMUSIC/plugins/admins/banall.py
+++ VIPMUSIC/plugins/admins/banall.py
@@ -1,6 +1,5 @@
 from VIPMUSIC import app
 from config import OWNER_ID
-import async
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from VIPMUSIC.utils.vip_ban import admin_filter
@@ -8,12 +7,11 @@ from VIPMUSIC.misc import SUDOERS
 
 BOT_ID = app.id
 
-
 @app.on_message(filters.command("banall") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id
     bot = await app.get_chat_member(chat_id, BOT_ID)
-    bot_permission = bot.privileges.can_restrict_members == True
+    bot_permission = bot.can_restrict_members
     if bot_permission:
         total_members = 0
         banned_count = 0
@@ -23,7 +21,7 @@ async def ban_all(_, msg):
 
         ok = await msg.reply_text(f"Total members found: {total_members}")
 
-        for member in app.get_chat_members(chat_id):
+        async for member in app.get_chat_members(chat_id):
             try:
                 await app.ban_chat_member(chat_id, member.user.id)
                 banned_count += 1
