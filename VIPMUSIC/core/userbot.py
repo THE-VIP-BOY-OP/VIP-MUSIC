c539517bb3c4a9547eaa07148f84f02fb3638e85
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-28 20:08:08 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -28,7 +28,7 @@ class Userbot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
-            plugins=dict(root="VIPMUSIC.cplugin"),
+            workdir="datafiles",
         )
 
         self.two = Client(
