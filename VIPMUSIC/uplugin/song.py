dbef511e2bfe562b04e99f4defa9a467321650c5
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-28 16:30:21 +0000

    Auto fixes

--- VIPMUSIC/uplugin/song.py
+++ VIPMUSIC/uplugin/song.py
@@ -1,6 +1,6 @@
-#from pyrogram import Client, filters
+# from pyrogram import Client, filters
 
 
-#@Client.on_message(filters.text)
-#def handle_message(client, message):
-   # message.reply_text("Hello, world!")
+# @Client.on_message(filters.text)
+# def handle_message(client, message):
+# message.reply_text("Hello, world!")
