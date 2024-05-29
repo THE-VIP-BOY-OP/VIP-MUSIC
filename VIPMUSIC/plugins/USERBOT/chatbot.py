d091a6af7554f01880912f6320b66f2dd21b79c0
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-29 17:35:50 +0000

    Pylint: Auto Fixes

--- VIPMUSIC/plugins/USERBOT/chatbot.py
+++ VIPMUSIC/plugins/USERBOT/chatbot.py
@@ -1,18 +1,10 @@
 # © Vip TEAM
-from pyrogram import Client, filters
-import asyncio
-from pyrogram.types import *
-from pymongo import MongoClient
-import requests
 import random
-from pyrogram.errors import (
-    PeerIdInvalid,
-    ChatWriteForbidden
-)
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-import os
-import re
+
+from pymongo import MongoClient
+from pyrogram import Client, filters
+from pyrogram.types import *
+from pyrogram.types import Message
 
 from config import MONGO_DB_URI as MONGO_URL
 
