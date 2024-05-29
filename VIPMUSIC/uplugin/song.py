50f703c599685da36124f4f8d424e94326dce420
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-29 06:03:45 +0530

    Update song.py

--- VIPMUSIC/uplugin/song.py
+++ VIPMUSIC/uplugin/song.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.text)
+@Client.on_message(filters.text & filters.chat("vk_zone"))
 def handle_message(client, message):
     message.reply_text("Hello, world!")
