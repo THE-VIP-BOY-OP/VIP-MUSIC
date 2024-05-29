bd867ab09c95c745d596f321153f8b1e92cffc86
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-29 08:17:33 +0530

    Update Start.py

--- VIPMUSIC/uplugin/Start.py
+++ VIPMUSIC/uplugin/Start.py
@@ -198,3 +198,52 @@ async def start_gp(client, message: Message, _):
             await message.edit_text(
                 f"**ᴜɴᴀʙʟᴇ ᴛᴏ ɪɴᴠɪᴛᴇ ᴍʏ [ᴀssɪsᴛᴀɴᴛ](tg://openmessage?user_id={userbot.id}). ᴘʟᴇᴀsᴇ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ᴡɪᴛʜ ɪɴᴠɪᴛᴇ ᴜsᴇʀ ᴀᴅᴍɪɴ ᴘᴏᴡᴇʀ ᴛᴏ ɪɴᴠɪᴛᴇ ᴍʏ [ᴀssɪsᴛᴀɴᴛ](tg://openmessage?user_id={userbot.id}) ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ.**"
             )
+
+
+import time
+from datetime import datetime
+
+import psutil
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from config import SUPPORT_CHAT, PING_IMG_URL
+from .utils import StartTime
+from VIPMUSIC.utils import get_readable_time
+
+
+@Client.on_message(filters.command("ping"))
+async def ping_clone(client: Client, message: Message):
+    i = await client.get_me()
+    hmm = await message.reply_photo(
+        photo=PING_IMG_URL, caption=f"{i.mention} ɪs ᴘɪɴɢɪɴɢ..."
+    )
+    upt = int(time.time() - StartTime)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    start = datetime.now()
+    resp = (datetime.now() - start).microseconds / 1000
+    uptime = get_readable_time((upt))
+
+    await hmm.edit_text(
+        f"""➻ ᴩᴏɴɢ : `{resp}ᴍs`
+
+<b><u>{i.mention} sʏsᴛᴇᴍ sᴛᴀᴛs :</u></b>
+
+๏ **ᴜᴩᴛɪᴍᴇ :** {uptime}
+๏ **ʀᴀᴍ :** {mem}
+๏ **ᴄᴩᴜ :** {cpu}
+๏ **ᴅɪsᴋ :** {disk}""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url=SUPPORT_CHAT),
+                    InlineKeyboardButton(
+                        "✨ 𝙰𝙳𝙳 𝙼𝙴✨",
+                        url=f"https://t.me/{i.username}?startgroup=true",
+                    ),
+                ],
+            ]
+        ),
+    )
