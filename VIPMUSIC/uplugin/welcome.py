c3f3f9e4924404944d9a3e25281654e93ac52ab4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-29 06:29:28 +0530

    Create welcome.py

---
+++ VIPMUSIC/uplugin/welcome.py
@@ -0,0 +1,221 @@
+from re import findall
+
+from pyrogram import filters, Client
+from pyrogram.enums import ChatMemberStatus as CMS
+from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
+from pyrogram.types import (
+    Chat,
+    ChatMemberUpdated,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+
+from VIPMUSIC import app
+from VIPMUSIC.misc import SUDOERS
+from VIPMUSIC.plugins.admins.notes import extract_urls
+from VIPMUSIC.utils.database import (
+    del_welcome,
+    get_welcome,
+    is_gbanned_user,
+    set_welcome,
+)
+from VIPMUSIC.utils.error import capture_err
+from VIPMUSIC.utils.functions import check_format, extract_text_and_keyb
+from VIPMUSIC.utils.keyboard import ikb
+from VIPMUSIC.utils.permissions import adminsOnly
+
+
+async def handle_new_member(member, chat):
+
+    try:
+        if member.id in SUDOERS:
+            return
+        if await is_gbanned_user(member.id):
+            await chat.ban_member(member.id)
+            await app.send_message(
+                chat.id,
+                f"{member.mention} was globally banned, and got removed,"
+                + " if you think this is a false gban, you can appeal"
+                + " for this ban in support chat.",
+            )
+            return
+        if member.is_bot:
+            return
+        return await send_welcome_message(chat, member.id)
+
+    except ChatAdminRequired:
+        return
+
+
+@Client.on_chat_member_updated(filters.group, group=16)
+@capture_err
+async def welcome(_, user: ChatMemberUpdated):
+    if not (
+        user.new_chat_member
+        and user.new_chat_member.status not in {CMS.RESTRICTED}
+        and not user.old_chat_member
+    ):
+        return
+
+    member = user.new_chat_member.user if user.new_chat_member else user.from_user
+    chat = user.chat
+    return await handle_new_member(member, chat)
+
+
+async def send_welcome_message(chat: Chat, user_id: int, delete: bool = False):
+    welcome, raw_text, file_id = await get_welcome(chat.id)
+
+    if not raw_text:
+        return
+    text = raw_text
+    keyb = None
+    if findall(r"\[.+\,.+\]", raw_text):
+        text, keyb = extract_text_and_keyb(ikb, raw_text)
+
+    if "{chat}" in text:
+        text = text.replace("{chat}", chat.title)
+    if "{name}" in text:
+        text = text.replace("{name}", (await app.get_users(user_id)).mention)
+    if "{id}" in text:
+        text = text.replace("{id}", f"`{user_id}`")
+
+    if welcome == "Text":
+        m = await app.send_message(
+            chat.id,
+            text=text,
+            reply_markup=keyb,
+            disable_web_page_preview=True,
+        )
+    elif welcome == "Photo":
+        m = await app.send_photo(
+            chat.id,
+            photo=file_id,
+            caption=text,
+            reply_markup=keyb,
+        )
+    else:
+        m = await app.send_animation(
+            chat.id,
+            animation=file_id,
+            caption=text,
+            reply_markup=keyb,
+        )
+
+
+@Client.on_message(filters.command("setwelcome") & ~filters.private)
+@adminsOnly("can_change_info")
+async def set_welcome_func(_, message):
+    usage = "You need to reply to a text, gif or photo to set it as greetings.\n\nNotes: caption required for gif and photo."
+    key = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="More Help",
+                    url=f"t.me/{app.username}?start=greetings",
+                )
+            ],
+        ]
+    )
+    replied_message = message.reply_to_message
+    chat_id = message.chat.id
+    try:
+        if not replied_message:
+            await message.reply_text(usage, reply_markup=key)
+            return
+        if replied_message.animation:
+            welcome = "Animation"
+            file_id = replied_message.animation.file_id
+            text = replied_message.caption
+            if not text:
+                return await message.reply_text(usage, reply_markup=key)
+            raw_text = text.markdown
+        if replied_message.photo:
+            welcome = "Photo"
+            file_id = replied_message.photo.file_id
+            text = replied_message.caption
+            if not text:
+                return await message.reply_text(usage, reply_markup=key)
+            raw_text = text.markdown
+        if replied_message.text:
+            welcome = "Text"
+            file_id = None
+            text = replied_message.text
+            raw_text = text.markdown
+        if replied_message.reply_markup and not findall(r"\[.+\,.+\]", raw_text):
+            urls = extract_urls(replied_message.reply_markup)
+            if urls:
+                response = "\n".join(
+                    [f"{name}=[{text}, {url}]" for name, text, url in urls]
+                )
+                raw_text = raw_text + response
+        raw_text = await check_format(ikb, raw_text)
+        if raw_text:
+            await set_welcome(chat_id, welcome, raw_text, file_id)
+            return await message.reply_text(
+                "Welcome message has been successfully set."
+            )
+        else:
+            return await message.reply_text(
+                "Wrong formatting, check the help section.\n\n**Usage:**\nText: `Text`\nText + Buttons: `Text ~ Buttons`",
+                reply_markup=key,
+            )
+    except UnboundLocalError:
+        return await message.reply_text(
+            "**Only Text, Gif and Photo welcome message are supported.**"
+        )
+
+
+@Client.on_message(filters.command(["delwelcome", "deletewelcome"]) & ~filters.private)
+@adminsOnly("can_change_info")
+async def del_welcome_func(_, message):
+    chat_id = message.chat.id
+    await del_welcome(chat_id)
+    await message.reply_text("Welcome message has been deleted.")
+
+
+@Client.on_message(filters.command("getwelcome") & ~filters.private)
+@adminsOnly("can_change_info")
+async def get_welcome_func(_, message):
+    chat = message.chat
+    welcome, raw_text, file_id = await get_welcome(chat.id)
+    if not raw_text:
+        return await message.reply_text("No welcome message set.")
+    if not message.from_user:
+        return await message.reply_text("You're anon, can't send welcome message.")
+
+    await send_welcome_message(chat, message.from_user.id)
+
+    await message.reply_text(
+        f'Welcome: {welcome}\n\nFile_id: `{file_id}`\n\n`{raw_text.replace("`", "")}`'
+    )
+
+
+__MODULE__ = "Wᴇʟᴄᴏᴍᴇ"
+__HELP__ = """
+/setwelcome - Reply this to a message containing correct
+format for a welcome message, check end of this message.
+
+/delwelcome - Delete the welcome message.
+/getwelcome - Get the welcome message.
+
+**SET_WELCOME ->**
+
+**To set a photo or gif as welcome message. Add your welcome message as caption to the photo or gif. The caption muse be in the format given below.**
+
+For text welcome message just send the text. Then reply with the command 
+
+The format should be something like below.
+
+```
+**Hi** {name} [{id}] Welcome to {chat}
+
+~ #This separater (~) should be there between text and buttons, remove this comment also
+
+button=[Duck, https://duckduckgo.com]
+button2=[Github, https://github.com]
+```
+
+**NOTES ->**
+
+Checkout /markdownhelp to know more about formattings and other syntax.
+"""
