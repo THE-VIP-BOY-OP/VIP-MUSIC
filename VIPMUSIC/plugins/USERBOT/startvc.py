d53d6164efa277fe793b59f49f96467b1c0896df
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-29 12:11:58 +0530

    Update startvc.py

--- VIPMUSIC/plugins/USERBOT/startvc.py
+++ VIPMUSIC/plugins/USERBOT/startvc.py
@@ -15,23 +15,50 @@ from VIPMUSIC import app
 
 @Client.on_message(filters.command("startvc"))
 async def startvc(client: Client, message: Message):
-
-    call_name = message.text.split(maxsplit=1)[1] if len(message.command) > 1 else " VC"
-    hell = await message.reply_text("Starting Voice Chat...")
     userbot = await get_assistant(message.chat.id)
-
+    call_name = message.text.split(maxsplit=1)[1] if len(message.command) > 1 else " VC"
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    hell = await message.reply_text("Starting Voice Chat...")
     try:
         await client.invoke(
             CreateGroupCall(
-                peer=(await userbot.resolve_peer(message.chat.id)),
+                peer=(await client.resolve_peer(chat_id)),
                 random_id=int(str(uuid.uuid4().int)[:8]),
                 title=call_name,
             )
         )
-
+        
+        
         await hell.edit_text("Voice Chat started!")
+        return
     except Exception as e:
-        await hell.edit_text(str(e))
+        try:
+            await app.promote_chat_member(
+                chat_id,
+                userbot.id,
+                privileges=ChatPrivileges(
+                    can_change_info=False,
+                    can_invite_users=True,
+                    can_delete_messages=False,
+                    can_restrict_members=False,
+                    can_pin_messages=False,
+                    can_promote_members=False,
+                    can_manage_chat=True,
+                    can_manage_video_chats=True,
+                ),
+            )
+            await client.invoke(
+                CreateGroupCall(
+                    peer=(await client.resolve_peer(chat_id)),
+                    random_id=int(str(uuid.uuid4().int)[:8]),
+                    title=call_name,
+                )
+            )
+            
+            await hell.edit_text("Voice Chat started!")
+        except Exception as e:
+            await message.reply_text("Please make me an admin to start a voice chat.")
 
 
 @Client.on_message(filters.command("endvc"))
@@ -47,7 +74,6 @@ async def endvc(client: Client, message: Message):
         await hell.edit_text("Voice Chat ended!")
     except Exception as e:
         await hell.edit_text(str(e))
-
 
 @Client.on_message(filters.command("vclink"))
 async def vclink(client: Client, message: Message):
