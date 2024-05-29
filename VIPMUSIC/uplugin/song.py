68a49106b3514d720ce266d94e181a6a6c0edcc4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-29 06:05:48 +0530

    Update song.py

--- VIPMUSIC/uplugin/song.py
+++ VIPMUSIC/uplugin/song.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
+from VIPMUSIC.misc import SUDOERS
 
-
-@Client.on_message(filters.text & filters.chat("vk_zone"))
+@Client.on_message(filters.text & SUDOERS)
 def handle_message(client, message):
     message.reply_text("Hello, world!")
