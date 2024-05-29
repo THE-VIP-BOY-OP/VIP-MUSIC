39efa6ab0ee0b51b857eb693b90521f10568e836
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-05-29 12:28:56 +0530

    Update startvc.py

--- VIPMUSIC/plugins/USERBOT/startvc.py
+++ VIPMUSIC/plugins/USERBOT/startvc.py
@@ -18,7 +18,7 @@ async def startvc(client: Client, message: Message):
 
     call_name = message.text.split(maxsplit=1)[1] if len(message.command) > 1 else " VC"
     chat_id = message.chat.id
-    user_id = client.me.id
+    user_id = (await client.get_me()).id
     hell = await message.reply_text("Starting Voice Chat...")
     try:
         await client.invoke(
