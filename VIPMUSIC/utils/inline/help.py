2ed6bd0a52e8094a55426b86f11581e88f69809d
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-01 04:44:28 +0000

    Pylint: Auto Fixes

--- VIPMUSIC/utils/inline/help.py
+++ VIPMUSIC/utils/inline/help.py
@@ -11,48 +11,28 @@ def first_page(_):
     first_page_menu = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    text=_["H_B_1"], callback_data="helpcallback hb1"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"], callback_data="helpcallback hb2"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"], callback_data="helpcallback hb3"
-                ),
+                InlineKeyboardButton(text=_["H_B_1"], callback_data="helpcallback hb1"),
+                InlineKeyboardButton(text=_["H_B_2"], callback_data="helpcallback hb2"),
+                InlineKeyboardButton(text=_["H_B_3"], callback_data="helpcallback hb3"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_4"], callback_data="helpcallback hb4"
-                ),
+                InlineKeyboardButton(text=_["H_B_4"], callback_data="helpcallback hb4"),
                 InlineKeyboardButton(
                     text=_["H_B_12"], callback_data="helpcallback hb12"
                 ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"], callback_data="helpcallback hb5"
-                ),
+                InlineKeyboardButton(text=_["H_B_5"], callback_data="helpcallback hb5"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_6"], callback_data="helpcallback hb6"
-                ),
+                InlineKeyboardButton(text=_["H_B_6"], callback_data="helpcallback hb6"),
                 InlineKeyboardButton(
                     text=_["H_B_10"], callback_data="helpcallback hb10"
                 ),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_8"], callback_data="helpcallback hb8"
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"], callback_data="helpcallback hb9"
-                ),
+                InlineKeyboardButton(text=_["H_B_8"], callback_data="helpcallback hb8"),
+                InlineKeyboardButton(text=_["H_B_9"], callback_data="helpcallback hb9"),
             ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_11"], callback_data="helpcallback hb11"
-                )
-            ],
+            [InlineKeyboardButton(text=_["H_B_11"], callback_data="helpcallback hb11")],
             controll_button,
         ]
     )
@@ -66,9 +46,7 @@ def second_page(_):
     second_page_menu = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    text=_["H_B_7"], callback_data="helpcallback hb7"
-                ),
+                InlineKeyboardButton(text=_["H_B_7"], callback_data="helpcallback hb7"),
                 InlineKeyboardButton(
                     text=_["H_B_19"], callback_data="helpcallback hb19"
                 ),
@@ -103,11 +81,7 @@ def second_page(_):
                     text=_["H_B_22"], callback_data="help_callback hb22"
                 ),
             ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_21"], callback_data="helpcallback hb21"
-                )
-            ],
+            [InlineKeyboardButton(text=_["H_B_21"], callback_data="helpcallback hb21")],
             controll_button,
         ]
     )
