49be9b912c9e6479dd91b88e5dd060da2cf04a97
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-08 12:33:49 +0530

    Update help.py

--- VIPMUSIC/plugins/bot/help.py
+++ VIPMUSIC/plugins/bot/help.py
@@ -30,7 +30,7 @@ async def clean(_, m):
 
 
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
-async def helpupdate(_, m):
+async def helpupdate(CallbackQuery, _, m):
     text, keyboard = await help_parser(m.from_user.mention)
     await CallbackQuery.m.edit_photo(
         photo=config.START_IMG_URL, caption=text, reply_markup=keyboard
