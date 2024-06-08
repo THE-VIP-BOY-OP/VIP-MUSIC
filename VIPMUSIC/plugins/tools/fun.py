e37e2e3fdb4bdea85cf10c71cd2866f32b216785
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-08 06:18:48 +0000

    Pylint: Auto Fixes

--- VIPMUSIC/plugins/tools/fun.py
+++ VIPMUSIC/plugins/tools/fun.py
@@ -9,5 +9,6 @@ async def dice(c, m: Message):
     dicen = await c.send_dice(m.chat.id, reply_to_message_id=m.id)
     await dicen.reply_text("results is {0}".format(dicen.dice.value))
 
+
 __MODULE__ = "Fun"
 __HELP__ = f"comming soon"
