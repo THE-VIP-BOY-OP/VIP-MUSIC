f8a5f79868d4b3338487718355377a61b0f717f4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-09 14:44:14 +0530

    Update channel.py

--- VIPMUSIC/plugins/play/channel.py
+++ VIPMUSIC/plugins/play/channel.py
@@ -79,3 +79,8 @@ async def playmode_(client, message: Message, _):
             return await message.reply_text(_["cplay_6"].format(chat.title, cusn))
         await set_cmode(message.chat.id, chat.id)
         return await message.reply_text(_["cplay_3"].format(chat.title, chat.id))
+
+__MODULE__ = "C-Play"
+__HELP__ = """
+/channelplay @username :- To link a channel for play songs."""
+                    
