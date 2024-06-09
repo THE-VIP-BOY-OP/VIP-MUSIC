145af7221070b3630a5c188d922dd50c60cd925c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-09 17:07:36 +0530

    Update seek.py

--- VIPMUSIC/plugins/admins/seek.py
+++ VIPMUSIC/plugins/admins/seek.py
@@ -73,3 +73,20 @@ async def seek_comm(cli, message: Message, _, chat_id):
         text=_["admin_25"].format(seconds_to_min(to_seek), message.from_user.mention),
         reply_markup=close_markup(_),
     )
+
+__MODULE__ = "Seek"
+__HELP__ = """
+**Seek**
+
+This module allows administrators to seek forward or backward in the currently playing track.
+
+Commands:
+- /seek <time>: Seeks forward by the specified time (in seconds) for group.
+- /cseek <time>: Seeks backward by the specified time (in seconds) for channels.
+- /seekback <time>: Seeks backward by the specified time (in seconds) for group.
+- /cseekback <time>: Seeks backward by the specified time (in seconds) for channels.
+
+Note:
+- Only administrators can use these commands.
+- Time should be specified in seconds.
+"""
