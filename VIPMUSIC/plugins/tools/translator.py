aab5e5d5e1e700a3a02df1cc4bb0c64574071719
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-09 10:22:22 +0530

    Update translator.py

--- VIPMUSIC/plugins/tools/translator.py
+++ VIPMUSIC/plugins/tools/translator.py
@@ -1,20 +1,23 @@
+__MODULE__ = "Translator"
+__HELP__ = """
+This module provides commands to translate messages.
+
+- `/tr [language_code]`: Translate a message. Reply to the message you want to translate. Optionally, specify the target language code. If not specified, the target language will be English (en).
+- `/langcodes`: Get a list of language codes and their corresponding languages.
+"""
+
 from gpytranslate import Translator
 from pyrogram import filters
 
 from VIPMUSIC import app
 
-# .......
-
 trans = Translator()
-
-# ......
-
 
 @app.on_message(filters.command("tr"))
 async def translate(_, message) -> None:
     reply_msg = message.reply_to_message
     if not reply_msg:
-        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ !")
+        await message.reply_text("Please reply to a message to translate it!")
         return
     if reply_msg.caption:
         to_translate = reply_msg.caption
@@ -32,5 +35,24 @@ async def translate(_, message) -> None:
         source = await trans.detect(to_translate)
         dest = "en"
     translation = await trans(to_translate, sourcelang=source, targetlang=dest)
-    reply = f"ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} to {dest}:\n" f"{translation.text}"
+    reply = f"Translated from {source} to {dest}:\n{translation.text}"
     await message.reply_text(reply)
+
+@app.on_message(filters.command("langcodes"))
+async def language_codes(_, message):
+    languages = """
+    Language Codes:
+    en - English
+    es - Spanish
+    fr - French
+    de - German
+    it - Italian
+    pt - Portuguese
+    ru - Russian
+    zh - Chinese
+    ar - Arabic
+    ja - Japanese
+    ko - Korean
+    hi - Hindi
+    """
+    await message.reply_text(languages)
