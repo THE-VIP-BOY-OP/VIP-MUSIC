0b74bdd27b81aefdbf21dd48e78d66b400897f4f
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-09 06:13:58 +0000

    Pylint: Auto Fixes

--- VIPMUSIC/plugins/sudo/assistant.py
+++ VIPMUSIC/plugins/sudo/assistant.py
@@ -109,6 +109,7 @@ async def eor(msg: Message, **kwargs):
     spec = getfullargspec(func.__wrapped__).args
     return await func(**{k: v for k, v in kwargs.items() if k in spec})
 
+
 __MODULE__ = "Assistant"
 __HELP__ = """
 - `.setdp`: Reply to a photo and use this command to set it as the profile picture of all userbot accounts.
@@ -117,4 +118,3 @@ __HELP__ = """
 - `.deldp`: Delete the current profile picture of all userbot accounts.
 - `.delalldp`: Delete all profile pictures except the current one of all userbot accounts.
 """
-        
