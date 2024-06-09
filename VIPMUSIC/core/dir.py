1908dfffe19d0ed0b432e5d04b006e03ca904c17
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-09 09:45:59 +0530

    Update dir.py

--- VIPMUSIC/core/dir.py
+++ VIPMUSIC/core/dir.py
@@ -1,7 +1,7 @@
 import os
-
+import shutil
 from ..logging import LOGGER
-
+from config import EXTRA_PLUGINS_FOLDER
 
 def dirr():
     for file in os.listdir():
@@ -16,5 +16,9 @@ def dirr():
         os.mkdir("downloads")
     if "cache" not in os.listdir():
         os.mkdir("cache")
+
+    if EXTRA_PLUGINS_FOLDER in os.listdir():
+        shutil.rmtree(EXTRA_PLUGINS_FOLDER)
+
 
     LOGGER(__name__).info("Directories Updated.")
