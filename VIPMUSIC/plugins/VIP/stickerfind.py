2010825010f6550677b00d275ac673e1a67e293a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-09 15:40:25 +0530

    Update stickerfind.py

--- VIPMUSIC/plugins/VIP/stickerfind.py
+++ VIPMUSIC/plugins/VIP/stickerfind.py
@@ -13,3 +13,22 @@ def generate_sticker(client, message):
             message.reply_text(f"Error: {e}")
     else:
         message.reply_text("Please provide a sticker ID after /st command.")
+
+__MODULE__ = "Sticker Find"
+__HELP__ = """
+**Sticker Command**
+
+This command allows users to send stickers by providing a sticker ID.
+
+Features:
+- Reply to a command with a sticker ID to send the corresponding sticker.
+
+Commands:
+- /st <sticker_id>: Send a sticker by providing its ID.
+
+Example:
+- /st <sticker_id>: Sends the sticker corresponding to the provided ID.
+
+Note: Sticker IDs can be obtained from sticker packs or individual stickers.
+"""
+            
