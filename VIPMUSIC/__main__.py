86b76961a4f9afa62a7b654b3448cb75a2dad54f
SURAJ SAINI <161472575+SURAJ-SAINI-DEV@users.noreply.github.com>
2024-06-09 10:20:55 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -48,14 +48,14 @@ async def init():
     await VIP.start()
     await VIP.decorators()
     await restart_bots()
-    LOGGER("VIPMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("VIPMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝐒𝐔𝐑𝐀𝐉 𝐒𝐀𝐈𝐍𝐈 𝐒𝐈𝐑♨️\n╚═════ஜ۩۞۩ஜ════╝")
     await idle()
 
     await app.stop()
     await userbot.stop()
 
     LOGGER("VIPMUSIC").info(
-        "                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝"
+        "                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝐒𝐔𝐑𝐀𝐉 𝐒𝐀𝐈𝐍𝐈 𝐒𝐈𝐑♨️\n╚═════ஜ۩۞۩ஜ════╝"
     )
 
 
