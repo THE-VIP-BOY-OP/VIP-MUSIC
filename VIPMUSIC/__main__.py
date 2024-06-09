0f7fb1304efe48d4dde37daa908956e34cf593bc
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-09 09:48:12 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -43,7 +43,8 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module(f"VIPMUSIC.plugins" + all_module)
+        imported_module = importlib.import_module(all_module)
+
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
