a1ea91b83a360ab610355e85c3b8426fcea62c76
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-10 03:55:30 +0000

    Pylint: Auto Fixes

--- VIPMUSIC/utils/log_ignore.py
+++ VIPMUSIC/utils/log_ignore.py
@@ -1,15 +1,17 @@
 import logging
 
+
 class IgnoreSpecificErrors(logging.Filter):
     def filter(self, record):
         # Specify the error messages to ignore
         error_messages_to_ignore = [
-            "pyrogram.errors.exceptions.forbidden_403.ChatWriteForbidden: Telegram says: [403 CHAT_WRITE_FORBIDDEN] - You don't have rights to send messages in this chat (caused by \"messages.SendMessage\")",
-            "Error: Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by \"messages.ExportChatInvite\")"
+            'pyrogram.errors.exceptions.forbidden_403.ChatWriteForbidden: Telegram says: [403 CHAT_WRITE_FORBIDDEN] - You don\'t have rights to send messages in this chat (caused by "messages.SendMessage")',
+            'Error: Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.ExportChatInvite")',
         ]
-        
+
         # Return False if the log message matches any of the specified error messages, thus excluding it from logging
         return not any(err in record.getMessage() for err in error_messages_to_ignore)
+
 
 # Add this custom filter to your logging configuration
 logging.basicConfig(
@@ -20,5 +22,5 @@ logging.basicConfig(
         logging.FileHandler("log.txt"),
         logging.StreamHandler(),
     ],
-    filters=[IgnoreSpecificErrors()]
-        )
+    filters=[IgnoreSpecificErrors()],
+)
