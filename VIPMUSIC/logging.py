021029b9dcd606d84c47522e006214cc1b46a73e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-10 08:48:05 +0530

    Update logging.py

--- VIPMUSIC/logging.py
+++ VIPMUSIC/logging.py
@@ -1,13 +1,12 @@
 import logging
 
-
-# Custom logging filter to ignore specific messages
-class IgnoreChatAdminRequired(logging.Filter):
+# Define a custom logging filter to ignore specific messages
+class IgnoreSpecificErrors(logging.Filter):
     def filter(self, record):
-        return "ChatAdminRequired" not in record.getMessage()
+        # Return False if the log message contains "ChatAdminRequired" or "ChannelPrivate", thus excluding them from logging
+        return not any(err in record.getMessage() for err in ["ChatAdminRequired", "ChannelPrivate"])
 
-
-# Configure logging
+# Configure logging with basic settings
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -18,17 +17,25 @@ logging.basicConfig(
     ],
 )
 
-# Create a filter instance
-filter = IgnoreChatAdminRequired()
+# Create an instance of the custom filter
+filter = IgnoreSpecificErrors()
 
-# Apply the filter to relevant loggers
-loggers_to_filter = ["httpx", "pyrogram", "pytgcalls", "pymongo", "ntgcalls"]
+# List of loggers to apply the filter and set the logging level to ERROR
+loggers_to_filter = [
+    "httpx",
+    "pyrogram",
+    "pytgcalls",
+    "pymongo",
+    "ntgcalls"
+]
 
+# Apply the filter and set the logging level for each specified logger
 for logger_name in loggers_to_filter:
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.ERROR)
-    logger.addFilter(filter)
+    logger.setLevel(logging.ERROR)  # Set the logging level to ERROR
+    logger.addFilter(filter)  # Add the custom filter to the logger
 
-
+# Function to get a logger by name
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+    
