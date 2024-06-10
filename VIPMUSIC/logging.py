a77e32e174d470f8a83496a5da814f5720c3ad7e
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-10 03:18:24 +0000

    Pylint: Auto Fixes

--- VIPMUSIC/logging.py
+++ VIPMUSIC/logging.py
@@ -1,10 +1,15 @@
 import logging
 
+
 # Define a custom logging filter to ignore specific messages
 class IgnoreSpecificErrors(logging.Filter):
     def filter(self, record):
         # Return False if the log message contains "ChatAdminRequired" or "ChannelPrivate", thus excluding them from logging
-        return not any(err in record.getMessage() for err in ["ChatAdminRequired", "ChannelPrivate"])
+        return not any(
+            err in record.getMessage()
+            for err in ["ChatAdminRequired", "ChannelPrivate"]
+        )
+
 
 # Configure logging with basic settings
 logging.basicConfig(
@@ -21,13 +26,7 @@ logging.basicConfig(
 filter = IgnoreSpecificErrors()
 
 # List of loggers to apply the filter and set the logging level to ERROR
-loggers_to_filter = [
-    "httpx",
-    "pyrogram",
-    "pytgcalls",
-    "pymongo",
-    "ntgcalls"
-]
+loggers_to_filter = ["httpx", "pyrogram", "pytgcalls", "pymongo", "ntgcalls"]
 
 # Apply the filter and set the logging level for each specified logger
 for logger_name in loggers_to_filter:
@@ -35,7 +34,7 @@ for logger_name in loggers_to_filter:
     logger.setLevel(logging.ERROR)  # Set the logging level to ERROR
     logger.addFilter(filter)  # Add the custom filter to the logger
 
+
 # Function to get a logger by name
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-    
