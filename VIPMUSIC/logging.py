cf0cd3a29d6aa3145b0adf0f78589c5c8b5cec9b
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-10 03:13:02 +0000

    Pylint: Auto Fixes

--- VIPMUSIC/logging.py
+++ VIPMUSIC/logging.py
@@ -1,9 +1,11 @@
 import logging
 
+
 # Custom logging filter to ignore specific messages
 class IgnoreChatAdminRequired(logging.Filter):
     def filter(self, record):
         return "ChatAdminRequired" not in record.getMessage()
+
 
 # Configure logging
 logging.basicConfig(
@@ -20,18 +22,13 @@ logging.basicConfig(
 filter = IgnoreChatAdminRequired()
 
 # Apply the filter to relevant loggers
-loggers_to_filter = [
-    "httpx",
-    "pyrogram",
-    "pytgcalls",
-    "pymongo",
-    "ntgcalls"
-]
+loggers_to_filter = ["httpx", "pyrogram", "pytgcalls", "pymongo", "ntgcalls"]
 
 for logger_name in loggers_to_filter:
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.ERROR)
     logger.addFilter(filter)
+
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
