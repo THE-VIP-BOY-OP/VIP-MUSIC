5b1eaa6afb7888f5a6611ffd43746aef2bd413a6
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-10 09:28:17 +0530

    Update log_ignore.py

--- VIPMUSIC/utils/log_ignore.py
+++ VIPMUSIC/utils/log_ignore.py
@@ -1,17 +1,16 @@
 import logging
 
-
 class IgnoreSpecificErrors(logging.Filter):
     def filter(self, record):
         # Specify the error messages to ignore
         error_messages_to_ignore = [
-            'pyrogram.errors.exceptions.forbidden_403.ChatWriteForbidden: Telegram says: [403 CHAT_WRITE_FORBIDDEN] - You don\'t have rights to send messages in this chat (caused by "messages.SendMessage")',
-            'Error: Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by "messages.ExportChatInvite")',
+            "pyrogram.errors.exceptions.forbidden_403.ChatWriteForbidden: Telegram says: [403 CHAT_WRITE_FORBIDDEN] - You don't have rights to send messages in this chat (caused by \"messages.SendMessage\")",
+            "Error: Telegram says: [400 CHAT_ADMIN_REQUIRED] - The method requires chat admin privileges (caused by \"messages.ExportChatInvite\")",
+            "pyrogram.errors.exceptions.not_acceptable_406.ChannelPrivate: Telegram says: [406 CHANNEL_PRIVATE] - The channel/supergroup is not accessible (caused by \"channels.GetFullChannel\")"
         ]
-
+        
         # Return False if the log message matches any of the specified error messages, thus excluding it from logging
         return not any(err in record.getMessage() for err in error_messages_to_ignore)
-
 
 # Add this custom filter to your logging configuration
 logging.basicConfig(
@@ -22,5 +21,5 @@ logging.basicConfig(
         logging.FileHandler("log.txt"),
         logging.StreamHandler(),
     ],
-    filters=[IgnoreSpecificErrors()],
-)
+    filters=[IgnoreSpecificErrors()]
+        )
