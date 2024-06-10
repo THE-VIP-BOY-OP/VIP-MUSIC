dbf260a16a886e5633412e96f06709d97a31d449
THE-VIP-BOY-OP <THE-VIP-BOY-OP@users.noreply.github.com>
2024-06-10 03:52:55 +0000

    Pylint: Auto Fixes

--- VIPMUSIC/utils/log_ignore.py
+++ VIPMUSIC/utils/log_ignore.py
@@ -1,12 +1,14 @@
 import logging
 
+
 class IgnoreSpecificErrors(logging.Filter):
     def filter(self, record):
         # Specify the error message to ignore
-        error_message_to_ignore = "pyrogram.errors.exceptions.forbidden_403.ChatWriteForbidden: Telegram says: [403 CHAT_WRITE_FORBIDDEN] - You don't have rights to send messages in this chat (caused by \"messages.SendMessage\")"
-        
+        error_message_to_ignore = 'pyrogram.errors.exceptions.forbidden_403.ChatWriteForbidden: Telegram says: [403 CHAT_WRITE_FORBIDDEN] - You don\'t have rights to send messages in this chat (caused by "messages.SendMessage")'
+
         # Return False if the log message matches the specified error message, thus excluding it from logging
         return error_message_to_ignore not in record.getMessage()
+
 
 # Add this custom filter to your logging configuration
 logging.basicConfig(
@@ -17,5 +19,5 @@ logging.basicConfig(
         logging.FileHandler("log.txt"),
         logging.StreamHandler(),
     ],
-    filters=[IgnoreSpecificErrors()]
+    filters=[IgnoreSpecificErrors()],
 )
