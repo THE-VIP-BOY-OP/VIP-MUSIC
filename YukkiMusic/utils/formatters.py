08bba92dde22b955965444d527494f0d1791180d
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-01 00:34:26 +0530

    Update formatters.py

--- YukkiMusic/utils/formatters.py
+++ YukkiMusic/utils/formatters.py
@@ -149,7 +149,67 @@ def seconds_to_min(seconds):
         elif s > 0:
             return "00:{:02d}".format(s)
     return "-"
+def speed_converter(seconds, speed):
+    if str(speed) == str("0.5"):
+        seconds = seconds * 2
+    if str(speed) == str("0.75"):
+        seconds = seconds + ((50 * seconds) // 100)
+    if str(speed) == str("1.5"):
+        seconds = seconds - ((25 * seconds) // 100)
+    if str(speed) == str("2.0"):
+        seconds = seconds - ((50 * seconds) // 100)
+    collect = seconds
+    if seconds is not None:
+        seconds = int(seconds)
+        d, h, m, s = (
+            seconds // (3600 * 24),
+            seconds // 3600 % 24,
+            seconds % 3600 // 60,
+            seconds % 3600 % 60,
+        )
+        if d > 0:
+            convert = "{:02d}:{:02d}:{:02d}:{:02d}".format(d, h, m, s)
+            return convert, collect
+        elif h > 0:
+            convert = "{:02d}:{:02d}:{:02d}".format(h, m, s)
+            return convert, collect
+        elif m > 0:
+            convert = "{:02d}:{:02d}".format(m, s)
+            return convert, collect
+        elif s > 0:
+            convert = "00:{:02d}".format(s)
+            return convert, collect
+    return "-"
 
+
+def check_duration(file_path):
+    command = [
+        "ffprobe",
+        "-loglevel",
+        "quiet",
+        "-print_format",
+        "json",
+        "-show_format",
+        "-show_streams",
+        file_path,
+    ]
+
+    pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, err = pipe.communicate()
+    _json = json.loads(out)
+
+    if "format" in _json:
+        if "duration" in _json["format"]:
+            return float(_json["format"]["duration"])
+
+    if "streams" in _json:
+        for s in _json["streams"]:
+            if "duration" in s:
+                return float(s["duration"])
+
+    return "Unknown"
+
+                
 
 formats = [
     "webm",
