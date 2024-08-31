cf2aacd0632d14b8b86b5a5b602eca341b4c62cb
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-01 02:59:07 +0530

    Update pause.py

--- YukkiMusic/plugins/admins/pause.py
+++ YukkiMusic/plugins/admins/pause.py
@@ -1,34 +1,48 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
-from strings import get_command
 from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
+from YukkiMusic.core.call import Yukki as VIP
 from YukkiMusic.utils.database import is_music_playing, music_off
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
-# Commands
-PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
-
-@app.on_message(filters.command(PAUSE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
-        return await message.reply_text(_["general_2"])
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Yukki.pause_stream(chat_id)
-    await message.reply_text(_["admin_2"].format(message.from_user.mention))
+    await VIP.pause_stream(chat_id)
+
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="ʀᴇᴘʟᴀʏ", callback_data=f"ADMIN Replay|{chat_id}"
+            ),
+        ],
+    ]
+
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.mention),
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
+
+
+__MODULE__ = "Pause"
+__HELP__ = """
+**Pause Music**
+
+This module allows administrators to pause the music playback in the group.
+
+Commands:
+- /pause: Pause the music playback in groups.
+- /cpause: Pause the music playback in channels.
+
+Note:
+- Only administrators can use these commands.
+"""
