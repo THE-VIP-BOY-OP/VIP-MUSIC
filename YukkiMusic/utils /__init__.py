76ea586e9ed50ee0e58c1314e5d17263781b7bd5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-08-31 20:12:18 +0530

    Create __init__.py

---
+++ YukkiMusic/utils /__init__.py
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
