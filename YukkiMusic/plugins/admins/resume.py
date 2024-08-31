f2dedb6bdc508f3ff9dc27f552831f8303bfb662
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-01 03:00:28 +0530

    Update resume.py

--- YukkiMusic/plugins/admins/resume.py
+++ YukkiMusic/plugins/admins/resume.py
@@ -1,34 +1,48 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
-from strings import get_command
 from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
+from YukkiMusic.core.call import Yukki as VIP
 from YukkiMusic.utils.database import is_music_playing, music_on
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
-# Commands
-RESUME_COMMAND = get_command("RESUME_COMMAND")
 
-
-@app.on_message(filters.command(RESUME_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
-        return await message.reply_text(_["general_2"])
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Yukki.resume_stream(chat_id)
-    await message.reply_text(_["admin_4"].format(message.from_user.mention))
+    await VIP.resume_stream(chat_id)
+    buttons_resume = [
+        [
+            InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴘᴀᴜsᴇ",
+                callback_data=f"ADMIN Pause|{chat_id}",
+            ),
+        ],
+    ]
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.mention),
+        reply_markup=InlineKeyboardMarkup(buttons_resume),
+    )
+
+
+__MODULE__ = "Resume"
+__HELP__ = """
+**Resume**
+
+This module allows administrators to resume playback of the currently paused track.
+
+Commands:
+- /resume: Resumes playback of the currently paused track for group.
+- /cresume: Resumes playback of the currently paused track for channel.
+
+Note:
+- Only administrators can use these commands.
+"""
