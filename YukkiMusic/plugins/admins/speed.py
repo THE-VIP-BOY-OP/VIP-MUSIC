bc789549bd4162a147e24d8138c3cf7745ee9c44
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-01 00:01:06 +0530

    Update speed.py

--- YukkiMusic/plugins/admins/speed.py
+++ YukkiMusic/plugins/admins/speed.py
@@ -25,14 +25,14 @@ async def playback(cli, message: Message, _, chat_id):
         return await app.send_message(message.chat.id, text=_["queue_2"])
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
-        return await app.send_message(message.chat.id, text=_["admin_27"])
+        return await app.send_message(message.chat.id, text=_["speed_1"])
     file_path = playing[0]["file"]
     if "downloads" not in file_path:
-        return await app.send_message(message.chat.id, text=_["admin_27"])
+        return await app.send_message(message.chat.id, text=_["speed_1"])
     upl = speed_markup(_, chat_id)
     return await app.send_message(
         message.chat.id,
-        text=_["admin_28"].format(app.mention),
+        text=_["speed_2"].format(app.mention),
         reply_markup=upl,
     )
 
@@ -60,29 +60,29 @@ async def del_back_playlist(client, callback_query, _):
         return await callback_query.answer(_["queue_2"], show_alert=True)
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
-        return await callback_query.answer(_["admin_27"], show_alert=True)
+        return await callback_query.answer(_["speed_1"], show_alert=True)
     file_path = playing[0]["file"]
     if "downloads" not in file_path:
-        return await callback_query.answer(_["admin_27"], show_alert=True)
+        return await callback_query.answer(_["speed_1"], show_alert=True)
     checkspeed = playing[0].get("speed")
     if checkspeed:
         if str(checkspeed) == str(speed):
             if str(speed) == str("1.0"):
-                return await callback_query.answer(_["admin_29"], show_alert=True)
+                return await callback_query.answer(_["speed_3"], show_alert=True)
     else:
         if str(speed) == str("1.0"):
-            return await callback_query.answer(_["admin_29"], show_alert=True)
+            return await callback_query.answer(_["speed_3"], show_alert=True)
     if chat_id in checker:
-        return await callback_query.answer(_["admin_30"], show_alert=True)
+        return await callback_query.answer(_["speed_4"], show_alert=True)
     else:
         checker.append(chat_id)
     try:
-        await callback_query.answer(_["admin_31"])
+        await callback_query.answer(_["speed_5"])
     except:
         pass
     mystic = await app.send_message(
         callback_query.message.chat.id,
-        text=_["admin_32"].format(callback_query.from_user.mention),
+        text=_["speed_6"].format(callback_query.from_user.mention),
     )
     try:
         await Yukki.speedup_stream(
@@ -96,14 +96,14 @@ async def del_back_playlist(client, callback_query, _):
             checker.remove(chat_id)
         return await app.send_message(
             callback_query.message.chat.id,
-            text=_["admin_33"],
+            text=_["speed_7"],
             reply_markup=close_markup(_),
         )
     if chat_id in checker:
         checker.remove(chat_id)
     await app.send_message(
         callback_query.message.chat.id,
-        text=_["admin_34"].format(speed, callback_query.from_user.mention),
+        text=_["speed_8"].format(speed, callback_query.from_user.mention),
         reply_markup=close_markup(_),
     )
 
