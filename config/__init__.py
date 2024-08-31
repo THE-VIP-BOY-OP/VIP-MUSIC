da8066337e86ab4b8bd24a716a475570129af8cc
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-08-31 20:25:55 +0530

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .config import *
