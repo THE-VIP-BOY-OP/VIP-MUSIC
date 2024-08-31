13ce89a7b9baff23cfe85e7473d8bede10ecd686
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-31 19:08:47 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM nikolaik/python-nodejs:python3.10-nodejs19
+
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends ffmpeg aria2 \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY . /app/
+WORKDIR /app/
+RUN python -m pip install --no-cache-dir --upgrade pip
+RUN pip3 install --no-cache-dir --upgrade --requirement requirements.txt
+
+CMD python3 -m YukkiMusic
---
+++ genstring.py
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+
+import asyncio
+
+from pyrogram import Client as c
+
+API_ID = input("\nEnter Your API_ID:\n > ")
+API_HASH = input("\nEnter Your API_HASH:\n > ")
+
+i = c("yukkistring", in_memory=True, api_id=API_ID, api_hash=API_HASH)
+
+
+async def main():
+    await i.start()
+    ss = await i.export_session_string()
+    print("\nHERE IS YOUR STRING SESSION, COPY IT, DON'T SHARE!!\n")
+    print(f"\n{ss}\n")
+    print("\n STRING GENERATED\n")
+    xx = f"HERE IS YOUR STRING SESSION, COPY IT, DON'T SHARE!!\n\n`{ss}`\n\n STRING GENERATED"
+    try:
+        await i.send_message("me", xx)
+    except BaseException:
+        pass
+
+
+asyncio.run(main())
