3b46ef46919751de4e0bc37147813577077fe1e7
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-08-31 20:14:25 +0530

    Create channelplay.py

---
+++ YukkiMusic/utils /channelplay.py
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+
+from YukkiMusic import app
+from YukkiMusic.utils.database import get_cmode
+
+
+async def get_channeplayCB(_, command, CallbackQuery):
+    if command == "c":
+        chat_id = await get_cmode(CallbackQuery.message.chat.id)
+        if chat_id is None:
+            try:
+                return await CallbackQuery.answer(_["setting_12"], show_alert=True)
+            except:
+                return
+        try:
+            chat = await app.get_chat(chat_id)
+            channel = chat.title
+        except:
+            try:
+                return await CallbackQuery.answer(_["cplay_4"], show_alert=True)
+            except:
+                return
+    else:
+        chat_id = CallbackQuery.message.chat.id
+        channel = None
+    return chat_id, channel
