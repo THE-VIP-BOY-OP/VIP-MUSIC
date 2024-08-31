79dd5907a7c5ceadf93013f2611e3872f3624f8f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-08-31 21:21:50 +0530

    Add files via upload

---
+++ YukkiMusic/utils/database/__init__.py
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+
+from .assistantdatabase import *
+from .memorydatabase import *
+from .mongodatabase import *
