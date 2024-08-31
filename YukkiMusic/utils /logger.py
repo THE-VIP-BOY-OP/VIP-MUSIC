104099ace013cba3a7a9495a6c651f5416a52e6c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-08-31 20:18:22 +0530

    Create logger.py

---
+++ YukkiMusic/utils /logger.py
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from config import LOG, LOG_GROUP_ID
+from YukkiMusic import app
+from YukkiMusic.utils.database import is_on_off
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(LOG):
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "ᴘʀɪᴠᴀᴛᴇ ɢʀᴏᴜᴘ"
+
+        logger_text = f"""
+**{app.mention} ᴘʟᴀʏ ʟᴏɢ**
+
+**ᴄʜᴀᴛ ɪᴅ :** `{message.chat.id}`
+**ᴄʜᴀᴛ ɴᴀᴍᴇ :** {message.chat.title}
+**ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :** {chatusername}
+
+**ᴜsᴇʀ ɪᴅ :** `{message.from_user.id}`
+**ɴᴀᴍᴇ :** {message.from_user.mention}
+**ᴜsᴇʀɴᴀᴍᴇ :** @{message.from_user.username}
+
+**ǫᴜᴇʀʏ :** {message.text.split(None, 1)[1]}
+**sᴛʀᴇᴀᴍᴛʏᴘᴇ :** {streamtype}"""
+        if message.chat.id != LOG_GROUP_ID:
+            try:
+                await app.send_message(
+                    chat_id=LOG_GROUP_ID,
+                    text=logger_text,
+                    disable_web_page_preview=True,
+                )
+            except Exception as e:
+                print(e)
+        return
