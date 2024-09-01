5fdde271781f8a0475ef117f6258a67a7f86deba
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-01 23:40:30 +0530

    Update and rename rstats.py to real_stats.py

--- VIPMUSIC/plugins/tools/real_stats.py
+++ VIPMUSIC/plugins/tools/real_stats.py
@@ -18,7 +18,9 @@ async def get_served_chats() -> list:
     async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
         chats_list.append(chat)
     return chats_list
-
+    
+async def delete_served_chat(chat_id: int):
+    await chatsdb.delete_one({"chat_id": chat_id})
 
 @app.on_message(filters.command(["rstats", "allstats"]) & SUDOERS)
 async def all_stats(client, message: Message):
