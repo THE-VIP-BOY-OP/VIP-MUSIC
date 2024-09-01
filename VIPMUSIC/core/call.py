069de9ecb172c22d8cd7973d3fae23430b3613d4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-02 05:09:34 +0530

    Update call.py

--- VIPMUSIC/core/call.py
+++ VIPMUSIC/core/call.py
@@ -71,7 +71,7 @@ async def _clear_(chat_id):
     db[chat_id] = []
     await remove_active_video_chat(chat_id)
     await remove_active_chat(chat_id)
-    AMBOT = await app.send_message(chat_id, f"Pls Play New Song/Next SOng")
+    AMBOT = await app.send_message(chat_id, f"**ꜱᴏɴɢ ʜᴀꜱ ʙᴇᴇɴ ᴇɴᴅᴇᴅ. ʟᴇᴀᴠɪɴɢ ᴠᴄ..**")
     await asyncio.sleep(10)
     await AMBOT.delete()
 
