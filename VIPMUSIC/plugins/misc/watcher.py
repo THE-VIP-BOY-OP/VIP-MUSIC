d3c0bfa3407f1a0b86f6e74acb4d7198d9d3e2f4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-02 12:56:52 +0530

    Update and rename watcher to watcher.py

--- VIPMUSIC/plugins/misc/watcher.py
+++ VIPMUSIC/plugins/misc/watcher.py
@@ -15,13 +15,13 @@ from VIPMUSIC import app
 from VIPMUSIC.core.call import VIP
 
 
-@app.on_message(filters.video_chat_started, group=20)
-@app.on_message(filters.video_chat_ended, group=30)
+@app.on_message(filters.video_chat_started, group=21)
+@app.on_message(filters.video_chat_ended, group=31)
 @app.on_message(filters.left_chat_member)
 async def force_stop_stream(_, message: Message):
     try:
         if message.left_chat_member and not message.left_chat_member is None:
-            if message.left_chat_member.id == (await get_assistant(message.chat.id)).id:
+            if message.left_chat_member.id == (await get_assistant(message.chat.id)):
                 return await VIP.force_stop_stream(message.chat.id)
         await VIP.force_stop_stream(message.chat.id)
     except Exception as e:
