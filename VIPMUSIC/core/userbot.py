c7f25a1ec7202ac9bfd45577e53b209aca47dba8
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-02 05:48:56 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -70,7 +70,7 @@ class Userbot(Client):
                 LOGGER(__name__).error(
                     f"Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
-                sys.exit()
+                
             get_me = await self.one.get_me()
             self.one.username = get_me.username
             self.one.id = get_me.id
@@ -124,7 +124,7 @@ class Userbot(Client):
                 LOGGER(__name__).error(
                     f"Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
-                sys.exit()
+                #sys.exit()
             get_me = await self.three.get_me()
             self.three.username = get_me.username
             self.three.id = get_me.id
@@ -151,7 +151,7 @@ class Userbot(Client):
                 LOGGER(__name__).error(
                     f"Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
-                sys.exit()
+                #sys.exit()
             get_me = await self.four.get_me()
             self.four.username = get_me.username
             self.four.id = get_me.id
@@ -178,7 +178,7 @@ class Userbot(Client):
                 LOGGER(__name__).error(
                     f"Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
-                sys.exit()
+                #sys.exit()
             get_me = await self.five.get_me()
             self.five.username = get_me.username
             self.five.id = get_me.id
