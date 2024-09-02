a2f3a9568a045b601541204349ebdc615864fb5d
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-02 13:42:45 +0530

    Update genstring.py

--- genstring.py
+++ genstring.py
@@ -15,7 +15,7 @@ from pyrogram import Client as c
 API_ID = input("\nEnter Your API_ID:\n > ")
 API_HASH = input("\nEnter Your API_HASH:\n > ")
 
-i = c("yukkistring", in_memory=True, api_id=API_ID, api_hash=API_HASH)
+i = c("vipstring", in_memory=True, api_id=API_ID, api_hash=API_HASH)
 
 
 async def main():
