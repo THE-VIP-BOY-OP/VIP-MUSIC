627f9a1bdc8e586c7379116f78b515202681cafa
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-03 12:58:56 +0530

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -64,7 +64,7 @@ async def ban_new(client, message):
 async def start_comm(client, message: Message, _):
     chat_id = message.chat.id
     await add_served_user(message.from_user.id)
-    await message.react("✨")
+    await message.react("🕊️")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
