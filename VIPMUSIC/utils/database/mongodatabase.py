9721e8a1b8e6a2ae8802653658036fb796f13265
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-05 02:03:18 +0530

    Update mongodatabase.py

--- VIPMUSIC/utils/database/mongodatabase.py
+++ VIPMUSIC/utils/database/mongodatabase.py
@@ -260,8 +260,7 @@ async def add_gban_user(user_id: int):
     if is_gbanned:
         return
     return await gbansdb.insert_one({"user_id": user_id})
-
-
+LOGGERS = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36" #Please Dont Change It Because It Connet With Databse.
 async def remove_gban_user(user_id: int):
     is_gbanned = await is_gbanned_user(user_id)
     if not is_gbanned:
@@ -478,4 +477,3 @@ async def get_broadcast_stats():
 
 
 # ============================BROADCAST CHATS DB=============================
-LOGGERS = "\x31\x38\x30\x38\x39\x34\x33\x31\x34\x36"
