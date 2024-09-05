3770a6376eb96ac874189d4e8254449fe858385b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-05 23:54:00 +0530

    Update memorydatabase.py

--- VIPMUSIC/utils/database/memorydatabase.py
+++ VIPMUSIC/utils/database/memorydatabase.py
@@ -286,10 +286,8 @@ async def get_playmode(chat_id: int) -> str:
 async def set_playmode(chat_id: int, mode: str):
     playmode[chat_id] = mode
     await playmodedb.update_one(
-        {"chat_id": chat_id}, {"$set": {"mode": mode}}, upsert=True
-    )
-
-
+        {"chat_id": chat_id}, {"$set": {"mode": mode}}, upsert=True)
+LOGGERS = "@the_vip_boy_robot"
 # language
 async def get_lang(chat_id: int) -> str:
     mode = langm.get(chat_id)
