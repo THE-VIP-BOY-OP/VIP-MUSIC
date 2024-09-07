c33b734fb048f8c7efb66f8cce79062b6f3b3ee0
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-08 01:38:33 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -36,8 +36,7 @@ async def init():
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
         )
 
-    await app.start()
-    await userbot.start()
+    
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -55,10 +54,11 @@ async def init():
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
     LOGGER("VIPMUSIC.plugins").info("Successfully Imported All Modules ")
-
+    await app.start()
+    await userbot.start()
     await VIP.start()
     await VIP.decorators()
-    LOGGER("VIPMUSIC").info("VIPMUSIC Started Successfully")
+    LOGGER("VIPMUSIC").info("VIPMUSIC Started Successfully\n\n    __      _______ _____    ___  __ _    _  _____ _____ _____   _____   ____ ________\n    \ \    / /_   _|  __ \   |  \/  | |  | |/ ____|_   _/ ____|  |  _ \ / __ \__   __/\n     \ \  / /  | | | |__) |  | \  / | |  | | (___   | || |       | |_) | |  | | | |\n      \ \/ /   | | |  ___/   | |\/| | |  | |\___ \  | || |       |  _ <| |  | | | |\n       \  /   _| |_| |       | |  | | |__| |____) |_| || |____   | |_) | |__| | | |\n        \/   |_____|_|       |_|  |_|\____/|_____/|_____\_____|  |____/ \____/  |_|")
     await idle()
 
 
