e6bbb2e82f03961aaf4263dc580a6fc2237b0634
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-08 10:33:47 +0530

    Update bot.py

--- VIPMUSIC/core/bot.py
+++ VIPMUSIC/core/bot.py
@@ -11,19 +11,12 @@ import uvloop
 
 uvloop.install()
 
-
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
-from pyrogram.types import (
-    BotCommand,
-    BotCommandScopeAllChatAdministrators,
-    BotCommandScopeAllGroupChats,
-    BotCommandScopeAllPrivateChats,
-)
-
+from pyrogram.types import BotCommand, BotCommandScopeAllChatAdministrators, BotCommandScopeAllGroupChats, BotCommandScopeAllPrivateChats
 import config
-
 from ..logging import LOGGER
+import sys
 
 
 class VIPBot(Client):
@@ -39,8 +32,9 @@ class VIPBot(Client):
     async def start(self):
         await super().start()
         get_me = await self.get_me()
-        self.username = get_me.username
-        self.id = get_me.id
+        self.me = get_me
+        self.username = self.me.username
+        self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.mention = self.me.mention
 
@@ -49,74 +43,78 @@ class VIPBot(Client):
                 config.LOG_GROUP_ID,
                 f"╔═══❰𝐖𝐄𝐋𝐂𝐎𝐌𝐄❱═══❍⊱❁۪۪\n║\n║┣⪼🥀𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐁𝐚𝐛𝐲🎉\n║\n║◈ {config.MUSIC_BOT_NAME}\n║\n║┣⪼🎈𝐈𝐃:- `{self.id}` \n║\n║┣⪼🎄@{self.username} \n║ \n║┣⪼💖𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠😍\n║\n╚══════════════❍⊱❁",
             )
-        except:
-            LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
-            )
-            # sys.exit()
-        if config.SET_CMDS == str(True):
-            try:
+        except pyrogram.errors.ChatWriteForbidden as e:
+            LOGGER(__name__).error(f"Bot cannot write to the log group: {e}")
+            sys.exit("Bot cannot access the log group.")
 
+        if config.SET_CMDS:
+            try:
+                # Set commands for private chats
                 await self.set_bot_commands(
                     commands=[
-                        BotCommand("start", "sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ"),
-                        BotCommand("help", "ɢᴇᴛ ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ"),
-                        BotCommand("ping", "ᴄʜᴇᴄᴋ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ"),
+                        BotCommand("start", "Start the bot"),
+                        BotCommand("help", "Get the help menu"),
+                        BotCommand("ping", "Check if the bot is alive or dead"),
                     ],
                     scope=BotCommandScopeAllPrivateChats(),
                 )
+
+                # Set commands for group chats
                 await self.set_bot_commands(
                     commands=[
-                        BotCommand("play", "sᴛᴀʀᴛ ᴘʟᴀʏɪɴɢ ʀᴇǫᴜᴇᴛᴇᴅ sᴏɴɢ"),
+                        BotCommand("play", "Start playing requested song"),
+                        BotCommand("stop", "Stop the current song"),
+                        BotCommand("pause", "Pause the current song"),
+                        BotCommand("resume", "Resume the paused song"),
+                        BotCommand("queue", "Check the queue of songs"),
+                        BotCommand("skip", "Skip the current song"),
+                        BotCommand("volume", "Adjust the music volume"),
+                        BotCommand("lyrics", "Get lyrics of the song"),
                     ],
                     scope=BotCommandScopeAllGroupChats(),
                 )
+
+                # Set commands for chat administrators
                 await self.set_bot_commands(
                     commands=[
-                        BotCommand("start", "❥ ✨ᴛᴏ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ✨"),
-                        BotCommand("ping", "❥ 🍁ᴛᴏ ᴄʜᴇᴄᴋ ᴛʜᴇ ᴘɪɴɢ🍁"),
-                        BotCommand("help", "❥ 🥺ᴛᴏ ɢᴇᴛ ʜᴇʟᴘ🥺"),
-                        BotCommand("vctag", "❥ 😇ᴛᴀɢᴀʟʟ ғᴏʀ ᴠᴄ🙈"),
-                        BotCommand("stopvctag", "❥ 📍sᴛᴏᴘ ᴛᴀɢᴀʟʟ ғᴏʀ ᴠᴄ 💢"),
-                        BotCommand("tagall", "❥ 🔻ᴛᴀɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs ʙʏ ᴛᴇxᴛ🔻"),
-                        BotCommand("cancel", "❥ 🔻ᴄᴀɴᴄᴇʟ ᴛʜᴇ ᴛᴀɢɢɪɴɢ🔻"),
-                        BotCommand("settings", "❥ 🔻ᴛᴏ ɢᴇᴛ ᴛʜᴇ sᴇᴛᴛɪɴɢs🔻"),
-                        BotCommand("reload", "❥ 🪐ᴛᴏ ʀᴇʟᴏᴀᴅ ᴛʜᴇ ʙᴏᴛ🪐"),
-                        BotCommand("play", "❥ ❣️ᴛᴏ ᴘʟᴀʏ ᴛʜᴇ sᴏɴɢ❣️"),
-                        BotCommand("vplay", "❥ ❣️ᴛᴏ ᴘʟᴀʏ ᴛʜᴇ ᴍᴜsɪᴄ ᴡɪᴛʜ ᴠɪᴅᴇᴏ❣️"),
-                        BotCommand("pause", "❥ 🥀ᴛᴏ ᴘᴀᴜsᴇ ᴛʜᴇ sᴏɴɢs🥀"),
-                        BotCommand("resume", "❥ 💖ᴛᴏ ʀᴇsᴜᴍᴇ ᴛʜᴇ sᴏɴɢ💖"),
-                        BotCommand("end", "❥ 🐚ᴛᴏ ᴇᴍᴘᴛʏ ᴛʜᴇ ϙᴜᴇᴜᴇ🐚"),
-                        BotCommand("queue", "❥ 🤨ᴛᴏ ᴄʜᴇᴄᴋ ᴛʜᴇ ϙᴜᴇᴜᴇ🤨"),
-                        BotCommand("playlist", "❥ 🕺ᴛᴏ ɢᴇᴛ ᴛʜᴇ ᴘʟᴀʏʟɪsᴛ🕺"),
-                        BotCommand("stop", "❥ ❤‍🔥ᴛᴏ sᴛᴏᴘ ᴛʜᴇ sᴏɴɢs❤‍🔥"),
-                        BotCommand("lyrics", "❥ 🕊️ᴛᴏ ɢᴇᴛ ᴛʜᴇ ʟʏʀɪᴄs🕊️"),
-                        BotCommand("song", "❥ 🔸ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ sᴏɴɢ🔸"),
-                        BotCommand("video", "❥ 🔸ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ᴠɪᴅᴇᴏ sᴏɴɢ🔸"),
-                        BotCommand("gali", "❥ 🔻ᴛᴏ ʀᴇᴘʟʏ ғᴏʀ ғᴜɴ🔻"),
-                        BotCommand("shayri", "❥ 🔻ᴛᴏ ɢᴇᴛ ᴀ sʜᴀʏᴀʀɪ🔻"),
-                        BotCommand("love", "❥ 🔻ᴛᴏ ɢᴇᴛ ᴀ ʟᴏᴠᴇ sʜᴀʏᴀʀɪ🔻"),
-                        BotCommand("sudolist", "❥ 🌱ᴛᴏ ᴄʜᴇᴄᴋ ᴛʜᴇ sᴜᴅᴏʟɪsᴛ🌱"),
-                        BotCommand("owner", "❥ 💝ᴛᴏ ᴄʜᴇᴄᴋ ᴛʜᴇ ᴏᴡɴᴇʀ💝"),
-                        BotCommand("update", "❥ 🐲ᴛᴏ ᴜᴘᴅᴀᴛᴇ ʙᴏᴛ🐲"),
-                        BotCommand("gstats", "❥ 💘ᴛᴏ sᴛᴀᴛs ᴏғ ᴛʜᴇ ʙᴏᴛ💘"),
-                        BotCommand("repo", "❥ 🍌ᴛᴏ ᴄʜᴇᴄᴋ ᴛʜᴇ 𝚁𝙴𝙿𝙾🍌"),
+                        BotCommand("start", "❥ Start the bot"),
+                        BotCommand("ping", "❥ Check the ping"),
+                        BotCommand("help", "❥ Get help"),
+                        BotCommand("vctag", "❥ Tag all for voice chat"),
+                        BotCommand("stopvctag", "❥ Stop tagging for VC"),
+                        BotCommand("tagall", "❥ Tag all members by text"),
+                        BotCommand("cancel", "❥ Cancel the tagging"),
+                        BotCommand("settings", "❥ Get the settings"),
+                        BotCommand("reload", "❥ Reload the bot"),
+                        BotCommand("play", "❥ Play the requested song"),
+                        BotCommand("vplay", "❥ Play video along with music"),
+                        BotCommand("end", "❥ Empty the queue"),
+                        BotCommand("playlist", "❥ Get the playlist"),
+                        BotCommand("stop", "❥ Stop the song"),
+                        BotCommand("lyrics", "❥ Get the song lyrics"),
+                        BotCommand("song", "❥ Download the requested song"),
+                        BotCommand("video", "❥ Download the requested video song"),
+                        BotCommand("gali", "❥ Reply with fun"),
+                        BotCommand("shayri", "❥ Get a shayari"),
+                        BotCommand("love", "❥ Get a love shayari"),
+                        BotCommand("sudolist", "❥ Check the sudo list"),
+                        BotCommand("owner", "❥ Check the owner"),
+                        BotCommand("update", "❥ Update bot"),
+                        BotCommand("gstats", "❥ Get stats of the bot"),
+                        BotCommand("repo", "❥ Check the repo"),
                     ],
                     scope=BotCommandScopeAllChatAdministrators(),
                 )
-            except:
-                pass
-        else:
-            pass
-        try:
-            a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-            if a.status != ChatMemberStatus.ADMINISTRATOR:
-                LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+            except Exception as e:
+                LOGGER(__name__).error(f"Failed to set bot commands: {e}")
 
-        except Exception:
-            pass
-        if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
-        else:
-            self.name = get_me.first_name
+        try:
+            chat_member_info = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+            if chat_member_info.status != ChatMemberStatus.ADMINISTRATOR:
+                LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+        except Exception as e:
+            LOGGER(__name__).error(f"Error occurred while checking bot status: {e}")
+
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+
+# You can add the run or the main part here if needed.
