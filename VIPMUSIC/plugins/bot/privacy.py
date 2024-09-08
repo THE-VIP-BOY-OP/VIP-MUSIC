d10a7ac884dfff6719141ecc56266ac8a1e40491
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-08 09:23:05 +0530

    Update privacy.py

--- VIPMUSIC/plugins/bot/privacy.py
+++ VIPMUSIC/plugins/bot/privacy.py
@@ -5,20 +5,20 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import config
 from VIPMUSIC import app
 
-TEXT = f"""
-🔒 **Privacy Policy for {app.me.mention} !**
-
-Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: [Privacy Policy]({config.PRIVACY_LINK}).
-
-If you have any questions or concerns, feel free to reach out to our [Support Team]({config.SUPPORT_GROUP}).
-"""
-
 
 @app.on_message(filters.command("privacy"))
 async def privacy(client, message: Message):
     keyboard = InlineKeyboardMarkup(
         [[InlineKeyboardButton("View Privacy Policy", url=config.PRIVACY_LINK)]]
     )
+    TEXT = f"""
+🔒 **Privacy Policy for {client.me.mention} !**
+
+Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: [Privacy Policy]({config.PRIVACY_LINK}).
+
+If you have any questions or concerns, feel free to reach out to our [Support Team]({config.SUPPORT_GROUP}).
+    """
+
     await message.reply_text(
         TEXT,
         reply_markup=keyboard,
