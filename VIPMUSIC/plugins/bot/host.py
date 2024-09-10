f2ff561fc444dd6c36472e3933e40cee80a1496a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-11 03:55:25 +0530

    Update host.py

--- VIPMUSIC/plugins/bot/host.py
+++ VIPMUSIC/plugins/bot/host.py
@@ -54,7 +54,7 @@ def make_heroku_request(endpoint, api_key, method="get", payload=None):
         )
 
 
-"""
+
 def make_heroku_request(endpoint, api_key, method="get", payload=None):
     headers = {
         "Authorization": f"Bearer {api_key}",
@@ -65,7 +65,7 @@ def make_heroku_request(endpoint, api_key, method="get", payload=None):
     response = getattr(requests, method)(url, headers=headers, json=payload)
     return response.status_code, response.json() if method != "get" else response
 
-
+"""
 def make_heroku_request(endpoint, api_key, method="get", payload=None):
     headers = {
         "Authorization": f"Bearer {api_key}",
