84c4729de1fa6eccf0081dd3e1d79479222208b7
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-10 23:24:35 +0530

    Update host.py

--- VIPMUSIC/plugins/sudo/host.py
+++ VIPMUSIC/plugins/sudo/host.py
@@ -1,6 +1,6 @@
 import os
 import socket
-
+import config
 import requests
 import urllib3
 from pyrogram import filters
@@ -44,8 +44,12 @@ def make_heroku_request(endpoint, api_key, method="get", payload=None):
     }
     url = f"{HEROKU_API_URL}/{endpoint}"
     response = getattr(requests, method)(url, headers=headers, json=payload)
-    return response.status_code, response.json() if method != "get" else response
 
+    # Return parsed JSON for `get` method as well
+    if method == "get":
+        return response.status_code, response.json()
+    else:
+        return response.status_code, response.json() if response.status_code == 200 else response.text
 
 async def collect_env_variables(message, env_vars):
     user_inputs = {}
