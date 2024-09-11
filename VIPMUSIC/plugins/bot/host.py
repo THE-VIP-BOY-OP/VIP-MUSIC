c00194fcb2bbd7121e3811425e504d0ce18d7945
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-11 19:55:11 +0530

    Update host.py

--- VIPMUSIC/plugins/bot/host.py
+++ VIPMUSIC/plugins/bot/host.py
@@ -7,7 +7,7 @@ import urllib3
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyromod.exceptions import ListenerTimeout
-
+import config
 from VIPMUSIC import app
 from VIPMUSIC.misc import SUDOERS
 from VIPMUSIC.utils.database import get_app_info, save_app_info
@@ -15,12 +15,15 @@ from VIPMUSIC.utils.pastebin import VIPbin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-HEROKU_API_URL = "https://api.heroku.com"
-HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")  # Pre-defined variable
-REPO_URL = "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"  # Pre-defined variable
-BUILDPACK_URL = "https://github.com/heroku/heroku-buildpack-python"
-UPSTREAM_REPO = "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"  # Pre-defined variable
-UPSTREAM_BRANCH = "master"  # Pre-defined variable
+HEROKU_API_URL = "heroku/python"
+HEROKU_API_URLS = "https://api.heroku.com"
+HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
+REPO_URL = "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
+BUILDPACK_URL = "https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest.git"
+BUILDPACK_URLS = "https://github.com/heroku/heroku-buildpack-python"
+
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO")  # Pre-defined variable
+UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH")  # Pre-defined variable
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 
