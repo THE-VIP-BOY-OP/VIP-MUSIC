e1ca3846a78e3f18a6daaf57dfdec226c054c093
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-12 05:10:22 +0530

    Update host.py

--- VIPMUSIC/plugins/bot/host.py
+++ VIPMUSIC/plugins/bot/host.py
@@ -38,7 +38,36 @@ def fetch_app_json(repo_url):
     response = requests.get(app_json_url)
     return response.json() if response.status_code == 200 else None
 
+def make_heroku_request(endpoint, api_key, method="get", payload=None):
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Accept": "application/vnd.heroku+json; version=3",
+        "Content-Type": "application/json",
+    }
+    url = f"{HEROKU_API_URL}/{endpoint}"
+    response = getattr(requests, method)(url, headers=headers, json=payload)
 
+    # Return parsed JSON for `get` method as well
+    if method == "get":
+        return response.status_code, response.json()
+    else:
+        return response.status_code, (
+            response.json() if response.status_code == 200 else response.text
+        )
+
+
+def make_heroku_request(endpoint, api_key, method="get", payload=None):
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Accept": "application/vnd.heroku+json; version=3",
+        "Content-Type": "application/json",
+    }
+    url = f"{HEROKU_API_URL}/{endpoint}"
+    response = getattr(requests, method)(url, headers=headers, json=payload)
+    return response.status_code, (
+        response.json() if response.status_code == 200 else None
+        )
+        
 def make_heroku_requesta(endpoint, api_key, method="get", payload=None):
     headers = {
         "Authorization": f"Bearer {api_key}",
