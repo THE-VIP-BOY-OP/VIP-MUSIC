389209d47878247b4f72243f505a18228e45f474
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-12 22:11:38 +0530

    Update hostcallback.py

--- VIPMUSIC/plugins/bot/hostcallback.py
+++ VIPMUSIC/plugins/bot/hostcallback.py
@@ -177,7 +177,7 @@ async def app_options(client, callback_query):
     buttons = [
         [
             InlineKeyboardButton(
-                "Edit Variables", callback_data=f"edit_vars:{app_name}"
+                "Variables", callback_data=f"edit_vars:{app_name}"
             ),
             InlineKeyboardButton("Get Logs", callback_data=f"get_logs:{app_name}"),
         ],
