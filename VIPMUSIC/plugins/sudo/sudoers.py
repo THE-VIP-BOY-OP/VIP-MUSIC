4efc421363ce30033b410bd6a0a20c7ccf15eae5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-12 07:12:48 +0530

    Update sudoers.py

--- VIPMUSIC/plugins/sudo/sudoers.py
+++ VIPMUSIC/plugins/sudo/sudoers.py
@@ -1,4 +1,4 @@
-from pyrogram import filters
+from pyrogram import filters, Client
 from pyrogram.types import (
     CallbackQuery,
     InlineKeyboardButton,
@@ -15,45 +15,43 @@ from VIPMUSIC.utils.extraction import extract_user
 
 
 @app.on_message(
-    filters.command(["addsudo"], prefixes=["/", "!", "%", ",", ".", "@", "#"])
+    filters.command(["addsudo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
     & filters.user(OWNER_ID)
 )
 @language
-async def useradd(client, message: Message, _):
+async def useradd(client: Client, message: Message, _):
     if not message.reply_to_message:
         if len(message.command) != 2:
             return await message.reply_text(_["general_1"])
     user = await extract_user(message)
     if user.id in SUDOERS:
-        return await message.reply_text(
-            _["sudo_1"].format(user.mention or user.first_name)
-        )
+        return await message.reply_text(_["sudo_1"].format(user.mention))
     added = await add_sudo(user.id)
     if added:
         SUDOERS.add(user.id)
-        await message.reply_text(_["sudo_2"].format(user.mention or user.first_name))
+        await message.reply_text(_["sudo_2"].format(user.mention))
     else:
         await message.reply_text(_["sudo_8"])
 
 
 @app.on_message(
-    filters.command(["delsudo", "rmsudo"], prefixes=["/", "!", "%", ",", ".", "@", "#"])
+    filters.command(
+        ["delsudo", "rmsudo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]
+    )
     & filters.user(OWNER_ID)
 )
 @language
-async def userdel(client, message: Message, _):
+async def userdel(client: Client, message: Message, _):
     if not message.reply_to_message:
         if len(message.command) != 2:
             return await message.reply_text(_["general_1"])
     user = await extract_user(message)
     if user.id not in SUDOERS:
-        return await message.reply_text(
-            _["sudo_3"].format(user.mention or user.first_name)
-        )
+        return await message.reply_text(_["sudo_3"].format(user.mention))
     removed = await remove_sudo(user.id)
     if removed:
         SUDOERS.remove(user.id)
-        await message.reply_text(_["sudo_4"].format(user.mention or user.first_name))
+        await message.reply_text(_["sudo_4"].format(user.mention))
     else:
         await message.reply_text(_["sudo_8"])
 
@@ -64,11 +62,11 @@ photo_url = "https://telegra.ph/file/20b4a9fd06ea4a9457a61.jpg"
 @app.on_message(
     filters.command(
         ["sudolist", "listsudo", "sudoers"],
-        prefixes=["/", "!", "%", ",", ".", "@", "#"],
+        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
     )
     & ~BANNED_USERS
 )
-async def sudoers_list(client, message: Message):
+async def sudoers_list(client: Client, message: Message):
     keyboard = [
         [InlineKeyboardButton("๏ ᴠɪᴇᴡ sᴜᴅᴏʟɪsᴛ ๏", callback_data="check_sudo_list")]
     ]
@@ -81,15 +79,16 @@ async def sudoers_list(client, message: Message):
 
 
 @app.on_callback_query(filters.regex("^check_sudo_list$"))
-async def check_sudo_list(client, callback_query: CallbackQuery):
+async def check_sudo_list(client: Client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
         return await callback_query.answer(
-            "You are not authorized to view the sudo list.", show_alert=True
+            "𝐍𝐢𝐤𝐚𝐥 𝐑𝐚𝐧𝐝𝐢 𝐁𝐚𝐥𝐚 𝐒𝐮𝐝𝐨𝐥𝐢𝐬𝐭 𝐃𝐞𝐤𝐡𝐧𝐞 𝐀𝐚𝐲𝐚 𝐇𝐚𝐢 𝐛𝐚𝐝𝐚🖕😎😂", show_alert=True
         )
     else:
         user = await app.get_users(OWNER_ID)
-        user_mention = user.mention or user.first_name
+
+        user_mention = user.first_name if not user.mention else user.mention
         caption = f"**˹ʟɪsᴛ ᴏғ ʙᴏᴛ ᴍᴏᴅᴇʀᴀᴛᴏʀs˼**\n\n**🌹Oᴡɴᴇʀ** ➥ {user_mention}\n\n"
 
         keyboard.append(
@@ -105,7 +104,9 @@ async def check_sudo_list(client, callback_query: CallbackQuery):
             if user_id != OWNER_ID:
                 try:
                     user = await app.get_users(user_id)
-                    user_mention = user.mention or f"**🎁 Sᴜᴅᴏ {count} ɪᴅ:** {user_id}"
+                    user_mention = (
+                        user.mention if user else f"**🎁 Sᴜᴅᴏ {count} ɪᴅ:** {user_id}"
+                    )
                     caption += f"**🎁 Sᴜᴅᴏ** {count} **»** {user_mention}\n"
                     button_text = f"๏ ᴠɪᴇᴡ sᴜᴅᴏ {count} ๏ "
                     keyboard.append(
@@ -132,14 +133,14 @@ async def check_sudo_list(client, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("^back_to_main_menu$"))
-async def back_to_main_menu(client, callback_query: CallbackQuery):
+async def back_to_main_menu(client: Client, callback_query: CallbackQuery):
     keyboard = [
         [InlineKeyboardButton("๏ ᴠɪᴇᴡ sᴜᴅᴏʟɪsᴛ ๏", callback_data="check_sudo_list")]
     ]
-    reply_markups = InlineKeyboardMarkup(keyboard)
+    reply_markupes = InlineKeyboardMarkup(keyboard)
     await callback_query.message.edit_caption(
         caption="**» ᴄʜᴇᴄᴋ sᴜᴅᴏ ʟɪsᴛ ʙʏ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ.**\n\n**» ɴᴏᴛᴇ:**  ᴏɴʟʏ sᴜᴅᴏ ᴜsᴇʀs ᴄᴀɴ ᴠɪᴇᴡ. ",
-        reply_markup=reply_markups,
+        reply_markup=reply_markupes,
     )
 
 
@@ -151,3 +152,4 @@ __HELP__ = """
 
 # Commands for SUDOERS:
 """
+    
