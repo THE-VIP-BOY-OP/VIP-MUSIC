6a366f08695062ebd861823fdb7a7454b5f510cd
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-14 20:45:07 +0530

    Update hostcallback.py

--- VIPMUSIC/plugins/bot/hostcallback.py
+++ VIPMUSIC/plugins/bot/hostcallback.py
@@ -923,7 +923,7 @@ async def check_and_restart_apps():
     while True:
         apps = await fetch_apps()  # Get the list of apps
         if not apps:
-            await asyncio.sleep(600)  # Sleep for 10 minutes
+            await asyncio.sleep(5)  # Sleep for 10 minutes
             continue
 
         for app in apps:
