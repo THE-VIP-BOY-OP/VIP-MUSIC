18312796b5321fa79868617a37137c092aa47a4a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-22 01:58:12 +0530

    Update host.py

--- VIPMUSIC/plugins/bot/host.py
+++ VIPMUSIC/plugins/bot/host.py
@@ -289,13 +289,11 @@ async def handle_repo_choice(client, callback_query):
 
             branches = await fetch_repo_branches(REPO_URL)
 
-            if (
-                branches is "list index out of range" or "None"
-            ):  # Check if branches are None, indicating an error
-                await callback_query.message.reply_text(
-                    "Your repo is either private or wrong. Please try again from /host."
-                )
+            if branches in ["list index out of range", None]:
+                await callback_query.message.reply_text("Your repo is either private or wrong. Please try again from /host.")
                 return
+    
+    
 
             default_branch = "master"  # Or fetch the actual default branch dynamically
             await ask_for_branch(callback_query, branches, default_branch)
