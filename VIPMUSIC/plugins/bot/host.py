2f644d4cb109d6bc63fc99f6c3e3ab3765754bd2
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-22 19:15:34 +0530

    Update host.py

--- VIPMUSIC/plugins/bot/host.py
+++ VIPMUSIC/plugins/bot/host.py
@@ -301,11 +301,13 @@ async def handle_repo_choice(client, callback_query):
                 "**Please provide me any public external GitHub repo URL:**\n\nType /cancel for cancel the process",
                 timeout=300,
             )
-            REPO_URL = response.text  # Set the external repo URL
-            if REPO_URL == "/cancel":
+              # Set the external repo URL
+            if response.text == "/cancel":
                 await message.reply_text("**Deployment canceled.**")
                 REPO_URL = "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
-                return None  # Cancel the process and return
+                return None
+                
+            REPO_URL = response.text# Cancel the process and return
             branches = await fetch_repo_branches(REPO_URL)
 
             if branches is None:
