da6be2a329ef3a1ee05e1a6b9a7b2d2d9c7e6fa4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-23 14:04:10 +0530

    Update seeker.py

--- VIPMUSIC/plugins/misc/seeker.py
+++ VIPMUSIC/plugins/misc/seeker.py
@@ -35,7 +35,7 @@ asyncio.create_task(timer())
 
 
 async def markup_timer():
-    while not await asyncio.sleep(30):
+    while not await asyncio.sleep(2):
         active_chats = await get_active_chats()
         for chat_id in active_chats:
             try:
