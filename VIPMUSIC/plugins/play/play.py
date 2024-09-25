447bebe383b86b63e063351d45e4fa7b2489a36d
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-26 02:58:14 +0530

    Update play.py

--- VIPMUSIC/plugins/play/play.py
+++ VIPMUSIC/plugins/play/play.py
@@ -122,19 +122,20 @@ async def play_commnd(
     url,
     fplay,
 ):
-    user_id = message.from_user.id
+    
     userbot = await get_assistant(message.chat.id)
     userbot_id = userbot.id
 
-    # Check if the userbot is in the VC or not
+    
     try:
         async for member in userbot.get_call_members(message.chat.id):
             if not member.user.id == userbot_id:
-                # Userbot is in VC, proceed with play function
+                
                 await stop_stream_if_not_in_vc(client, message, _)
     except Exception as e:
-        print(f"Error checking voice chat members: {e}")  # Log the specific exception
-
+        print(f"Error checking voice chat members: {e}")
+        
+    user_id = message.from_user.id
     current_time = time()
     last_message_time = user_last_message_time.get(user_id, 0)
 
