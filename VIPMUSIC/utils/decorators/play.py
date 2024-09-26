1b66480da4d738178c87610144ba01a1d3cbfa1a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-26 21:59:09 +0530

    Update play.py

--- VIPMUSIC/utils/decorators/play.py
+++ VIPMUSIC/utils/decorators/play.py
@@ -209,7 +209,7 @@ def PlayWrapper(command):
             member.chat.id async for member in userbot.get_call_members(chat_id)
         ]
         if await is_active_chat(chat_id) and userbot.id not in call_participants_id:
-            await VIP.stop_stream(chat_id)
+            await VIP.st_stream(chat_id)
 
         return await command(
             client,
