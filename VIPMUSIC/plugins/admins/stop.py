d7c144e200a83e2d39ba891af53ef23e2986b8d3
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-26 22:29:59 +0530

    Update stop.py

--- VIPMUSIC/plugins/admins/stop.py
+++ VIPMUSIC/plugins/admins/stop.py
@@ -174,7 +174,11 @@ async def assistant_banned(client: app, member: ChatMemberUpdated):
             await app.unban_chat_member(chat_id, userbot.id)
             await asyncio.sleep(10)
     except UserNotParticipant:
-        return
+        return await app.send_photo(chat_id, photo=random.choice(photo), caption=left_message, reply_markup=keyboard)
+               await VIP.st_stream(chat_id)
+               await set_loop(chat_id, 0)
+               await app.unban_chat_member(chat_id, userbot.id)
+               await asyncio.sleep(10)
     except Exception as e:
         return
 
@@ -186,35 +190,25 @@ async def assistant_left(client: app, member: ChatMemberUpdated):
     try:
         userbot = await get_assistant(member.chat.id)
         get = await app.get_chat_member(chat_id, userbot.id)
-        if get.status in [ChatMemberStatus.LEFT]:
+        if get.status not in [ChatMemberStatus.BANNED, ChatMemberStatus.RESTRICTED, ChatMemberStatus.KICKED]:
 
-            # Assistant bot has been banned
-            remove_by = member.from_user.mention if member.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
-            chat_id = member.chat.id
-            title = member.chat.title
-            username = (
-                f"@{member.chat.username}" if member.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
-            )
-
-            # Construct message
+            
             left_message = f"**Assistant Has Left This Chat**\n\n**Id:** `{userbot.id}`\n**Name:** @{userbot.username}\n\n**Invite Assistant By: /userbotjoin**"
-
-            # Create keyboard for unban button
-
-            # Send photo with the left message and keyboard
             await app.send_photo(
                 chat_id,
                 photo=random.choice(photo),
                 caption=left_message,
                 reply_markup=keyboard,
             )
-            # Perform actions like stopping streams or loops
+            
             await VIP.st_stream(chat_id)
             await set_loop(chat_id, 0)
-
             await asyncio.sleep(10)
     except UserNotParticipant:
-        return
+        return return await app.send_photo(chat_id, photo=random.choice(photo), caption=left_message, reply_markup=keyboard)
+               await VIP.st_stream(chat_id)
+               await set_loop(chat_id, 0)
+               await asyncio.sleep(10)
     except Exception as e:
         return
 
