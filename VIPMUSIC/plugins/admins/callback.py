f304feb862bd27f1fdc3102351db3ef3beb47c8b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-26 07:53:57 +0530

    Update callback.py

--- VIPMUSIC/plugins/admins/callback.py
+++ VIPMUSIC/plugins/admins/callback.py
@@ -452,7 +452,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
-                    title,
+                    f"https://t.me/{app.username}?start=info_{videoid}",
                     title[:23],
                     duration,
                     user,
