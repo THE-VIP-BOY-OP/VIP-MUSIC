bc221b96b4b095dc4a9b0d4649f9824c2ea6a874
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-13 00:15:43 +0530

    Update git.py

--- VIPMUSIC/core/git.py
+++ VIPMUSIC/core/git.py
@@ -82,4 +82,4 @@ def git():
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     install_req("pip3 install --no-cache-dir -r requirements.txt")
-    LOGGER(__name__).info(f"Fetched Updates from: {REPO_LINK}")
+    LOGGER(__name__).info(f"Fetched Updates from: UPSTREAM_REPO")
