28aa9ba86fb49dd653ddfc40851589bce7b1c743
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-15 14:31:13 +0530

    Update play.py

--- VIPMUSIC/plugins/play/play.py
+++ VIPMUSIC/plugins/play/play.py
@@ -303,7 +303,7 @@ async def play_commnd(
                     f"**ʜᴇʏ [ᴏᴡɴᴇʀ](tg://user?id={OWNER_ID[0]}) ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs**",
                 )
                 return await app.send_message(
-                    OWNER_ID,
+                    OWNER_ID[0],
                     f"**ʜᴇʏ [ᴏᴡɴᴇʀ](tg://user?id={OWNER_ID[0]}) ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs**",
                 )
 
