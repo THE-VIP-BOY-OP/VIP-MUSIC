47395ca422009f2912ea0187a9d874c1d4b75bd4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-21 16:41:51 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -27,15 +27,15 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 
-TOKEN_DATA = 
+TOKEN_DATA = "
 {
     "access_token": "ya29.a0AcM612x6xjTKmTpSHstn1vpFe4R-4nlC-a537bZMFvBAKrDA7MdjgKj71_dLwijsrP5ZLSKFIUNQyHpFOjGh5ifATiFHyXhc4sArPYlbIa_9PqfVJMyfDWLa8vw19SZwHo--fxjDusyV5nqvoGYNcIqyTN8zG7gJl3YIvcSa0P0-rLQNAs0AaCgYKAb0SARMSFQHGX2MivXte9hRxxKS07uayRvAw7w0187",
-    "expires": "1729406829.524444",
+    "expires": 1729406829.524444,
     "refresh_token": "1//05vYI0c8OP0b4CgYIARAAGAUSNwF-L9IrJvP8EzLj-4wkJD-hYD9y1fXRNSGS9CjEQ1YwRxFw1OjatSgXsGooDbs5QcqAPOs3TvM",
     "token_type": "Bearer"
 }
 
-
+"
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 
