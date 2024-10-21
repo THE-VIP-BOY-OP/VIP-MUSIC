92524845239bf92c73d64b64e60f9ada9fcb99fe
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-21 16:49:05 +0530

    Update Youtube.py

--- VIPMUSIC/platforms/Youtube.py
+++ VIPMUSIC/platforms/Youtube.py
@@ -36,33 +36,33 @@ def cookies():
 def get_ytdl_options(ytdl_opts, commamdline=True) -> Union[str, dict, list]:
     if commamdline:
         if isinstance(ytdl_opts, list):
-            if config.TOKEN_DATA:
+            if os.getenv("TOKEN_DATA"):
                 ytdl_opts += ["--username", "oauth2", "--password", "''"]
             else:
                 ytdl_opts += ["--cookies", cookies()]
         elif isinstance(ytdl_opts, str):
-            if config.TOKEN_DATA:
+            if os.getenv("TOKEN_DATA"):
                 ytdl_opts += "--username oauth2 --password '' "
             else:
                 ytdl_opts += f"--cookies {cookies()}"
         elif isinstance(ytdl_opts, dict):
-            if config.TOKEN_DATA:
+            if os.getenv("TOKEN_DATA"):
                 ytdl_opts.update({"username": "oauth2", "password": ""})
             else:
                 ytdl_opts["cookiefile"] = cookies()
     else:
         if isinstance(ytdl_opts, list):
-            if config.TOKEN_DATA:
+            if os.getenv("TOKEN_DATA"):
                 ytdl_opts += ["username", "oauth2", "password", "''"]
             else:
                 ytdl_opts += ["cookiefile", cookies()]
         elif isinstance(ytdl_opts, str):
-            if config.TOKEN_DATA:
+            if os.getenv("TOKEN_DATA"):
                 ytdl_opts += "username oauth2 password '' "
             else:
                 ytdl_opts += f"cookiefile {cookies()}"
         elif isinstance(ytdl_opts, dict):
-            if config.TOKEN_DATA:
+            if os.getenv("TOKEN_DATA"):
                 ytdl_opts.update({"username": "oauth2", "password": ""})
             else:
                 ytdl_opts["cookiefile"] = cookies()
