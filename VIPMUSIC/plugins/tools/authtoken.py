0d2798639e601d08dcbc2620575ffb684159d39f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-25 00:44:01 +0530

    Update authtoken.py

--- VIPMUSIC/plugins/tools/authtoken.py
+++ VIPMUSIC/plugins/tools/authtoken.py
@@ -1,22 +1,18 @@
 import os
 
 from pyrogram import filters
-
+from VIPMUSIC.misc import SUDOERS
 from VIPMUSIC import app
 
 
-@app.on_message(filters.command("authtoken"))
+@app.on_message(filters.command("authtoken") & SUDOERS)
 async def list_formats(client, message):
     try:
-        youtube_url = "https://www.youtube.com/watch?v=LLF3GMfNEYU"
-
         await message.reply_text(
             "**Go to Logs and verify to google account then save token data in variable**\n\n**If you dont want to generate new token data then just restart bot otherwise bot will stay offline**"
         )
 
-        command = f"yt-dlp -F {youtube_url}"
-
-        result = os.popen(command).read()
+        os.system(f"yt-dlp --username oauth2 --password '' -F https://www.youtube.com/watch?v=LLF3GMfNEYU")
 
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
