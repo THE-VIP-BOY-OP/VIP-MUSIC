b02d918efd769781aa941cfc61ad8d5974357d8e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-25 11:39:59 +0530

    Update Youtube.py

--- VIPMUSIC/platforms/Youtube.py
+++ VIPMUSIC/platforms/Youtube.py
@@ -55,7 +55,7 @@ async def check_auth_token():
 
 
 def get_ytdl_options(ytdl_opts, commamdline=True) -> Union[str, dict, list]:
-    use_token = asyncio.run(check_auth_token())  # Check if OAuth token is valid
+    use_token = await check_auth_token()
 
     if commamdline:
         if isinstance(ytdl_opts, list):
