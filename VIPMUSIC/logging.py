f4c73326512c70dc851cd6c1280317382599eb95
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-25 07:36:52 +0530

    Update logging.py

--- VIPMUSIC/logging.py
+++ VIPMUSIC/logging.py
@@ -23,7 +23,7 @@ logging.basicConfig(
     ],
 )
 
-logging.getLogger(" apscheduler").setLevel(logging.ERROR)
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR)
