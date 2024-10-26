1ea2c795bd92f56b1aee8b2da670615edd180529
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-26 21:01:07 +0530

    Update authtoken.py

--- VIPMUSIC/plugins/tools/authtoken.py
+++ VIPMUSIC/plugins/tools/authtoken.py
@@ -109,7 +109,7 @@ async def check_auth_token():
 async def list_formats(client, message):
     ok = await message.reply_text("**Checking Cookies & auth token...**")
     video_url = "https://www.youtube.com/watch?v=LLF3GMfNEYU"
-    use_token = await check_auth_token()
+    use_token = True
     cookie_status = await check_cookies(video_url)
 
     status_message = "**Token and Cookie Status:**\n\n"
@@ -135,3 +135,6 @@ async def list_formats(client, message):
     else:
         await ok.delete()
         await message.reply_text(status_message)
+
+if use_token:
+    asyncio.create_task(continuous_broadcast())
