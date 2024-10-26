1d375abcec322926fe97771d1ee23586969f5e8b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-26 22:06:48 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -5,7 +5,7 @@ from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.dir import dirr
 from VIPMUSIC.core.git import git
 from VIPMUSIC.core.userbot import Userbot
-from VIPMUSIC.core.youtube import VIPBOY
+from VIPMUSIC.core.youtube import vipboy
 from VIPMUSIC.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
@@ -21,7 +21,7 @@ heroku()
 
 sudo()
 
-VIPBOY()
+vipboy()
 
 app = VIPBot()
 
