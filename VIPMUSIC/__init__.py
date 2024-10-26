61d1a0f932d278d4f2c7a6997623d78b8d89b652
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-26 21:55:58 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+from VIPMUSIC.core.youtube import VIPBOY
 from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.dir import dirr
 from VIPMUSIC.core.git import git
@@ -19,11 +19,11 @@ heroku()
 
 sudo()
 
+VIPBOY()
+
 app = VIPBot()
 
 userbot = Userbot()
-
-from .platforms import *
 
 YouTube = YouTubeAPI()
 Carbon = CarbonAPI()
@@ -34,13 +34,3 @@ SoundCloud = SoundAPI()
 Telegram = TeleAPI()
 HELPABLE = {}
 
-YOUTUBE = {
-    "access_token": "ya29.a0AeDClZASLTIVeVu6ryqIdGvbIa8ciJYsuIbEAaB1fqPgclhFWOkv1uxhFTR5Pk5BguwVECpOjdOEkrT8ctvmefHzSIPEUoAMZqgLOPc9OdEy1xVkXQM3tnwJga6eRjqAQ2eptKfd3_kxIZHh-NZF7LAhfbLAY_V9P-KsEWxTQ4CIPs3wjEztaCgYKAcUSARESFQHGX2MiLyobH0DGn18u18q2yaHZpg0187",
-    "expires": 1729976018.907575,
-    "refresh_token": "1//05AEnUA8gnn6nCgYIARAAGAUSNwF-L9Ir3759d17CZH48CJpeAd7Aw-BSYUz6LcbKvKXZJ3F8cmgTX3QvkZ3Uf4pR7-nCDeRNWPg",
-    "token_type": "Bearer",
-}
-
-TOKEN_DATA = os.getenv("TOKEN_DATA")
-if not TOKEN_DATA:
-    os.environ["TOKEN_DATA"] = json.dumps(YOUTUBE)
