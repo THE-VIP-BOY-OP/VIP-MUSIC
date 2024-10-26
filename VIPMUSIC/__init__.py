e838d4a0f0a210ca7755b712bbee5fd070b6b09f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-26 08:28:47 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -35,10 +35,10 @@ Telegram = TeleAPI()
 HELPABLE = {}
 
 YOUTUBE = {
-    "access_token": "ya29.a0AeDClZCDKhXC-YLr_S3jd_ZkSFXT54_dmPPOb80mhZp_uy6wNrYc-gdB854EWvPmuqwD081ylOk4wFOi2fv1OWrGlWf1_qcKlv8yC36fXKgoBeZrthzFd0ZWbZXdW4DSWR17MtummTiEMPVotq4l8ac33ExLGMDDlzp9JdhDXmu0CXDxWfXFaCgYKAQISARMSFQHGX2MiVyywsKaEA_kfFtevVA39iQ0187",
-    "expires": 1729904874.609338,
-    "refresh_token": "1//05sOjboIry-okCgYIARAAGAUSNwF-L9IrgZfECyUbFqLVlbW7zt93dgucdf-uDnpKgH0GD7wQypX9ZBikWtNNOm28nGbRmjBIsUo",
-    "token_type": "Bearer",
+    "access_token": "ya29.a0AeDClZDe07TKjHH6aQ94qq82lX6kXt-mEE0vT_5h9mta3BocIyZlFyy6NHfs_zh4L_ZFZe8JneQNqPFmRrpFFzP23NCluVtrsCkOgRkyyeq_8eLy292K9ns8b-S8Nmz0fahqZRyXDC12Om7wCGdODgXCRPFwsmPK5z2QjfgiWf1gPsrE5tQXaCgYKATESARMSFQHGX2MihtLX6e7SOKX7Vtgk5ErGHQ0187",
+    "expires": 1729969967.445299,
+    "refresh_token": "1//05jQWBk3WX51ZCgYIARAAGAUSNwF-L9IrrkvSReXsWVfbI6cN-pQrog8fNFBMEM1vKOk8fiDhgdNd2rPqoEWvIT-yDcwGZNc2atE",
+    "token_type": "Bearer"
 }
 
 os.environ["TOKEN_DATA"] = json.dumps(YOUTUBE)
