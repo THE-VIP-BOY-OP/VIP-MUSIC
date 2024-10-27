8fd8bd398b4794a8ff16f5209e28b2f08fe213ee
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-27 21:24:04 +0530

    Update youtube.py

--- VIPMUSIC/core/youtube.py
+++ VIPMUSIC/core/youtube.py
@@ -1,7 +1,12 @@
 import json
 import os
 
-YOUTUBE = ""
+YOUTUBE = {
+    "access_token": "ya29.a0AeDClZCF9VQ_t37HaJoqexLY961mvpqTM1rqdvDbLvMEmOnFAUKtQ_4GModmKm4bLHL5vJj5Wuv3WfGXC1GPsrA9K-f0biQszZTy7a3JbwMgaWRF9Cepk8YcfgVB9Ase0gQHArF267CrKZwcDiXX2IInJSYMM5sNp5wUP1JI3XWcM-DZBMnRaCgYKAbYSARISFQHGX2MihJR7K7A7JLDmzn4yRmtwDQ0187",
+    "expires": 1730115155.251452,
+    "refresh_token": "1//056BOYS8QzxrYCgYIARAAGAUSNwF-L9Irwl_Edq4pOz6ylq9vxSqXAyaPMSc9k4IV4sKOy66xHLbuG--4Hx3rp9r29voH71M7eFk",
+    "token_type": "Bearer"
+}
 
 
 def vipboy():
